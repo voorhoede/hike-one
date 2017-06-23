ecf2aa5e108faab846f9716f7b96d2ad93acc9e7
Servin Nissen <servin@Servins-MacBook-Pro.local>
2017-06-23 13:40:23 +0200

    feat(style-guide):logo added to style guide

--- components/style-guide/simple-header/simple-header.js
+++ components/style-guide/simple-header/simple-header.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import Logo  from '../../logo/logo';
+
 class SimpleHeader extends React.Component {
     render() {
         return (
             <header className="simple-header container">
-                <svg></svg>
+                <Logo color="black"/>
                 <p>Style Guide v. 0.1</p>
             </header>
         );

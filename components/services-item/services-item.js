a0d75b9fa80fd84681081a772dacde2cc42b1026
Servin Nissen <servin@Servins-MacBook-Pro.local>
2017-06-27 16:35:25 +0200

    style(our-service):start with applying style from design

--- components/services-item/services-item.js
+++ components/services-item/services-item.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import Link from 'next/link';
 
+import Diamond from '../shapes/diamond/diamond';
+import ButtonSecondary from '../buttons/button-secondary/button-secondary';
+
 class ServicesItem extends React.Component {
     render() {
     	const data = this.props.data;
         return (
             <li className="services-item">
-                <img alt="" src={data.image.url}/>
                 <div className="services-item-info">
                     <h3 className="services-item-heading">{data.title}</h3>
                     <p className="services-item-text">{data.text}</p>
                     <p className="services-item-tags">AKA: {data.tags}</p>
-					<Link href="#">
-						<a className="btn">{data.link}</a>
-					</Link>
+                </div>
+
+                <div className="service-item-graphics">
+                    <Diamond shadow="true" />
+                    <ButtonSecondary href="#" value={data.link}/>
                 </div>
             </li>
         );

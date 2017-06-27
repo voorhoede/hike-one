cf7deb617b36056beca550e7137f75acf100b588
Servin Nissen <servin@Servins-MacBook-Pro.local>
2017-06-27 10:49:00 +0200

    feat(footer):the footer component and all social media icons wrapped in one component

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,10 +1,94 @@
-import React from 'react';
+import React 	   from 'react';
+import Link  	   from 'next/link';
+
+import Logo 	   from '../logo/logo';
+import SocialMedia from '../social-media/social-media';
+
+import ArrowRight  from '../icons/arrow-right/arrow-right';
 
 class Footer extends React.Component {
+
+	toggleLocationInfo(target) {
+		document.querySelector('.locations .active').classList.remove('active')
+		document.querySelector(`.locations [data-target='${target}']`).classList.add('active')
+
+		document.querySelector('.locations-info .active').classList.remove('active')
+		document.querySelector(`.locations-info .${target}`).classList.add('active')
+	}
+
 	render() {
 		return (
 			<footer className="footer">
-				<span>Footer</span>
+				<div className="container">
+					<Link href="/" >
+						<a className="header-logo">
+							<Logo color="full-white"/>
+							<h1 className="a11y-sr-only">Hike one</h1>
+						</a>
+					</Link>
+
+					<div className="footer-main-content">
+						<div className="left">
+							<div className="core-lists">
+								<ul className="no-style core-items">
+									<li><Link href="#"><a>Design</a></Link></li>
+									<li><Link href="#"><a>Research</a></Link></li>
+									<li><Link href="#"><a>Strategy</a></Link></li>
+									<li><Link href="#"><a>Training</a></Link></li>
+								</ul>
+
+								<ul className="no-style sub-core-items">
+									<li><Link href="#"><a>Team</a></Link></li>
+									<li><Link href="#"><a>Services</a></Link></li>
+									<li><Link href="#"><a>Work</a></Link></li>
+									<li><Link href="#"><a>Contact</a></Link></li>
+									<li><Link href="#"><a>Updates</a></Link></li>
+									<li><Link href="#"><a>Playground</a></Link></li>
+								</ul>
+							</div>
+
+							<Link href="#"><a className="join-us-button">Up for a new challenge yourself? Join us! <ArrowRight /> </a></Link>
+						</div>
+
+						<div className="right">
+							<div className="phone-and-email">
+								<div className="phone-number">+31 20 204 45 77</div>
+								<Link href="mailto:hello@hike.one"><a className="email-adress">hello@hike.one</a></Link>
+							</div>
+
+							<div>
+								<ul className="no-style locations">
+									<li><button onClick={this.toggleLocationInfo.bind(this, 'ams')} className="active" data-target="ams">Ams</button></li>
+
+									<li><button onClick={this.toggleLocationInfo.bind(this, 'rtm')} data-target="rtm">Rtm</button></li>
+
+									<li><button onClick={this.toggleLocationInfo.bind(this, 'ein')} data-target="ein">Ein</button></li>
+								</ul>
+
+								<div className="locations-info">
+									<div className="ams active">
+										<p>Rijnsburgstraat 9-11</p>
+										<p>1059AT Amsterdam</p>
+									</div>
+
+									<div className="rtm">
+										<p>Schiedamsedijk 40a</p>
+										<p>3011ED Rotterdam</p>
+									</div>
+
+									<div className="ein">
+										<p>Rijnsburgstraat 9-11</p>
+										<p>1059AT Amsterdam</p>
+									</div>
+								</div>
+							</div>
+
+							<SocialMedia />
+						</div>
+					</div>
+
+					<p className="copyright">@ Hike One 2017</p>
+				</div>
 			</footer>
 		);
 	}
--- components/menu/menu.js
+++ components/menu/menu.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 
-import Facebook  from '../icons/facebook/facebook';
-import Twitter   from '../icons/twitter/twitter';
-import Instagram from '../icons/instagram/instagram';
-import LinkedIn  from '../icons/linkedin/linkedin';
-import Medium    from '../icons/medium/medium';
+import SocialMedia from '../social-media/social-media';
 
 class Menu extends React.Component {
 	render() {
@@ -18,17 +14,14 @@ class Menu extends React.Component {
 						<li><Link href="/work"><a>Work</a></Link></li>
 						<li><Link href="/contact"><a>Contact</a></Link></li>
 					</ul>
+
 					<ul className="menu-list menu-list-sub">
 						<li><Link href="/updates"><a>Updates</a></Link></li>
 						<li><Link href="/playground"><a>Playground</a></Link></li>
 					</ul>
-					<div className="menu-social">
-						<Link href="#"><a><Facebook /></a></Link>
-						<Link href="#"><a><Twitter /></a></Link>
-						<Link href="#"><a><Instagram /></a></Link>
-						<Link href="#"><a><LinkedIn /></a></Link>
-						<Link href="#"><a><Medium /></a></Link>
-					</div>
+
+					<SocialMedia />
+
 				</div>
 			</div>
 		);

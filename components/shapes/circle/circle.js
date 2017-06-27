522bbc53429a4bfed50c97c2f2ad0cfdca7125a5
Servin Nissen <servin@Servins-MacBook-Pro.local>
2017-06-27 16:03:48 +0200

    feat(style-guide-graphic-elements):graphic elements added to style guide and as components

---
+++ components/shapes/circle/circle.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Circle extends React.Component {
+    render() {
+        return (
+            <svg className={[this.props.shadow == "true" ? 'shadow' : '', 'circle'].join(' ')} xmlns="http://www.w3.org/2000/svg" width="300" height="300" viewBox="0 0 300 300">
+                <g fill="none" fillRule="evenodd">
+                    <rect className="circle-bg" width="299.511" height="299.824" rx="4"/>
+                    <circle className="circle-front" cx="150" cy="150" r="100" />
+                </g>
+            </svg>
+
+        );
+    }
+}
+
+export default Circle;
---
+++ components/shapes/diamond/diamond.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Diamond extends React.Component {
+    render() {
+        return (
+            <svg className={[this.props.shadow == "true" ? 'shadow' : '', 'diamond'].join(' ')} xmlns="http://www.w3.org/2000/svg" width="300" height="300" viewBox="0 0 300 300">
+                <g fill="none" fillRule="evenodd">
+                    <rect className="bg" width="299.848" height="299.973" rx="4"/>
+                    <rect className="front" width="144.23" height="144.29" x="77.607" y="79.804" rx="4" transform="rotate(45 149.722 151.95)"/>
+                </g>
+            </svg>
+        );
+    }
+}
+
+export default Diamond;

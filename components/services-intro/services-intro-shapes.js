1da3619003f913198c4d93427e123b4100d623b5
tjerksmit <tjerksmit@gmail.com>
2017-06-30 15:02:48 +0200

    feat(parallax): only animate layers when in view

--- components/services-intro/services-intro-shapes.js
+++ components/services-intro/services-intro-shapes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import elementIsInView from '../_helpers/elementIsInView';
 import CircleBorder from '../shapes/circle-border/circle-border';
 import DiamondBorder from '../shapes/diamond-border/diamond-border';
 import RectangleBorder from '../shapes/rectangle-border/rectangle-border';
@@ -9,42 +10,49 @@ class Shapes extends React.Component {
 	constructor() {
 		super();
 		this.onScroll = this.onScroll.bind(this);
+		this.addScrollEventListener = this.addScrollEventListener.bind(this);
 		this.animateLayers = this.animateLayers.bind(this);
+		this.ticking = false;
 	}
 
 	componentDidMount() {
-		this.animateLayers();
+		this.addScrollEventListener();
 	}
 
-	animateLayers() {
-		let ticking = false;
+	addScrollEventListener() {
 		window.addEventListener('scroll', () => {
-			const scrolledHeight = window.pageYOffset.toFixed();
-
-			if (!ticking) {
-				window.requestAnimationFrame(() => {
-					this.onScroll(scrolledHeight);
-					ticking = false;
-				});
+			if (!this.ticking) {
+				this.onScroll();
 			}
-			ticking = true;
+			this.ticking = true;
 		});
 	}
 
-	onScroll(scrolledHeight) {
-		this.front.style.transform = `translate3d(0px, ${-scrolledHeight * 0.5}px, 0px)`;
-		this.back.style.transform = `translate3d(0px, ${scrolledHeight * 0.3}px, 0px)`;
+	onScroll() {
+		const scrolledHeight = window.pageYOffset.toFixed();
+
+		window.requestAnimationFrame(() => {
+			this.animateLayers(scrolledHeight);
+			this.ticking = false;
+		});
+	}
+
+	animateLayers(scrolledHeight) {
+		if (!elementIsInView(this.frontLayer)) {return;} // don't animate when element is not in view
+
+		this.frontLayer.style.transform = `translate3d(0px, ${-scrolledHeight * 0.5}px, 0px)`;
+		this.backLayer.style.transform = `translate3d(0px, ${scrolledHeight * 0.3}px, 0px)`;
 	}
 
 	render() {
 		return (
 			<div className="services-intro-shapes">
-				<div ref={(node) => this.back = node} className="shapes-back" >
+				<div ref={(node) => this.backLayer = node} className="shapes-back" >
 					<RectangleBorder classes="services-intro-rectangle-1" color="red"/>
 					<DiamondBorder classes="services-intro-diamond-1" color="yellow"/>
 					<CircleBorder classes="services-intro-circle-1" color="green" />
 				</div>
-				<div ref={(node) => this.front = node} className="shapes-front">
+				<div ref={(node) => this.frontLayer = node} className="shapes-front">
 					<Triangle classes="services-intro-triangle-1" color="red" />
 					<Triangle classes="services-intro-triangle-2" color="purple" />
 					<Triangle classes="services-intro-triangle-3" color="blue" shadow/>

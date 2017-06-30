262ae00385aa2861658c75926a82af35b36d6359
tjerksmit <tjerksmit@gmail.com>
2017-06-30 16:04:28 +0200

    feat(parallax): is now separate component

---
+++ components/parallax/parallax.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import elementIsInView from '../_helpers/elementIsInView';
+
+class parallax extends React.Component {
+	constructor() {
+		super();
+		this.onScroll = this.onScroll.bind(this);
+		this.animateLayers = this.animateLayers.bind(this);
+		this.ticking = false;
+	}
+
+	componentDidMount() {
+		window.addEventListener('scroll', this.onScroll);
+	}
+
+	componentUnMount() {
+		window.removeEventListener('scroll', this.onScroll);
+	}
+
+	onScroll() {
+		if (!this.ticking) {
+			const scrolledHeight = window.pageYOffset.toFixed();
+
+			window.requestAnimationFrame(() => {
+				this.animateLayers(scrolledHeight);
+				this.ticking = false;
+			});
+		}
+		this.ticking = true;
+	}
+
+	animateLayers(scrolledHeight) {
+		if (!elementIsInView(this.frontLayer)) {return;} // don't animate when element is not in view
+
+		this.frontLayer.style.transform = `translate3d(0px, ${-scrolledHeight * 0.5}px, 0px)`;
+		this.backLayer.style.transform = `translate3d(0px, ${scrolledHeight * 0.3}px, 0px)`;
+	}
+
+	render() {
+		return (
+			<div>
+				<div ref={(node) => this.backLayer = node} className="shapes-back" >
+					{ this.props.backLayer }
+				</div>
+				<div ref={(node) => this.frontLayer = node} className="shapes-front">
+					{ this.props.frontLayer }
+				</div>
+			</div>
+		);
+	}
+}
+
+export default parallax;
--- components/services-intro/services-intro-shapes.js
+++ components/services-intro/services-intro-shapes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import elementIsInView from '../_helpers/elementIsInView';
+import Parallax from '../parallax/parallax';
 import CircleBorder from '../shapes/circle-border/circle-border';
 import DiamondBorder from '../shapes/diamond-border/diamond-border';
 import RectangleBorder from '../shapes/rectangle-border/rectangle-border';
@@ -9,55 +9,35 @@ import Triangle from '../shapes/triangle/triangle';
 class Shapes extends React.Component {
 	constructor() {
 		super();
-		this.onScroll = this.onScroll.bind(this);
-		this.addScrollEventListener = this.addScrollEventListener.bind(this);
-		this.animateLayers = this.animateLayers.bind(this);
-		this.ticking = false;
 	}
 
-	componentDidMount() {
-		this.addScrollEventListener();
+	backLayer() {
+		return (
+			<div>
+				<RectangleBorder classes="services-intro-rectangle-1" color="red"/>
+				<DiamondBorder classes="services-intro-diamond-1" color="yellow"/>
+				<CircleBorder classes="services-intro-circle-1" color="green" />
+			</div>
+		);
 	}
 
-	addScrollEventListener() {
-		window.addEventListener('scroll', () => {
-			if (!this.ticking) {
-				this.onScroll();
-			}
-			this.ticking = true;
-		});
-	}
-
-	onScroll() {
-		const scrolledHeight = window.pageYOffset.toFixed();
-
-		window.requestAnimationFrame(() => {
-			this.animateLayers(scrolledHeight);
-			this.ticking = false;
-		});
-	}
-
-	animateLayers(scrolledHeight) {
-		if (!elementIsInView(this.frontLayer)) {return;} // don't animate when element is not in view
-
-		this.frontLayer.style.transform = `translate3d(0px, ${-scrolledHeight * 0.5}px, 0px)`;
-		this.backLayer.style.transform = `translate3d(0px, ${scrolledHeight * 0.3}px, 0px)`;
+	frontLayer() {
+		return (
+			<div>
+				<Triangle classes="services-intro-triangle-1" color="red" />
+				<Triangle classes="services-intro-triangle-2" color="purple" />
+				<Triangle classes="services-intro-triangle-3" color="blue" shadow/>
+				<Triangle classes="services-intro-triangle-4" color="yellow" />
+			</div>
+		);
 	}
 
 	render() {
 		return (
 			<div className="services-intro-shapes">
-				<div ref={(node) => this.backLayer = node} className="shapes-back" >
-					<RectangleBorder classes="services-intro-rectangle-1" color="red"/>
-					<DiamondBorder classes="services-intro-diamond-1" color="yellow"/>
-					<CircleBorder classes="services-intro-circle-1" color="green" />
-				</div>
-				<div ref={(node) => this.frontLayer = node} className="shapes-front">
-					<Triangle classes="services-intro-triangle-1" color="red" />
-					<Triangle classes="services-intro-triangle-2" color="purple" />
-					<Triangle classes="services-intro-triangle-3" color="blue" shadow/>
-					<Triangle classes="services-intro-triangle-4" color="yellow" />
-				</div>
+				<Parallax
+					backLayer={this.backLayer()}
+					frontLayer={this.frontLayer()} />
 			</div>
 		);
 	}

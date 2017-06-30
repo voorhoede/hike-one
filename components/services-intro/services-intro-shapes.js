90db64854827a932e0eb6f6ed05338098c2cae8b
tjerksmit <tjerksmit@gmail.com>
2017-06-30 14:49:12 +0200

    feat(service): basic parallax effect

--- components/services-intro/services-intro-shapes.js
+++ components/services-intro/services-intro-shapes.js
@@ -6,15 +6,45 @@ import RectangleBorder from '../shapes/rectangle-border/rectangle-border';
 import Triangle from '../shapes/triangle/triangle';
 
 class Shapes extends React.Component {
+	constructor() {
+		super();
+		this.onScroll = this.onScroll.bind(this);
+		this.animateLayers = this.animateLayers.bind(this);
+	}
+
+	componentDidMount() {
+		this.animateLayers();
+	}
+
+	animateLayers() {
+		let ticking = false;
+		window.addEventListener('scroll', () => {
+			const scrolledHeight = window.pageYOffset.toFixed();
+
+			if (!ticking) {
+				window.requestAnimationFrame(() => {
+					this.onScroll(scrolledHeight);
+					ticking = false;
+				});
+			}
+			ticking = true;
+		});
+	}
+
+	onScroll(scrolledHeight) {
+		this.front.style.transform = `translate3d(0px, ${-scrolledHeight * 0.5}px, 0px)`;
+		this.back.style.transform = `translate3d(0px, ${scrolledHeight * 0.3}px, 0px)`;
+	}
+
 	render() {
 		return (
 			<div className="services-intro-shapes">
-				<div className="shapes-back">
+				<div ref={(node) => this.back = node} className="shapes-back" >
 					<RectangleBorder classes="services-intro-rectangle-1" color="red"/>
 					<DiamondBorder classes="services-intro-diamond-1" color="yellow"/>
 					<CircleBorder classes="services-intro-circle-1" color="green" />
 				</div>
-				<div className="shapes-front">
+				<div ref={(node) => this.front = node} className="shapes-front">
 					<Triangle classes="services-intro-triangle-1" color="red" />
 					<Triangle classes="services-intro-triangle-2" color="purple" />
 					<Triangle classes="services-intro-triangle-3" color="blue" shadow/>

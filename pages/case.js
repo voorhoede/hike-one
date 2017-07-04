568cdd50a703b260b2aa7c2b640ce76f25fa7171
Servin Nissen <servin@Servins-MacBook-Pro.local>
2017-07-04 11:50:35 +0200

    feat(customisable-to-all):the component and some style changes

--- pages/case.js
+++ pages/case.js
@@ -16,6 +16,12 @@ class Case extends React.Component {
                     <CaseText title="Truly global" text="We created a real smart system, that could predict the most used presets in languages at certain times of the day. This helped passengers in quickly selecting a language." />
                     <CaseText className="first-on-mobile" image="static/images/drop_fly1.jpg" />
                 </CaseTextContainer>
+
+                <CaseTextContainer>
+                    <CaseText image="static/images/visuele-stijl.jpg" />
+                    <CaseText title="Customisable  to all clients" text="The system adjusts to the specific style of the airline the customer checks in with, to reassure and confirm." />
+                </CaseTextContainer>
+
 				<h1>Case</h1>
 				<p>This is the Case page.</p>
 				<Link href="/"><a href="/">Homepage</a></Link>

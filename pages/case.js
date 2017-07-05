04398b27138ea50f5be9e36c3bd2f5485ed343fd
tjerksmit <tjerksmit@gmail.com>
2017-07-05 11:58:06 +0200

    feat(case) added main, header and footer

--- pages/case.js
+++ pages/case.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Link from 'next/link'
 
 import Layout from '../components/layout/layout';
 import Header from '../components/header/header';
+import Footer from '../components/footer/footer';
 import CaseTextCenter from '../components/case-text-center/case-text-center';
 
 import CaseTextContainer from '../components/case-text-container/case-text-container';
@@ -12,15 +12,15 @@ class Case extends React.Component {
 	render() {
 		return (
 			<Layout title="Hike One - Case">
-				<Header />
-                <CaseTextContainer>
+				<main className="main js-main">
+					<Header />
 					<CaseTextCenter />
-                    <CaseText title="Truly global" text="We created a real smart system, that could predict the most used presets in languages at certain times of the day. This helped passengers in quickly selecting a language." />
-                    <CaseText className="first-on-mobile" image="static/images/drop_fly1.jpg" />
-                </CaseTextContainer>
-				<h1>Case</h1>
-				<p>This is the Case page.</p>
-				<Link href="/"><a href="/">Homepage</a></Link>
+					<CaseTextContainer>
+						<CaseText title="Truly global" text="We created a real smart system, that could predict the most used presets in languages at certain times of the day. This helped passengers in quickly selecting a language." />
+						<CaseText className="first-on-mobile" image="static/images/drop_fly1.jpg" />
+					</CaseTextContainer>
+					<Footer />
+				</main>
 			</Layout>
 		);
 	}

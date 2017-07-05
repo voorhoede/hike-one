cf1bd3ad4f1a96ba7214d010e6b55dbdbf58f600
tjerksmit <tjerksmit@gmail.com>
2017-07-05 15:24:51 +0200

    fix(service page) corrected img for cta block

--- components/get-to-know-us/get-to-know-us.js
+++ components/get-to-know-us/get-to-know-us.js
@@ -6,7 +6,7 @@ class GetToKnowUs extends React.Component {
     render() {
         const props = this.props;
         return (
-            <section className="get-to-know-us js-bottom-element">
+            <section className="get-to-know-us js-bottom-element" style={{backgroundImage: `url(${props.image})`}}>
                 <div className="container">
                     <h2>{props.title}</h2>
                     <PrimaryButton href="#" value={props.button} />

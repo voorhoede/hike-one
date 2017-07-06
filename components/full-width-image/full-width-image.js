58007f9fcc5a62770f54a71660ae0625102210a0
tjerksmit <tjerksmit@gmail.com>
2017-07-06 16:12:34 +0200

    code(full width image): moved to separate lines

--- components/full-width-image/full-width-image.js
+++ components/full-width-image/full-width-image.js
@@ -5,10 +5,12 @@ class FullWidthImage extends React.Component {
         const props = this.props;
         return (
             <div className="full-width-image"  style={{backgroundImage: `url(${this.props.image})`}}>
-                {(props.title || props.subtitle) && <div className="full-width-image-text">
-                    {props.title && <h2>{props.title}</h2>}
-                    {props.subtitle && <p>{props.subtitle}</p>}
-                </div>}
+                {(props.title || props.subtitle) &&
+					<div className="full-width-image-text">
+                    	{ props.title && <h2>{props.title}</h2> }
+                    	{ props.subtitle && <p>{props.subtitle}</p> }
+                	</div>
+                }
             </div>
         );
     }

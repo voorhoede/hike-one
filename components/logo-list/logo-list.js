835254b54e8511ee81bd6a46fae8c9e746e3744d
tjerksmit <tjerksmit@gmail.com>
2017-07-06 14:42:11 +0200

    code(logo list) moved everything from 1 line

--- components/logo-list/logo-list.js
+++ components/logo-list/logo-list.js
@@ -4,9 +4,14 @@ import Link from 'next/link'
 class LogoList extends React.Component {
     render() {
         return (
-            <ul className="logo-list list-no-style container">
-                {this.props.logos.map((logo, i) => <li key={i}><Link href={logo.link}><a><img src={logo.image} alt="" /></a></Link></li>)}
-            </ul>
+			<ul className="logo-list list-no-style container">
+				{ this.props.logos.map((logo, index) =>
+					<li key={index}>
+						<Link href={logo.link}>
+							<a><img src={logo.image} alt="" /></a>
+						</Link>
+					</li> )}
+			</ul>
         );
     }
 }

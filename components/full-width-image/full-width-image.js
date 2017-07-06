314dcc732455fd41613483bfda1e11dc9dda79ff
Servin Nissen <servin@Servins-MacBook-Pro.local>
2017-07-06 14:46:24 +0200

    feat(full-width-image):component with option for text inside

--- components/full-width-image/full-width-image.js
+++ components/full-width-image/full-width-image.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 class FullWidthImage extends React.Component {
     render() {
+        const props = this.props;
         return (
-            <div className="full-width-image"  style={{backgroundImage: `url(${this.props.image})`}}></div>
+            <div className="full-width-image"  style={{backgroundImage: `url(${this.props.image})`}}>
+                {(props.title || props.subtitle) && <div className="full-width-image-text">
+                    {props.title && <h2>{props.title}</h2>}
+                    {props.subtitle && <p>{props.subtitle}</p>}
+                </div>}
+            </div>
         );
     }
 }

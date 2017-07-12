c8fb5adebc21e7a97e451dde8ce6f71e4d8c68ba
tjerksmit <tjerksmit@gmail.com>
2017-07-12 16:19:06 +0200

    feat(case intro) made case intro fixed

--- components/case-intro/case-intro.js
+++ components/case-intro/case-intro.js
@@ -3,7 +3,7 @@ import ButtonTertiary from '../buttons/button-tertiary/button-tertiary';
 const CaseIntro = ({image, title, subtitle}) => {
 	return (
 		<section className="case-intro" style={{backgroundImage: `url(${image})`}}>
-			<div className="container">
+			<div className="case-intro-inner container">
 				<h1 className="case-intro-title">{title}</h1>
 				<p className="case-intro-subtitle">{subtitle}</p>
 				<ButtonTertiary />

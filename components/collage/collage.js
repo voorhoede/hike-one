cab3bd6b7086f54205ee09a64573fad79d3a44a6
Jesse Eikema <jesse@voorhoede.nl>
2017-07-13 10:05:57 +0200

    feat(grid): added correct column widths to collage component and general container classes

--- components/collage/collage.js
+++ components/collage/collage.js
@@ -1,15 +1,13 @@
 const Collage = ({ imageMedium, imageSmall, title = '', text = '' }) => {
 	return (
 		<section className="collage clearfix container">
-			<div className="container-inner">
-				<div className="collage-image-container">
-					<img className="collage-image-medium" src={imageMedium} alt="" />
-					<img className="collage-image-small shadow" src={imageSmall} alt="" />
-				</div>
-				<div className="collage-text-container">
-					<h2 className="collage-text-title">{title}</h2>
-					<p className="collage-text-description">{text}</p>
-				</div>
+			<div className="collage-image-container">
+				<img className="collage-image-medium" src={imageMedium} alt="" />
+				<img className="collage-image-small shadow" src={imageSmall} alt="" />
+			</div>
+			<div className="collage-text-container">
+				<h2 className="collage-text-title">{title}</h2>
+				<p className="collage-text-description">{text}</p>
 			</div>
 		</section>
 	);

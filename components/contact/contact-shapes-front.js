52f78ac2e645e11799ca10e7a8eda5b527c805ce
tjerksmit <tjerksmit@gmail.com>
2017-07-13 10:59:13 +0200

    feat(parallax) seperated layers, added new layers and adjusted speeds

---
+++ components/contact/contact-shapes-front.js
@@ -0,0 +1,19 @@
+import Parallax from '../parallax/parallax';
+import Triangle from '../shapes/triangle/triangle';
+
+const ShapesFront = () => {
+	return (
+		<div className="contact-shapes">
+			<Parallax speed="-0.25" distance="400">
+				<Triangle classes="shape-triangle-2" color="red" />
+				<Triangle classes="shape-triangle-4" color="yellow" />
+			</Parallax>
+			<Parallax speed="-0.5"  distance="400">
+				<Triangle classes="shape-triangle-1" color="blue" />
+				<Triangle classes="shape-triangle-3" color="purple" />
+			</Parallax>
+		</div>
+	);
+};
+
+export default ShapesFront;
---
+++ components/services-overview/services-overview-shapes-front.js
@@ -0,0 +1,14 @@
+import Parallax from '../parallax/parallax';
+import Triangle from '../shapes/triangle/triangle';
+
+const ShapesFront = () => {
+	return (
+		<div className="services-overview-shapes parallax-layer">
+			<Parallax speed="-0.5">
+				<Triangle classes="shape-triangle-1" color="red" />
+			</Parallax>
+		</div>
+	);
+};
+
+export default ShapesFront;

cfcd3f09d2b6aeef81b9a0e9819615111934ad83
tjerksmit <tjerksmit@gmail.com>
2017-07-13 11:35:37 +0200

    fix(parallax) tweaked contact

--- components/contact/contact-shapes-front.js
+++ components/contact/contact-shapes-front.js
@@ -4,11 +4,11 @@ import Triangle from '../shapes/triangle/triangle';
 const ShapesFront = () => {
 	return (
 		<div className="contact-shapes">
-			<Parallax speed="-0.25" distance="400">
+			<Parallax speed="-0.25" distance="400" offset="100">
 				<Triangle classes="shape-triangle-2" color="red" />
 				<Triangle classes="shape-triangle-4" color="yellow" />
 			</Parallax>
-			<Parallax speed="-0.5"  distance="400">
+			<Parallax speed="-0.5"  distance="400" offset="100">
 				<Triangle classes="shape-triangle-1" color="blue" />
 				<Triangle classes="shape-triangle-3" color="purple" />
 			</Parallax>

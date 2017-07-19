8f1e122651112869091270f499f23f4dfe5009a3
tjerksmit <tjerksmit@gmail.com>
2017-07-19 11:10:54 +0200

    code(full width image) moved code to own functions and added comments

--- components/full-width-image/full-width-image.js
+++ components/full-width-image/full-width-image.js
@@ -1,36 +1,117 @@
 import React from 'react';
-import Parallax from '../parallax/parallax';
+import elementIsInView from '../_helpers/isElementInView';
 
 class FullWidthImage extends React.Component {
 	constructor() {
 		super();
-		this.speed = 1.5;
+		this.setImageBottomOffset = this.setImageBottomOffset.bind(this);
+		this.setInitialOffset = this.setInitialOffset.bind(this);
+		this.setLayerOffsets = this.setLayerOffsets.bind(this);
+		this.resetElement = this.resetElement.bind(this);
+		this.onScroll = this.onScroll.bind(this);
+		this.animateLayers = this.animateLayers.bind(this);
+		this.speed = 0.5;
 		this.state = {
-			offset: 0
+			imageBottomOffset: 0
 		};
 	}
 
 	componentDidMount() {
+		this.initialScrollHeight =  document.body.scrollTop || document.documentElement.scrollTop || 0;
+		this.elBoundingRect = this.element.getBoundingClientRect();
+		this.setImageBottomOffset();
+
+		if (elementIsInView(this.element)) {
+			this.setInitialOffset();
+		}
+
+		window.addEventListener('scroll', this.onScroll);
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener('scroll', this.onScroll);
+	}
+
+	onScroll() {
+		// update an animation before the next repaint with requestAnimationFrame
+		if (!this.ticking) {
+			window.requestAnimationFrame(() => {
+				this.animateLayers();
+				this.ticking = false;
+			});
+		}
+		this.ticking = true;
+	}
+
+	animateLayers() {
+		if (!elementIsInView(this.element) ) {
+			if (!this.elementReset) {
+				this.resetElement();
+			}
+
+			this.elementReset = true;
+			return;
+		}
+
+		this.elementReset = false;
+		const scrolledHeight = document.body.scrollTop || document.documentElement.scrollTop || 0;
+		const relativeScroll = this.initialScrollHeight - scrolledHeight;
+		const yOffsetFixed = this.elBoundingRect.top + relativeScroll;
+		const yOffsetImage = -(this.elBoundingRect.top + relativeScroll) * this.speed;
+
+		this.setLayerOffsets(yOffsetFixed, yOffsetImage);
+	}
+
+	setInitialOffset() {
+		const yOffsetFixed = this.elBoundingRect.top;
+		const yOffsetImage = -this.elBoundingRect.top * this.speed;
+
+		this.setLayerOffsets(yOffsetFixed, yOffsetImage);
+	}
+
+	setLayerOffsets(yOffsetFixed, yOffsetImage) {
+		this.fixedElement.style.transform = `translate3d(0px, ${yOffsetFixed}px, 0px)`;
+		this.imageElement.style.transform = `translate3d(0px, ${yOffsetImage}px, 0px)`;
+	}
+
+	setImageBottomOffset() {
 		const windowHeight = document.body.clientHeight || document.documentElement.clientHeight || 0;
-		const elementRect = this.parallaxLayer.elementRect;
-		const elementHalf = elementRect.height / 2;
-		const windowHalf = windowHeight / 2;
-		const offset = (elementHalf + windowHalf) * (1 - this.speed);
-		this.setState({offset});
+
+		// initial y offset image when not in view
+		const initialYOffsetImage = windowHeight * this.speed;
+
+		// total distance that the image moves
+		const totalTravelDistance = this.elBoundingRect.height * this.speed;
+
+		// if the total travel distance of the image is smaller then its initial y offset
+		// then the image needs to be made bigger so that its heigh enough that it fills the whole container element
+		// this is done by offsetting the bottom css attribute
+		if (totalTravelDistance < initialYOffsetImage) {
+			const imageBottomOffset = totalTravelDistance - initialYOffsetImage;
+			this.setState({imageBottomOffset});
+		}
+	}
+
+	resetElement() {
+		this.fixedElement.style.transform = `translate3d(0px, -110%, 0px)`;
 	}
 
 	render() {
         const props = this.props;
         return (
-            <div className="full-width-image" >
-				<Parallax speed={this.speed} duration="0" ref={node => this.parallaxLayer = node}>
+            <div className="full-width-image" ref={node => this.element = node}>
+				<div className="full-width-image-inner"
+					 ref={node => this.fixedElement = node}
+					 style={{transform: `translate3d(0px, -110%, 0px)`}}>
 					<div className="full-width-image-background"
-						style={{
-						top: this.state.offset,
-						bottom: this.state.offset,
-						backgroundImage: `url(${this.props.image})`}} >
+						 ref={node => this.imageElement = node}
+						 style={{
+						 	backgroundImage: `url(${this.props.image})`,
+							bottom: this.state.imageBottomOffset
+						 }} >
 					</div>
-				</Parallax>
+				</div>
+
                 {(props.title || props.subtitle) &&
 					<div className="full-width-image-text">
                     	{ props.title && <h2>{props.title}</h2> }

18857069a5fc7c75e9c9b3233a8a813d1c74de40
Jesse Eikema <jesse@voorhoede.nl>
2017-07-19 11:00:48 +0200

    refactor(services): removed old code

--- components/services-overview/services-overview.js
+++ components/services-overview/services-overview.js
@@ -7,16 +7,14 @@ import ShapesBack from './services-overview-shapes-back';
 class ServicesOverview extends React.Component {
     render() {
 		const props = this.props;
-
         return (
             <section className="services-overview">
 				<ShapesBack/>
 				<div className="container-inner">
 					<h2 className="section-header content">{props.title}</h2>
 					<ul className="no-style">
-						{ props.items.map((service, index) => <ServicesItem key={index} index={index} data={service} />) }
+						{ props.items.map((service, index) => <ServicesItem key={index} index={index} data={service}/>) }
 					</ul>
-
 				</div>
 				<ShapesFront />
             </section>

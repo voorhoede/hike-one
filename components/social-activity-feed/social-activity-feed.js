d583a991030395830dbf743dece2e26bc154df8b
Jesse Eikema <jesse@voorhoede.nl>
2017-07-21 15:32:28 +0200

    feat(events): add events and general styling fix

--- components/social-activity-feed/social-activity-feed.js
+++ components/social-activity-feed/social-activity-feed.js
@@ -1,12 +1,13 @@
 import BlogFeed from '../blog-feed/blog-feed';
 import SocialFeedSmall from '../social-feed-small/social-feed-small';
 import SocialFeedLarge from '../social-feed-large/social-feed-large';
+import SocialFeedEvents from '../social-events/social-events';
 
 import Data from '../../dummy-data/home/home.json';
 
 const socialActivityFeed = () => {
 	return (
-		<div className="container">
+		<div className="social-acitivity-feed container">
 			<div className="container-inner">
 				<BlogFeed 	title={Data.blogTitle}
 							subtitle={Data.blogSubtitle}
@@ -20,7 +21,11 @@ const socialActivityFeed = () => {
 				<SocialFeedSmall 
 					socialImagesFirst={Data.socialImagesFirst} 
 					socialImagesSecond={Data.socialImagesSecond} />
-			
+				<SocialFeedEvents 
+					subtitle={Data.eventsSubtitle}
+					events={Data.events}
+					link={Data.eventsLink} 
+				/>
 			</div>
 		</div>
 	)

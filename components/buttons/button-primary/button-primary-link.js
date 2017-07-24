d4948413f22f73f6729094fc854231bb3ec14222
tjerksmit <tjerksmit@gmail.com>
2017-07-24 16:14:27 +0200

    code(button primary) converted to const

--- components/buttons/button-primary/button-primary-link.js
+++ components/buttons/button-primary/button-primary-link.js
@@ -1,18 +1,15 @@
-import React from 'react';
-import Link from 'next/link'
+import Link from 'next/link';
 
-class PrimaryButtonLink extends React.Component {
-	render() {
-		return (
-			<div>
-				<Link href={this.props.href}>
-					<a className={`btn-primary ${this.props.classes ? this.props.classes: ''}`}>
-						{this.props.value}
-					</a>
-				</Link>
-			</div>
-		);
-	}
-}
+const PrimaryButtonLink = ({classes = '', href, value = ''}) => {
+	return (
+		<div>
+			<Link href={href}>
+				<a className={`btn-primary ${classes}`}>
+					{value}
+				</a>
+			</Link>
+		</div>
+	);
+};
 
 export default PrimaryButtonLink;

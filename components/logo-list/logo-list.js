ab1fd72a9bb18052533f25edd3241a59aea3948e
tjerksmit <tjerksmit@gmail.com>
2017-07-24 16:15:47 +0200

    code(logo list) converted to const

--- components/logo-list/logo-list.js
+++ components/logo-list/logo-list.js
@@ -1,19 +1,16 @@
-import React from 'react';
 import Link from 'next/link'
 
-class LogoList extends React.Component {
-    render() {
-        return (
-			<ul className="logo-list list-no-style container">
-				{ this.props.logos.map((logo, index) =>
-					<li key={index}>
-						<Link href={logo.link}>
-							<a><img src={logo.image} alt="" /></a>
-						</Link>
-					</li> )}
-			</ul>
-        );
-    }
-}
+const LogoList = ({logos}) => {
+	return (
+		<ul className="logo-list list-no-style container">
+			{ logos.map((logo, index) =>
+				<li key={index}>
+					<Link href={logo.link}>
+						<a><img src={logo.image} alt="" /></a>
+					</Link>
+				</li> )}
+		</ul>
+	);
+};
 
 export default LogoList;

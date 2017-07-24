4c44d35c075428195365daa8deb3730e096caeba
tjerksmit <tjerksmit@gmail.com>
2017-07-24 16:16:27 +0200

    code(quote block) converted to const

--- components/quote-block/quote-block.js
+++ components/quote-block/quote-block.js
@@ -1,23 +1,20 @@
 import React from 'react';
 
-class QuoteBlock extends React.Component {
-    render() {
-        const props = this.props;
-        return (
-            <blockquote className={`quote-block ${props.color ? props.color: ''}`}>
-				<span className="content">
-					“{props.quote}”
-				</span>
-                <cite className="quote-cite">
-                    <img src={props.citeImage} alt="" />
-                    <div className="quote-cite-content content">
-                        <p className="quote-cite-name">{props.citeName}</p>
-                        <p className="quote-cite-title">{props.citeTitle}</p>
-                    </div>
-                </cite>
-            </blockquote>
-        );
-    }
-}
+const QuoteBlock = ({color = '', quote = '', citeImage, citeName = '', citeTitle = ''}) => {
+	return (
+		<blockquote className={`quote-block ${color}`}>
+			<span className="content">
+				“{quote}”
+			</span>
+			<cite className="quote-cite">
+				<img src={citeImage} alt="" />
+				<div className="quote-cite-content content">
+					<p className="quote-cite-name">{citeName}</p>
+					<p className="quote-cite-title">{citeTitle}</p>
+				</div>
+			</cite>
+		</blockquote>
+	);
+};
 
 export default QuoteBlock;

ff4d8ca89d5b71e0cc5d2e2426753e7bce728cc4
Jesse Eikema <jesse@voorhoede.nl>
2017-07-25 12:04:34 +0200

    fix(blog-extact): add icon instead of button add link to component

--- components/blog-extract/blog-extract.js
+++ components/blog-extract/blog-extract.js
@@ -1,20 +1,19 @@
-import ButtonTertiary from '../buttons/button-tertiary/button-tertiary';
+import ArrowRightRound from '../icons/arrow-right-round/arrow-right-round';
 
-const BlogExtract = ({ blogImage, title, subtitle, date }) => {
-	return (
-		<a href="#blog" className="blog-extract">
-			<div className="blog-extract-image-container" style={{backgroundImage: `url(${blogImage})`}}></div>
-			<div className="blog-extract-text-container">
-				<div className="blog-extract-triangle"></div>	
-				<h3>{subtitle}</h3>
-				<h2>{title}</h2>
-				<div className="blog-extract-footer-container">
-					<span>{date}</span>
-					<ButtonTertiary href="#blog" classes="btn-white" iconType="arrowRight" />
-				</div>
+const BlogExtract = ({ blogImage, title, subtitle, date }) => (
+	<a href="#blog" className="blog-extract">
+		<div className="blog-extract-image-container" style={{backgroundImage: `url(${blogImage})`}}></div>
+		<div className="blog-extract-text-container">
+			<div className="blog-extract-triangle"></div>	
+			<h3>{subtitle}</h3>
+			<h2>{title}</h2>
+			<div className="blog-extract-footer-container">
+				<span>{date}</span>
+				<ArrowRightRound />
 			</div>
-		</a>
-	) 
-}
+		</div>
+	</a>
+) 
+
 
 export default BlogExtract;

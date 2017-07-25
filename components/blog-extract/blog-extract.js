6f5425925e23e3c3a8562b9469b937971c8104d0
Jesse Eikema <jesse@voorhoede.nl>
2017-07-25 15:12:39 +0200

    fix(blog): added actual img ta

--- components/blog-extract/blog-extract.js
+++ components/blog-extract/blog-extract.js
@@ -2,7 +2,10 @@ import ArrowRightRound from '../icons/arrow-right-round/arrow-right-round';
 
 const BlogExtract = ({ blogImage, title, subtitle, date, link }) => (
 	<a href={link} className="blog-extract">
-		<div className="blog-extract-image-container" style={{backgroundImage: `url(${blogImage})`}}></div>
+		<div className="blog-extract-image-container">
+			<img src={blogImage} alt=""/>
+		</div>
+
 		<div className="blog-extract-text-container">
 			<div className="blog-extract-triangle"></div>	
 			<h3>{subtitle}</h3>

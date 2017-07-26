809bd46635ef1deb43f886f5881949b64ac01c45
tjerksmit <tjerksmit@gmail.com>
2017-07-26 10:57:43 +0200

    code: simplified return components

--- components/services-intro/services-intro.js
+++ components/services-intro/services-intro.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ShapesFront from './services-intro-shapes-front';
 
-const ServiceApproach = ({text = ''}) => {
-	return (
-		<section className="services-approach js-approach">
-			<div className="services-approach-inner">
-				<p id="services-approach" className="content">
-					{text}
-				</p>
-			</div>
-			<ShapesFront />
-		</section>
-	);
-};
+const ServiceApproach = ({text = ''}) => (
+	<section className="services-approach js-approach">
+		<div className="services-approach-inner">
+			<p id="services-approach" className="content">
+				{text}
+			</p>
+		</div>
+		<ShapesFront />
+	</section>
+);
+
 
 export default ServiceApproach;

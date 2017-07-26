fac7b8b2c78657bada1e25dc5b529de4a54cf34f
tjerksmit <tjerksmit@gmail.com>
2017-07-26 09:12:22 +0200

    feat(twitter extract) removed link for external links

--- components/twitter-extract/twitter-extract.js
+++ components/twitter-extract/twitter-extract.js
@@ -1,38 +1,27 @@
 import ArrowRight from '../icons/arrow-right/arrow-right';
-import Link from 'next/link';
 
-const TwitterExtract = ({ twitterImage, subtitle, title, date, linkTwitterAccount, linkTwitterPost }) => {
-	return (
-		<div className="twitter-extract"> 
-			<div className={`twitter-extract-image ${twitterImage ? '' : 'is-hidden' }`}>
-				<Link href={linkTwitterPost}>
-					<a>
-						<img src={twitterImage} alt=""/>	
-					</a>	
-				</Link>
-			</div>
-			<div className={`twitter-extract-text ${twitterImage ? '' : 'twitter-extract-full-width' }`}>
-				<div className={`twitter-extract-triangle ${twitterImage ? '' : 'is-hidden' }`}></div>
-				<Link href={linkTwitterAccount}>
-					<a>
-						<h4>{subtitle}</h4>
-					</a>
-				</Link>
-				
-				<Link href={linkTwitterPost}>
-					<a>
-						<h3>{title}</h3>
-					</a>
-				</Link>
-				<span>{date}</span>
-				<Link href={linkTwitterAccount}>
-					<a>
-						<ArrowRight />
-					</a>
-				</Link>
-			</div>
+const TwitterExtract = ({ twitterImage, subtitle, title, date, linkTwitterAccount, linkTwitterPost }) => (
+	<div className="twitter-extract">
+		<div className={`twitter-extract-image ${twitterImage ? '' : 'is-hidden' }`}>
+			<a href={linkTwitterPost}>
+				<img src={twitterImage} alt=""/>
+			</a>
 		</div>
-	)
-}
+		<div className={`twitter-extract-text ${twitterImage ? '' : 'twitter-extract-full-width' }`}>
+			<div className={`twitter-extract-triangle ${twitterImage ? '' : 'is-hidden' }`}></div>
+			<a href={linkTwitterAccount}>
+				<h4>{subtitle}</h4>
+			</a>
+			<a href={linkTwitterPost}>
+				<h3>{title}</h3>
+			</a>
+			<span>{date}</span>
+			<a href={linkTwitterAccount}>
+				<ArrowRight />
+			</a>
+		</div>
+	</div>
+);
+
 
 export default TwitterExtract;

50f99519e62d18863836f30658b4b7b599bbfc2b
Jesse Eikema <jesse@voorhoede.nl>
2017-07-26 14:42:08 +0200

    feat(footer): added correct font-szies

--- components/page-header/page-header.js
+++ components/page-header/page-header.js
@@ -4,7 +4,7 @@ const HomeIntro = ({heroImage, title = '', subtitle = '', parallaxLayerFront, pa
 		<div className="home-intro container">
 			{parallaxLayerBack}
 			<div className="home-intro-overlay">
-				<div className="container-inner home-intro-inner" style={{backgroundImage: `url(${heroImage})`}}>
+				<div className="container-inner home-intro-inner" style={{backgroundImage: `url(${heroImage}?format=auto&width=200)`}}>
 					<h1 className="home-intro-heading">{title}</h1>
 					<h2 className="home-intro-subheading">{subtitle}</h2>
 					<button className="btn-clean home-intro-icon-container">

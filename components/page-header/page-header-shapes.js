3d7b80d04b00b600a810134754a1d774f7cf9c20
tjerksmit <tjerksmit@gmail.com>
2017-07-26 11:01:16 +0200

    code: simplified return components

--- components/page-header/page-header-shapes.js
+++ components/page-header/page-header-shapes.js
@@ -2,41 +2,37 @@ import Parallax from '../parallax/parallax';
 import DiamondBorder from '../shapes/diamond-border/diamond-border';
 import Triangle from '../shapes/triangle/triangle';
 
-export const FrontLayer1 = () => {
-	return (
-		<div className="home-intro-shapes">
-			<Parallax speed="1.25">
-				<Triangle classes="shape-triangle-1" color="green" />
-			</Parallax>
-			<Parallax speed="1.5">
-				<Triangle classes="shape-triangle-2" color="yellow" />
-				<Triangle classes="shape-triangle-3" color="purple" />
-			</Parallax>
-		</div>
-	);
-};
-
-export const FrontLayer2 = () => {
-	return (
-		<div className="home-intro-shapes">
-			<Parallax speed="1.25">
-				<Triangle classes="shape-triangle-1" color="blue" />
-			</Parallax>
-			<Parallax speed="1.5">
-				<Triangle classes="shape-triangle-2" color="yellow" />
-				<Triangle classes="shape-triangle-3" color="purple" />
-			</Parallax>
-		</div>
-	);
-};
+export const FrontLayer1 = () => (
+	<div className="home-intro-shapes">
+		<Parallax speed="1.25">
+			<Triangle classes="shape-triangle-1" color="green" />
+		</Parallax>
+		<Parallax speed="1.5">
+			<Triangle classes="shape-triangle-2" color="yellow" />
+			<Triangle classes="shape-triangle-3" color="purple" />
+		</Parallax>
+	</div>
+);
 
 
-export const BackLayer1 = () => {
-	return (
-		<div className="home-intro-shapes">
-			<Parallax speed="0.875">
-				<DiamondBorder classes="shape-diamond-1" color="purple"/>
-			</Parallax>
-		</div>
-	);
-};
+export const FrontLayer2 = () => (
+	<div className="home-intro-shapes">
+		<Parallax speed="1.25">
+			<Triangle classes="shape-triangle-1" color="blue" />
+		</Parallax>
+		<Parallax speed="1.5">
+			<Triangle classes="shape-triangle-2" color="yellow" />
+			<Triangle classes="shape-triangle-3" color="purple" />
+		</Parallax>
+	</div>
+);
+
+
+
+export const BackLayer1 = () => (
+	<div className="home-intro-shapes">
+		<Parallax speed="0.875">
+			<DiamondBorder classes="shape-diamond-1" color="purple"/>
+		</Parallax>
+	</div>
+);

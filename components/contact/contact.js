44599a2941e37eb8bf5967a9fd16397dff2616c4
tjerksmit <tjerksmit@gmail.com>
2017-07-31 16:44:59 +0200

    fix(contact) margin

--- components/contact/contact.js
+++ components/contact/contact.js
@@ -1,13 +1,13 @@
 import SecondaryButtonLink from '../buttons/button-secondary/button-secondary-link';
 
 const Contact = ({title = '', button = '', parallaxLayerFront}) => (
-	<div className="contact">
+	<section className="contact">
 		<div className="container-inner">
 			<h3 className="content">{title}</h3>
 			<SecondaryButtonLink noArrow href="#" classes="btn-secondary-alt content" value={button} />
 		</div>
 		{ parallaxLayerFront }
-	</div>
+	</section>
 );
 
 

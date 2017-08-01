cd6b11cc400c6b9f47289bad00b720cdee26111b
tjerksmit <tjerksmit@gmail.com>
2017-08-01 16:50:38 +0200

    fix(case extract) added type

--- components/case-extract/case-extract.js
+++ components/case-extract/case-extract.js
@@ -2,7 +2,7 @@ import ArrowRight from '../icons/arrow-right/arrow-right';
 import ButtonTertiary from '../buttons/button-tertiary/button-tertiary';
 import Link from 'next/link';
 
-const CaseExtract = ({ headerImage ,title = '', subtitle = '', type = '', link}) => (
+const CaseExtract = ({ headerImage ,title = '', subtitle = '', link}) => (
 
 	<section className="case-extract container">
 		<Link href={link}>
@@ -10,7 +10,7 @@ const CaseExtract = ({ headerImage ,title = '', subtitle = '', type = '', link})
 				<div className="case-extract-inner clearfix">
 					<div className="case-extract-image-container" style={{backgroundImage: `url(${headerImage})`}}></div>
 					<div className="case-extract-text-container shadow">
-						<span>{type}</span>
+						<span>Case</span>
 						<h3>{title}</h3>
 						<h4>{subtitle}</h4>
 						<div className="case-extract-button-container">

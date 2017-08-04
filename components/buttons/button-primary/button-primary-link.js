a7023ad2c5cb348bc489bbc027f68cb92699c650
tjerksmit <tjerksmit@gmail.com>
2017-08-04 16:09:17 +0200

    code(button) removed unused div

--- components/buttons/button-primary/button-primary-link.js
+++ components/buttons/button-primary/button-primary-link.js
@@ -2,18 +2,16 @@ import Link from 'next/link';
 import Icon from '../../icon/icon';
 
 const PrimaryButtonLink = ({classes = '', href, children = '', icon}) => (
-	<div>
-		<Link href={href}>
-			<a className={`btn-primary ${classes}`}>
-				{children}
-				{ icon &&
-				<span className="icon">
-					<Icon icon={icon}/>
-				</span>
-				}
-			</a>
-		</Link>
-	</div>
+	<Link href={href}>
+		<a className={`btn-primary ${classes}`}>
+			{children}
+			{ icon &&
+			<span className="icon">
+				<Icon icon={icon}/>
+			</span>
+			}
+		</a>
+	</Link>
 );
 
 

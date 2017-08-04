9234c0faa893a8531ed70a84279f9a077bdfdaeb
tjerksmit <tjerksmit@gmail.com>
2017-08-04 12:17:14 +0200

    refacor(buttons) primary button

--- components/buttons/button-primary/button-primary-link.js
+++ components/buttons/button-primary/button-primary-link.js
@@ -1,10 +1,16 @@
 import Link from 'next/link';
+import Icon from '../../icon/icon';
 
-const PrimaryButtonLink = ({classes = '', href, value = ''}) => (
+const PrimaryButtonLink = ({classes = '', href, children = '', icon}) => (
 	<div>
 		<Link href={href}>
 			<a className={`btn-primary ${classes}`}>
-				{value}
+				{children}
+				{ icon &&
+				<span className="icon">
+					<Icon icon={icon}/>
+				</span>
+				}
 			</a>
 		</Link>
 	</div>

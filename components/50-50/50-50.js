eae4fd7903a4ab3984c38fe32573433bb87f4955
tjerksmit <tjerksmit@gmail.com>
2017-08-11 14:44:16 +0200

    feat(5050) added responsive img

--- components/50-50/50-50.js
+++ components/50-50/50-50.js
@@ -10,7 +10,23 @@ const FiftyFifty = ({classes = '', image, title = '', text = '', noshadow, child
 			{parallaxLayerBack}
 			<div className="container-inner">
 				<div className="fifty-fifty-image">
-					<img className={`content ${noshadow ? 'no-shadow' : 'shadow-low-opacity'}` } src={image} alt="" />
+					<img className={`content ${noshadow ? 'no-shadow' : 'shadow-low-opacity'}` }
+						 srcSet={`
+						 	${image}&auto=format&fit=max&q90&w=250 250w,
+						 	${image}&auto=format&fit=max&q90&w=500 500w,
+						 	${image}&auto=format&fit=max&q90&w=630 630w,
+							${image}&auto=format&fit=max&q90&w=750 750w,
+							${image}&auto=format&fit=max&q90&w=1000 1000w,
+							${image}&auto=format&fit=max&q90&w=1260 1260w,
+							${image}&auto=format&fit=max&q90&w=1400 1400w
+						 `}
+						 sizes="
+							(max-width: 768px) calc(100vw - 30px),
+							(max-width: 1024px) calc(50vw - 50px),
+							(max-width: 1244px) calc(50vw - 30px),
+							630px"
+						 src={`${image}&auto=format&q=90&w=750`}
+						 alt="" />
 				</div>
 				<div className="fifty-fifty-content">
 					<h2 className="fifty-fifty-title content">{title}</h2>

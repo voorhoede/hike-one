eafc1e545a3391cf4169a89e7cc2e0b2e787fd3f
Jesse Eikema <jesse@voorhoede.nl>
2017-08-15 13:49:40 +0200

    style(team): renamed class

--- components/team-image-1-2/team-image-1-2.js
+++ components/team-image-1-2/team-image-1-2.js
@@ -1,5 +1,5 @@
 const TeamImage1_2 = ({image=''}) => (
-	<div className="image-composition-float-left image-composition-padded">
+	<div className="image-composition-align-left image-composition-padded">
 		<img src={image.photo.url} alt="" className="image-composition-image" />
 		<span className="image-composition-image-title">
 			{image.title}

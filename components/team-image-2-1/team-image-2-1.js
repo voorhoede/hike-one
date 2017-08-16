96c7222dee26b1ad9e42f9b87ea04022c7ef9cb6
Jesse Eikema <jesse@voorhoede.nl>
2017-08-16 14:40:59 +0200

    refactor(image-composition): add components

--- components/team-image-2-1/team-image-2-1.js
+++ components/team-image-2-1/team-image-2-1.js
@@ -1,12 +1,12 @@
 const TeamImage2_1 = ({image}) => (
-	<div className="image-composition-align-left image-composition-padded">
+	<div className="teamimage-large-align-left teamimage-large-padded">
 		<img srcSet={`
 			${image.photo.url}&auto=format&q=90&w=165 375w,
 			${image.photo.url}&auto=format&q=90&w=452 480w
 		`} sizes={`
 			(max-width: 375px) 165px, 452px
-		`} src={`${image.photo.url}&auto=format&q=90&w=165`}  alt="" className="image-composition-image" />
-		<span className="image-composition-image-title">
+		`} src={`${image.photo.url}&auto=format&q=90&w=165`}  alt="" className="teamimage-large-image" />
+		<span className="teamimage-large-title">
 			{image.title}
 		</span>
 	</div>
--- components/team-image-3-4/team-image-3-4.js
+++ components/team-image-3-4/team-image-3-4.js
@@ -1,12 +1,12 @@
 const TeamImage3_4 = ({image}) => (
-	<div className="image-composition-align-left">
+	<div className="teamimage-normal-align-left ">
 		<img srcSet={`
 			${image.photo.url}&auto=format&q=90&w=165 375w,
 			${image.photo.url}&auto=format&q=90&w=452 480w
 		`} sizes={`
 			(max-width: 375px) 165px, 452px
-		`} src={`${image.photo.url}&auto=format&q=90&w=165`} alt="" className="image-composition-image" />
-		<span className="image-composition-image-title">
+		`} src={`${image.photo.url}&auto=format&q=90&w=165`} alt="" className="teamimage-normal-image" />
+		<span className="teamimage-normal-title">
 			{image.title}
 		</span>
 	</div>

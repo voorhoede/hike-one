46ebd2b40ce9963085c02585852894db47d1f39d
Jesse Eikema <jesse@voorhoede.nl>
2017-08-16 14:17:08 +0200

    style(image-composition): add responsive images

--- components/team-image-2-1/team-image-2-1.js
+++ components/team-image-2-1/team-image-2-1.js
@@ -1,6 +1,11 @@
 const TeamImage2_1 = ({image}) => (
 	<div className="image-composition-align-left image-composition-padded">
-		<img src={image.photo.url} alt="" className="image-composition-image" />
+		<img srcSet={`
+			${image.photo.url}&auto=format&q=90&w=165 375w,
+			${image.photo.url}&auto=format&q=90&w=452 480w
+		`} sizes={`
+			(max-width: 375px) 165px, 452px
+		`} src={`${image.photo.url}&auto=format&q=90&w=165`}  alt="" className="image-composition-image" />
 		<span className="image-composition-image-title">
 			{image.title}
 		</span>

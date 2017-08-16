e5d176f07c0598eb130ec8c566fa9e5785531f94
Jesse Eikema <jesse@voorhoede.nl>
2017-08-16 11:25:06 +0200

    style(parralax): added tweaks to parallax elements

--- components/image-composition/image-composition.js
+++ components/image-composition/image-composition.js
@@ -1,11 +1,29 @@
 
+import React from 'react';
 
-const ImageComposition = ({TeamImage2_1}, {TeamImage3_4}, {Person}) => (
-	<div className="image-composition">
-		<TeamImage2_1 image={TeamImage2_1}  />; 
-		<TeamImage3_4 image={TeamImage3_4}  />;
-		<Person image={Person}  />;	
-	</div>
-);
+import TeamImage2_1 from '../team-image-2-1/team-image-2-1';
+import TeamImage2_1Data from '../../data/current/teamImages21.json';
+
+import TeamImage3_4 from '../team-image-3-4/team-image-3-4';
+import TeamImage3_4Data from '../../data/current/teamImages34.json';
+
+import Person from '../person/person';
+import PeopleData from '../../data/current/people.json';
+
+const ImageComposition = ({children}) => {
+	const childrenArray = React.Children.toArray(children);
+	const parallaxLayerFront = childrenArray.find(child => child.props.position === 'front');
+	const parallaxLayerBack = childrenArray.find(child => child.props.position === 'back');
+	
+	return (
+		<div className="image-composition">
+			{parallaxLayerBack}
+			<TeamImage2_1 image={TeamImage2_1Data}  />
+			<TeamImage3_4 image={TeamImage3_4Data}  />
+			<Person image={PeopleData}  />	
+			{parallaxLayerFront}
+		</div>
+	)
+};
 
 export default ImageComposition;
--- pages/team.js
+++ pages/team.js
@@ -7,24 +7,14 @@ import PageHeader from '../components/page-header/page-header';
 import * as PageHeaderShapes from '../components/page-header/page-header-shapes';
 import TextCenter from '../components/text-center/text-center';
 import * as TextCenterShapes from '../components/text-center/text-center-shapes';
-import ImageComposition from '../components/image-composition/image-composition';
 import Contact from '../components/contact/contact';
 import * as ContactShapes from '../components/contact/contact-shapes';
-
+import ImageComposition from '../components/image-composition/image-composition';
 import * as ImageCompositionShapes from '../components/image-composition/image-composition-shapes';
 
 import Data from '../data/current/team.json';
 
 import scrollToElement from '../components/_helpers/scrollToElement';
-
-import TeamImage2_1 from '../components/team-image-2-1/team-image-2-1';
-import TeamImage2_1Data from '../data/current/teamImages21.json';
-
-import TeamImage3_4 from '../components/team-image-3-4/team-image-3-4';
-import TeamImage3_4Data from '../data/current/teamImages34.json';
-
-import Person from '../components/person/person';
-import PeopleData from '../data/current/people.json';
 
 const Team = () => {
 	let scrollToTargetClass = 'js-scroll-to-target';
@@ -42,11 +32,10 @@ const Team = () => {
 						<PageHeaderShapes.variation1Back position="back"/>
 					</PageHeader>
 					
-					<div className="image-composition">
-						<TeamImage2_1 image={TeamImage2_1Data}  />
-						<TeamImage3_4 image={TeamImage3_4Data}  />
-						<Person image={PeopleData}  />	
-					</div>
+					<ImageComposition> 
+						<ImageCompositionShapes.variation1Front position="front"/>
+						<ImageCompositionShapes.variation1Back position="back"/>
+					</ImageComposition>
 				</article>
 				<Footer />
 			</main>

165ccec3fc12a9eb65413890fdfc866791111021
Jesse Eikema <jesse@voorhoede.nl>
2017-08-17 13:44:30 +0200

    refactor(teamimage): added generic teamimage classes

--- components/image-composition/image-composition.js
+++ components/image-composition/image-composition.js
@@ -9,29 +9,29 @@ const ImageComposition = ({children, TeamImage2_1, TeamImage3_4, Person}) => {
 	return (
 		<div className="image-composition clearfix">
 			{parallaxLayerBack}
-			<div className="teamimage-large-align-left teamimage-large-padded">
+			<div className="teamimage-container-large teamimage-container-spaced teamimage-container-align-left">
 				<img srcSet={`
 					${TeamImage2_1.photo.url}&auto=format&q=90&w=375 375w,
 					${TeamImage2_1.photo.url}&auto=format&q=90&w=480 480w
 				`} sizes={`
 					(max-width: 1100px) calc(50vw - 40px)
-				`} src={`${TeamImage2_1.photo.url}&auto=format&q=90&w=452`}  alt="" className="teamimage-large-image" />
-				<span className="teamimage-large-title">
+				`} src={`${TeamImage2_1.photo.url}&auto=format&q=90&w=452`}  alt="" className="teamimage-container-image" />
+				<span className="teamimage-container-text-title">
 					{TeamImage2_1.title}
 				</span>
 			</div>
-			<div className="teamimage-normal-align-left ">
+			<div className="teamimage-container-large">
 				<img srcSet={`
 					${TeamImage3_4.photo.url}&auto=format&q=90&w=375 375w,
 					${TeamImage3_4.photo.url}&auto=format&q=90&w=480 480w
 				`} sizes={`
 					(max-width: 1100px) 50vw
-				`} src={`${TeamImage3_4.photo.url}&auto=format&q=90&w=452`} alt="" className="teamimage-normal-image" />
-				<span className="teamimage-normal-title">
+				`} src={`${TeamImage3_4.photo.url}&auto=format&q=90&w=452`} alt="" className="teamimage-container-image" />
+				<span className="teamimage-container-text-title">
 					{TeamImage3_4.title}
 				</span>
 			</div>
-			<div className="person">
+			<div className="teamimage-container-person">
 				<div className="transition-transform-start transition-normal-transform">
 					<img srcSet={`
 						${Person.photo.url}&auto=format&q=90&w=375 375w,
@@ -39,12 +39,12 @@ const ImageComposition = ({children, TeamImage2_1, TeamImage3_4, Person}) => {
 					`} sizes={`
 						(max-width: 1100px) 50vw
 					`} src={`${Person.photo.url}&auto=format&q=90&w=375`} alt="" 
-						className="person-image" />
-					<div className="person-text">
-						<span className="person-title">
+						className="teamimage-container-person-image" />
+					<div className="teamimage-person-container-text transition-normal-opacity">
+						<span className="teamimage-person-container-title">
 							{Person.name}
 						</span>
-						<span className="person-subtitle">
+						<span className="teamimage-person-container-subtitle">
 							{Person.role}
 						</span>
 					</div>

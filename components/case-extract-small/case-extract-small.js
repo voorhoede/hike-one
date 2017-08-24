4793deee5f45dba19d30ae29636e28122e20d31c
Jesse Eikema <jesse@voorhoede.nl>
2017-08-24 13:53:52 +0200

    style(work-item): added hover state to extract small item

---
+++ components/case-extract-small/case-extract-small.js
@@ -0,0 +1,26 @@
+import Icon from '../icon/icon';
+import ButtonCleanLink from '../buttons/button-clean/button-clean-link';
+import Link from 'next/link';
+
+const CaseExtractSmall = ({ item }) => (
+	<div className={`case-extract-small`}>
+		<Link href={`/case?slug=${item.slug}`} as={`/case/${item.slug}`}>
+			<a>
+				<div className="case-extract-small-image"
+				 style={{backgroundImage: `url(${item.headerBackgroundImage.url})`}}></div>
+				<div className={`case-extract-small-text
+					${item.color ? item.color: ''} shadow`}>
+					<span>{item.companyName}</span>
+					<h3>{item.title}</h3>
+					<h4>{item.subtitle}</h4>
+					<div className="case-extract-small-button">
+						<Icon icon="arrowRightCircle"/>
+					</div>
+				</div>
+			</a>
+		</Link>
+	</div>
+);
+
+
+export default CaseExtractSmall;

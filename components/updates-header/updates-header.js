a7648ec86d708da802ceb27c9255bf367e380ec0
Jesse Eikema <jesse@voorhoede.nl>
2017-08-29 12:46:48 +0200

    feat(shapes): add shapes and story to updates header

--- components/updates-header/updates-header.js
+++ components/updates-header/updates-header.js
@@ -1,8 +1,15 @@
-const updatesHeader = ({title = ''}) => {
+const updatesHeader = ({title = '', children}) => {
+	const childrenArray = React.Children.toArray(children);
+	const parallaxLayerFront = childrenArray.find(child => child.props.position === 'front');
+	const parallaxLayerBack = childrenArray.find(child => child.props.position === 'back');
 	return (
 		<div className="updates-header container">
-			<h1 className="updates-header-title">{title}</h1>	
-			<div className="updates-header-bg"></div>
+			{parallaxLayerBack}	
+			<div className="updates-header-container">
+				<h1 className="updates-header-title">{title}</h1>	
+				<div className="updates-header-bg"></div>
+			</div>
+			{parallaxLayerFront}
 		</div>
 	);
 };
--- pages/updates.js
+++ pages/updates.js
@@ -2,7 +2,7 @@ import Layout from '../components/layout/layout';
 import MenuBar from '../components/menu-bar/menu-bar';
 import Footer from '../components/footer/footer';
 import UpdatesHeader from '../components/updates-header/updates-header';
-
+import * as UpdatesHeaderShapes from '../components/updates-header/updates-header-shapes';
 import data from '../data/current/update.json';
 
 const updates = () => {
@@ -15,6 +15,8 @@ const updates = () => {
 				<article className="article">
 					<UpdatesHeader 
 						title={data.updatePageTitle}>
+						<UpdatesHeaderShapes.variation2Front position="front"/>
+						<UpdatesHeaderShapes.variation1Back position="back"/>
 					</UpdatesHeader>
 				</article>
 				<Footer />

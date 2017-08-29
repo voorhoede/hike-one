730cd3e3b75568afefcf6a71d93e7d57e66a5a7a
Jesse Eikema <jesse@voorhoede.nl>
2017-08-29 10:50:15 +0200

    refactor(update-header): remove unused code

--- components/updates-header/updates-header.js
+++ components/updates-header/updates-header.js
@@ -1,8 +1,3 @@
-import React from 'react';
-import Icon from '../icon/icon';
-import TextCenter from '../text-center/text-center';
-import Triangle from '../shapes/triangle/triangle';
-
 const updatesHeader = ({title = ''}) => {
 	return (
 		<div className="updates-header container">

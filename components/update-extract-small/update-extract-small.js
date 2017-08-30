90e3683e67b0b44984e69dfabe60705ddd8a3426
tjerksmit <tjerksmit@gmail.com>
2017-08-30 12:06:04 +0200

    fix(update extract) added styling and more srcset items

--- components/update-extract-small/update-extract-small.js
+++ components/update-extract-small/update-extract-small.js
@@ -1,12 +1,13 @@
 import getContrastYIQ from '../_helpers/getContrastYIQ';
 
-const updateExtractSmall = ({title='', date='', name='', extractImage='', color='', target=''}) => { 
+const updateExtractSmall = ({title='', date='', name='', extractImage='', color='', target=''}) => {
 	return (
-		<a href={target} target="_blank" className={`update-extract-small 
+		<a href={target} target="_blank" className={`update-extract-small
 			${ getContrastYIQ(color) === 'black' ? 'update-extract-small-text-dark' : '' }`}>
 			<img srcSet={`
-				${extractImage}&fm=jpg&q=90&w=345 345w,
-				${extractImage}&fm=jpg&q=90&w=370 370w
+				${extractImage}&fm=jpg&fit=max&q=90&w=345 345w,
+				${extractImage}&fm=jpg&fit=max&q=90&w=370 370w,
+				${extractImage}&fm=jpg&fit=max&q=90&w=737 737w
 			`} sizes={`
 				(max-width: 768px) calc(100vw - 30px),
 				(max-width: 1024px) calc(50vw - 15px),
@@ -14,7 +15,7 @@ const updateExtractSmall = ({title='', date='', name='', extractImage='', color=
 				370px
 			`} src={`${extractImage}&fm=jpg&q=90&w=370`} alt=""
 			className="update-extract-small-image" />
-			<div className="update-extract-small-text" 
+			<div className="update-extract-small-text"
 				style={{backgroundColor: color}}>
 				<div className="update-extract-small-type" style={{color: color}}>update</div>
 				<h2 className="update-extract-small-title">{title}</h2>

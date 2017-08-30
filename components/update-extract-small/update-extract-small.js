44cb87982d9e3f25993b04caa58bb83f8d517d94
tjerksmit <tjerksmit@gmail.com>
2017-08-30 15:10:46 +0200

    feat(update extract) get data from cms and use background img for correct display

--- components/update-extract-small/update-extract-small.js
+++ components/update-extract-small/update-extract-small.js
@@ -1,28 +1,36 @@
 import getContrastYIQ from '../_helpers/getContrastYIQ';
 
-const updateExtractSmall = ({title='', date='', name='', extractImage='', color='', target=''}) => {
+const updateExtractSmall = ({title='', date='', author='', image='', color='', target='', index}) => {
+	const imageSmallScreen = `${image}&fm=jpg&q=90&w=737`;
+	const imageLargeScreen = `${image}&fm=jpg&q=90&h=332`;
+
+	const style ={__html:
+		`<style>
+			.update-extract-small-image-${index} {
+				background-image: url(${imageSmallScreen});
+			}
+			
+			@media only screen and (min-width: 768px) {
+				.update-extract-small-image-${index} {
+					background-image: url(${imageLargeScreen});
+				}
+			}			
+		</style>`};
+
+
 	return (
 		<a href={target} target="_blank" className={`update-extract-small
 			${ getContrastYIQ(color) === 'black' ? 'update-extract-small-text-dark' : '' }`}>
-			<img srcSet={`
-				${extractImage}&fm=jpg&fit=max&q=90&w=345 345w,
-				${extractImage}&fm=jpg&fit=max&q=90&w=370 370w,
-				${extractImage}&fm=jpg&fit=max&q=90&w=737 737w
-			`} sizes={`
-				(max-width: 768px) calc(100vw - 30px),
-				(max-width: 1024px) calc(50vw - 15px),
-				(max-width: 1170px) calc(33vw - 30px),
-				370px
-			`} src={`${extractImage}&fm=jpg&q=90&w=370`} alt=""
-			className="update-extract-small-image" />
-			<div className="update-extract-small-text"
-				style={{backgroundColor: color}}>
+			<div className={`update-extract-small-image-${index} update-extract-small-image`}></div>
+			<div className="update-extract-small-text" style={{backgroundColor: color}}>
 				<div className="update-extract-small-type" style={{color: color}}>update</div>
 				<h2 className="update-extract-small-title">{title}</h2>
-				<span className="update-extract-small-subtitle">{date} - {name}</span>
+				<span className="update-extract-small-subtitle">{date} - {author}</span>
 			</div>
+
+			<div dangerouslySetInnerHTML={style}></div>
 		</a>
-	)
-}
+	);
+};
 
 export default updateExtractSmall;

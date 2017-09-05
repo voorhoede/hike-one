2d6ca4c8472b2eb692699ae0308c4cb839ddb5cc
Jesse Eikema <jesse@voorhoede.nl>
2017-09-05 11:17:56 +0200

    feat(storie): add mock data

--- components/company-overview-small/company-overview-small.stories.js
+++ components/company-overview-small/company-overview-small.stories.js
@@ -2,11 +2,34 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, text} from '@storybook/addon-knobs';
 import CompanyOverviewSmall from './company-overview-small';
-// TODO add correct endpoint for company ref
-import DataService from '../../data/current/home.json';
+import GuideData from '../../data/current/component-guide.json'
+
+// const CompanyReferences = [
+// 	{
+// 		referenceText: 'We designed, build, tested and launched a global marketplace in 6 weeks',
+// 		referenceCompanyLogo: {
+// 			url: 'https://www.datocms-assets.com/2625/1504268600-olx.png?'
+// 		}
+// 	},
+// 	{
+// 		referenceCaseLink: {
+// 			slug: 'gone-in-60-seconds'
+// 		},
+// 		referenceText: 'We designed, build, tested and launched a global marketplace in 6 weeks',
+// 		referenceCompanyLogo: {
+// 			url: 'https://www.datocms-assets.com/2625/1504268600-olx.png?'
+// 		}
+// 	},
+// 	{
+// 		referenceText: 'We designed, build, tested and launched a global marketplace in 6 weeks',
+// 		referenceCompanyLogo: {
+// 			url: 'https://www.datocms-assets.com/2625/1504268600-olx.png?'
+// 		}
+// 	}
+// ]
 
 storiesOf('Company overview small', module)
 	.addDecorator(withKnobs)
 	.add('company overview small', () => (
-		<CompanyOverviewSmall services={DataService.serviceItems} />
+		<CompanyOverviewSmall services={[GuideData.components.companies]} />
 	));

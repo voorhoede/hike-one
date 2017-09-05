d32a6561a6cd409227e4b78cd78ab7ce760a3dc6
tjerksmit <tjerksmit@gmail.com>
2017-09-05 13:31:04 +0200

    chore(update detail) added all components on update detail page

--- pages/update.js
+++ pages/update.js
@@ -12,6 +12,48 @@ const Update = ({Data, fontsLoaded}) => (
 			<MenuBar/>
 			<article className="article">
 				<h1> { Data.title } </h1>
+
+				{ Data.content.map((component, index) => {
+					switch (component.itemType) {
+						case 'body_heading':
+							return <h2 key={index}>{ component.title }</h2>;
+						case 'body_quote':
+							return <blockquote key={index}>{ component.quote }</blockquote>;
+						case 'body_text':
+							return <p key={index}>{ component.text }</p>;
+						case 'inline_image':
+							return (
+								<div key={index}>
+									<img src={component.image.url} />
+									{ component.caption && <p>{ component.caption }</p> }
+								</div>
+							);
+						case 'inline_image_large':
+							return (
+								<div key={index}>
+									<img src={component.image.url} />
+									{ component.caption && <p>{ component.caption }</p> }
+								</div>
+							);
+						case 'list':
+							return (
+								<p key={index}>{component.items}</p>
+							);
+						case 'full_width_image_small':
+							return (
+								<div key={index}>
+									<img src={component.image.url} />
+								</div>
+							);
+					}
+				})}
+
+				<div>
+					<p>{Data.author.name}</p>
+					<p>{Data.author.role}</p>
+					<img src={Data.author.photo.url} />
+					<p>{Data.author.summary}</p>
+				</div>
 			</article>
 			<Footer />
 		</main>

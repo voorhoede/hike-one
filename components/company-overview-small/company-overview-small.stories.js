62e2ca306c51acb04db871975d23a1ab84fde622
Jesse Eikema <jesse@voorhoede.nl>
2017-09-05 11:38:17 +0200

    feat(stories): added mock data

--- components/company-overview-small/company-overview-small.stories.js
+++ components/company-overview-small/company-overview-small.stories.js
@@ -2,34 +2,33 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, text} from '@storybook/addon-knobs';
 import CompanyOverviewSmall from './company-overview-small';
-import GuideData from '../../data/current/component-guide.json'
 
-// const CompanyReferences = [
-// 	{
-// 		referenceText: 'We designed, build, tested and launched a global marketplace in 6 weeks',
-// 		referenceCompanyLogo: {
-// 			url: 'https://www.datocms-assets.com/2625/1504268600-olx.png?'
-// 		}
-// 	},
-// 	{
-// 		referenceCaseLink: {
-// 			slug: 'gone-in-60-seconds'
-// 		},
-// 		referenceText: 'We designed, build, tested and launched a global marketplace in 6 weeks',
-// 		referenceCompanyLogo: {
-// 			url: 'https://www.datocms-assets.com/2625/1504268600-olx.png?'
-// 		}
-// 	},
-// 	{
-// 		referenceText: 'We designed, build, tested and launched a global marketplace in 6 weeks',
-// 		referenceCompanyLogo: {
-// 			url: 'https://www.datocms-assets.com/2625/1504268600-olx.png?'
-// 		}
-// 	}
-// ]
+const CompanyReferences = [
+	{
+		referenceText: 'We designed, build, tested and launched a global marketplace in 6 weeks',
+		referenceCompanyLogo: {
+			url: '../../static/images/ziggo.png'
+		}
+	},
+	{
+		referenceCaseLink: {
+			slug: 'gone-in-60-seconds'
+		},
+		referenceText: 'We designed, build, tested and launched a global marketplace in 6 weeks',
+		referenceCompanyLogo: {
+			url: '../../static/images/ziggo.png'
+		}
+	},
+	{
+		referenceText: 'We designed, build, tested and launched a global marketplace in 6 weeks',
+		referenceCompanyLogo: {
+			url: '../../static/images/ziggo.png'
+		}
+	}
+]
 
 storiesOf('Company overview small', module)
 	.addDecorator(withKnobs)
 	.add('company overview small', () => (
-		<CompanyOverviewSmall services={[GuideData.components.companies]} />
+		<CompanyOverviewSmall services={CompanyReferences} />
 	));

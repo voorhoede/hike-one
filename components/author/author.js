832b6fc9847e822be89a655c079f826fe8d7aefa
Jesse Eikema <jesse@voorhoede.nl>
2017-09-12 17:19:43 +0200

    fix(images): removed auto format

--- components/author/author.js
+++ components/author/author.js
@@ -1,7 +1,8 @@
+
 const Author = ({name='', role='', photoUrl='', summary=''}) => (
 	<div className="author">
 		<img className="author-image"	
-			src={`${photoUrl}&auto=format&fm=jpg&fit=crop&q90&h=123&w=92 `}/>	
+			src={`${photoUrl}&fm=jpg&fit=crop&q90&h=123&w=92 `}/>	
 		<div className="author-text">
 			<p className="author-name">{name}</p>
 			<p className="author-role">{role}</p>

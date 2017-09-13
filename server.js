f30f9f531d287ed09c4cec2a7d1801b4b1343a0f
Jasper Moelker <j.b.moelker@activeids.nl>
2017-09-13 12:12:52 +0200

    Extract API into module, cleanup server.js

--- server.js
+++ server.js
@@ -1,71 +1,26 @@
-const dataLoader = require('./lib/data-loader')
+const apiRouter = require('./lib/api-router');
 const express = require('express');
 const next = require('next');
 const cookieParser = require('cookie-parser');
 
-require('dotenv').config();
-
 const dev = process.env.NODE_ENV !== 'production';
+const port = process.env.PORT || 3000;
 const app = next({ dev });
 const handle = app.getRequestHandler();
-const reloadToken = process.env.TOKEN;
+const server = express();
 
-app.prepare()
-	.then(() => {
-		const server = express();
+const startServer = () => server.listen(port, (err) => {
+	if (err) throw err;
+	console.log(`> Ready on http://localhost:${port}`);
+});
 
-		// so we can serve files from the root directory instead of next.js default static folder
-		server.use(express.static('./static/root'));
+server.use(express.static('./static/root'));
+server.use(cookieParser());
+server.use('/api/', apiRouter)
+server.use('/guide/', express.static('./build/guide/'));
+server.get('/case/:slug', (req, res) => app.render(req, res, '/case', {slug: req.params.slug}));
+server.get('/service/:slug', (req, res) => app.render(req, res, '/service', {slug: req.params.slug}));
+server.get('/update/:slug', (req, res) => app.render(req, res, '/update', {slug: req.params.slug}));
+server.get('*', (req, res) => handle(req, res));
 
-		server.use(cookieParser());
-		server.use('/guide/', express.static('./build/guide/'));
-
-		server.post('/api/reload-data/:token', (req, res) => {
-			if (reloadToken && reloadToken === req.params.token) {
-				dataLoader.reload()
-				.then(newData => {
-					res.json(newData)
-					console.log('Data reloaded.')
-				})
-				.catch(error => {
-					res.status(500).json({ status: 'Error loading data.' })
-					console.error('Error reloading data', error)
-				})
-			} else {
-				res.status(401).json({ status: 'Invalid token.' })
-			}
-		})
-
-		server.get('/api/:model', (req, res) => {
-			const { model } = req.params;
-			dataLoader.load(model).then(data => res.json(data));
-		})
-
-		server.get('/api/:model/:slug', (req, res) => {
-			const { model, slug } = req.params;
-			dataLoader.load(model)
-				.then(items => items.find(item => item.slug === slug))
-				.then(item => res.json(item));
-		});
-
-		server.get('/case/:slug', (req, res) => {
-			app.render(req, res, '/case', {slug: req.params.slug});
-		});
-
-		server.get('/service/:slug', (req, res) => {
-			app.render(req, res, '/service', {slug: req.params.slug});
-		});
-
-		server.get('/update/:slug', (req, res) => {
-			app.render(req, res, '/update', {slug: req.params.slug});
-		});
-
-		server.get('*', (req, res) => {
-			return handle(req, res);
-		});
-
-		server.listen(3000, (err) => {
-			if (err) throw err;
-			console.log('> Ready on http://localhost:3000');
-		});
-	});
+app.prepare().then(startServer);

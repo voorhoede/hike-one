9d0f052778af1c544a40f4b8c5d86d9e40828e89
Jesse Eikema <jesse@voorhoede.nl>
2017-09-13 10:52:38 +0200

    feat(author): added setImageParams function

--- components/author/author.js
+++ components/author/author.js
@@ -1,14 +1,18 @@
+import setImageParams from '../_helpers/setImageParameters';
 
-const Author = ({name='', role='', photoUrl='', summary=''}) => (
-	<div className="author">
-		<img className="author-image"	
-			src={`${photoUrl}&fm=jpg&fit=crop&q90&h=123&w=92 `}/>	
-		<div className="author-text">
-			<p className="author-name">{name}</p>
-			<p className="author-role">{role}</p>
-			<p className="author-summary">{summary}</p>
+const Author = ({name='', role='', photoUrl='', summary=''}) => {
+	const imageParameters = { fit: 'crop', fm: 'jpg', q: 90 };	
+	return (
+		<div className="author">
+			<img className="author-image"	
+				src={`${setImageParams(photoUrl, {...imageParameters, w: 92, h: 123} )}`}/>	
+			<div className="author-text">
+				<p className="author-name">{name}</p>
+				<p className="author-role">{role}</p>
+				<p className="author-summary">{summary}</p>
+			</div>
 		</div>
-	</div>	
-)
+	)
+}
 
 export default Author;

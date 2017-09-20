77b2e7cb955812ba5542989c543f93712f5ea9ae
Bas de Vleeschhouwer <hallo@basdevleeschhouwer.nl>
2017-09-20 16:29:46 +0200

    fix(error): set up error pages

--- pages/_error.js
+++ pages/_error.js
@@ -5,23 +5,28 @@ import MenuBar from '../components/menu-bar/menu-bar';
 import Footer from '../components/footer/footer';
 import cookie from '../components/_helpers/cookie';
 import TextCenter from '../components/text-center/text-center';
-
-// inline style is temp
+import * as TextCenterShapes from '../components/text-center/text-center-shapes';
 
 const Error = ({statusCode, fontsLoaded}) =>  (
 	<Layout title="Hike One - Home" fontsLoaded={fontsLoaded}>
 		<main className="main js-main" >
 			<MenuBar color="black" />
-			<article className="article" style={{height: '100vh'}}>
+			<article className="article article-error" style={{height: '100vh'}}>
 				<TextCenter
-					title="Oops something went wrong"
-					text={statusCode
-						? `An error ${statusCode} occurred on server`
-						: 'An error occurred on client'} />
+					title={statusCode == '404'
+						? `This page is not here`
+						: 'Whoops, something went wrong'}
+					text={statusCode == '404'
+						? 'We lost the page! Don′t worry we know the way ;) Check out <a href="/team">who we are</a> and <a href="/work">what we do</a>'
+						: 'Don′t worry we′ll be back as soon as we can. In the mean time, feel free to call <a href="tel:+31202044577">+31 20 204 45 77</a> or send us a message on <a href="mailto:hello@hike.one">hello@hike.one</a>'} >
+					<TextCenterShapes.variation3Back position="back" />
+					<TextCenterShapes.variation4Front position="front" />
+				</TextCenter>
 			</article>
 			<Footer
 				callToActionLabel="Up for a new challenge yourself? Join us!"
-				callToActionUrl="http://werkenbij.unitid.nl" />
+				callToActionUrl="http://werkenbij.unitid.nl"
+				disableParallax={true} />
 		</main>
 	</Layout>
 );

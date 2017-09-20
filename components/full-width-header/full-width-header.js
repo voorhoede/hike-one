135ce5b23ba5b0aa1d75c7f4fcd23ba8b779db97
tjerksmit <tjerksmit@gmail.com>
2017-09-20 12:17:15 +0200

    feat(full width header) added option for only showing title

--- components/full-width-header/full-width-header.js
+++ components/full-width-header/full-width-header.js
@@ -2,11 +2,13 @@ import Icon from '../icon/icon';
 import Link from 'next/link';
 import getDateFormat from '../_helpers/getDateFormat';
 
-const FullWidthHeader = ({headerImage='', color='', title='', authorName='', updatedDate=''}) => (
+const FullWidthHeader = ({headerImage='', color='', title='', authorName='', updatedDate='', titleOnly = false}) => (
 	<div className="full-width-header">
 		<div className="full-width-header-image" style={{backgroundImage: `url(${headerImage})`}}></div>
 		<div className="full-width-header-inner-container">
 			<div className="full-width-header-text shadow" style={{backgroundColor:`${color}`}}>
+
+				{ !titleOnly &&
 				<div className="full-width-header-back">
 					<Link href={`/updates`}  as={`/updates`}>
 						<a className="full-width-header-back-container">
@@ -15,11 +17,14 @@ const FullWidthHeader = ({headerImage='', color='', title='', authorName='', upd
 						</a>
 					</Link>
 				</div>
+				}
 				<div className="full-width-header-text-container">
 					<h1 className="full-width-header-title">{title}</h1>
-					<span className="full-width-header-author">
-						{authorName} - {`${getDateFormat(updatedDate)}`}
-					</span>
+					{ !titleOnly &&
+						<span className="full-width-header-author">
+							{authorName} - {`${getDateFormat(updatedDate)}`}
+						</span>
+					}
 				</div>
 			</div>
 		</div>

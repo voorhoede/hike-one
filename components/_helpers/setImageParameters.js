e890495cdd4b132ce771a77d883e0ed3df20c7a7
tjerksmit <tjerksmit@gmail.com>
2017-09-20 15:55:20 +0200

    feat(imgix) strip color data

--- components/_helpers/setImageParameters.js
+++ components/_helpers/setImageParameters.js
@@ -1,10 +1,11 @@
 function setImageParams(imageUrl, params){
-	const querString = Object.keys(params).reduce((acc, item, index, array) => {	
+	const defaults = 'cs=strip&';
+	const querString = Object.keys(params).reduce((acc, item, index, array) => {
 		const amp = array.length - 1 === index ? ' ' : '&';
 		return `${acc}${item}=${params[item]}${amp}`;
-	}, '') 
-	
-	return `${imageUrl}${querString}`;
+	}, '');
+
+	return `${imageUrl}${defaults}${querString}`;
 }
 
 export default setImageParams;

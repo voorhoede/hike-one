476db7b2ef86f68a4778e91652eee1a720fd2fbc
Jesse Eikema <jesse@voorhoede.nl>
2017-10-12 16:41:13 +0200

    feat(inline-image): add optional video for gifs

--- components/inline-image/inline-image.js
+++ components/inline-image/inline-image.js
@@ -1,25 +1,30 @@
 import setImageParams from '../_helpers/setImageParameters';
 
-const inlineImage= ({image = '#', caption = null, large = false}) => {
+const inlineImage= ({image = "#", caption = null, large = false, video = false, videoSrc = "" }) => {
 	const imageParameters = { fit: 'max', fm: 'pjpg', q: 85 }
 
 	return (
 		<div className={`inline-image ${large ? 'inline-image-large' : ''}`}>
 			<div className={`inline-image-container ${large ? 'inline-image-container-large': ''}`}>
-				<img
-				srcSet={`
-					${setImageParams(image, {...imageParameters, w: 445, h:350} )} 768w,
-					${setImageParams(image, {...imageParameters, w: 738, h:400} )} 1024w,
-					${setImageParams(image, {...imageParameters, w: 821, h:500} )} 1190w,
-					${setImageParams(image, {...imageParameters, w: 1100, h:650} )} 1440w,
-				`}
-				sizes={`
-					(max-width: 320px) calc(100vw - 30px),
-					(max-width: 768px) calc(100vw - 30px),
-					(max-width: 1024px) 790px,
-					821px"`}
-				alt=""
-				src={image}/>
+				{ videoSrc &&
+					<video src={`${videoSrc}`} autoPlay muted loop playsInline></video>	
+				}	
+				{ image &&
+					<img
+					srcSet={`
+						${setImageParams(image.url, {...imageParameters, w: 445, h:350} )} 768w,
+						${setImageParams(image.url, {...imageParameters, w: 738, h:400} )} 1024w,
+						${setImageParams(image.url, {...imageParameters, w: 821, h:500} )} 1190w,
+						${setImageParams(image.url, {...imageParameters, w: 1100, h:650} )} 1440w,
+					`}
+					sizes={`
+						(max-width: 320px) calc(100vw - 30px),
+						(max-width: 768px) calc(100vw - 30px),
+						(max-width: 1024px) 790px,
+						821px"`}
+					alt=""
+					src={image.url}/>
+				}
 			</div>
 			{caption && <p>{ caption }</p>}
 		</div>

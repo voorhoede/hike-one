2cc31dfeffe2e35d3eba3ec2ddea14d86ba52e5f
Selwyn <talk@selwyn.cc>
2017-11-02 15:20:18 +0100

    Fix contrast issues

--- components/update-extract-small/update-extract-small.js
+++ components/update-extract-small/update-extract-small.js
@@ -9,28 +9,28 @@ const updateExtractSmall = ({title='', date='', author='', image='', color='', t
 			.update-extract-small-image-${index} {
 				background-image: url("${setImageParams(image, { ...imageParameters, w: 550, h:200 })}");
 			}
-			
+
 			@media only screen and (min-width: 768px) {
 				.update-extract-small-image-${index} {
 					background-image: url("${setImageParams(image, { ...imageParameters, w: 470, h:332 })}");
 				}
-			}		
-			
+			}
+
 			@media only screen and (min-width: 1024px) {
 				.update-extract-small-image-${index} {
 					background-image: url("${setImageParams(image, { ...imageParameters, w: 337, h:366 })}");
 				}
-			}			
+			}
 		</style>`};
 
 	return (
 		<a href={target} target={external ? '_blank': ''} className="update-extract-small">
 			<div dangerouslySetInnerHTML={style}></div>
 			<div className={`update-extract-small-image-${index} update-extract-small-image`}></div>
-			<div className="update-extract-small-text" style={{backgroundColor: color}}>
-				<div className="update-extract-small-type" style={{color: color}}>{category}</div>
+			<div className="update-extract-small-text">
+				<div className="update-extract-small-type">{category}</div>
 				<h2 className="update-extract-small-title">{title}</h2>
-				<span className="update-extract-small-subtitle" style={{backgroundColor: color}}>
+				<span className="update-extract-small-subtitle">
 				{ `${getDateFormat(date)}` } - {author}</span>
 			</div>
 		</a>

0402256b31898b48908c61b33aa1df3dca777f4f
Artemis <artemis-kalemis@hotmail.com>
2018-07-11 16:27:35 +0200

    feat(context-menu) Created context menu component. No functionality yet.

---
+++ components/context-menu/context-menu.stories.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import ContextMenu from './context-menu';
+
+const ContextMenuDecorator = (storyFn) => (
+	<div style={{ margin: '20px' }}>
+		{ storyFn() }
+	</div>
+);
+
+
+storiesOf('Context Menu', module)
+	.addDecorator(ContextMenuDecorator)
+	.add('context menu', () => (
+		<ContextMenu />
+	));

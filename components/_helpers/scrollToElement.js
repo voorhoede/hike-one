6ea44a92e4c035f2b2ef3f1befd87f2b109f4fb1
Selwyn <talk@selwyn.cc>
2018-07-13 14:46:45 +0200

    chore(package): update non-breaking dependencies

--- components/_helpers/scrollToElement.js
+++ components/_helpers/scrollToElement.js
@@ -1,5 +1,5 @@
 import TweenLite from "gsap";
-import ScrollToPlugin from "gsap/ScrollToPlugin";
+import ScrollToPlugin from "gsap/umd/ScrollToPlugin";
 
 const scrollToElement = (elementClass) => {
 	TweenLite.to(window, 0.3, {scrollTo: { y:`.${elementClass}` , autoKill:false } });

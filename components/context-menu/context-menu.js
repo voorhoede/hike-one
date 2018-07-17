efeecdfdc4f6b34dc2b156baab8c10151ca9863c
Casper Boutens <casper@voorhoede.nl>
2018-07-17 13:39:17 +0200

    feat(context-menu): add context menu on right click under logo

--- components/context-menu/context-menu.js
+++ components/context-menu/context-menu.js
@@ -1,9 +1,9 @@
-const ContactMenu = () => {
-	return(
-		<section className="context-menu shadow-low-opacity">
-			<h5 className="context-menu-title">Op zoek naar ons logo?</h5>
-			<div className="context-menu-wrapper">
-				<table className="context-menu-table">
+const ContactMenu = ({isOpen}) => (
+	<section className={`context-menu shadow-low-opacity ${isOpen ? '' : 'is-hidden' }`}>
+		<p className="context-menu-title">Op zoek naar ons logo?</p>
+		<div className="context-menu-wrapper">
+			<table className="context-menu-table">
+				<tbody>
 					<tr className="context-menu-table-row">
 						<td>Voor een witte achtergrond:</td>
 						<td><a className="context-menu-logo-download" href="../../static/images/logo-black.png" download>PNG</a></td>
@@ -14,12 +14,12 @@ const ContactMenu = () => {
 						<td><a className="context-menu-logo-download" href="../../static/images/logo-white.png" download>PNG</a></td>
 						<td><a className="context-menu-logo-download" href="../../static/images/logo-white.eps" download>EPS</a></td>
 					</tr>
-				</table>
-				<img className="context-menu-logo-image" src="../../static/images/logo-black.png" alt="logo black"/>
-			</div>
-			<a className="context-menu-logo-download-all" href="../../static/images/hike-one-logo.zip" download>Download alle logo's</a>
-		</section>
-	);
-};
+				</tbody>
+			</table>
+			<img className="context-menu-logo-image" src="../../static/images/logo-black.png" alt="logo black"/>
+		</div>
+		<a className="context-menu-logo-download-all" href="../../static/images/hike-one-logo.zip" download>Download alle logo's</a>
+	</section>
+);
 
 export default ContactMenu;

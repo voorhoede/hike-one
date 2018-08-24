4e6f4e6428d1141f05e8cea70963501b8175a6f1
bruna <brunadafonseca@gmail.com>
2018-08-24 12:10:36 +0200

    Add responsive behavior

--- components/vacancy-card/vacancy-card.js
+++ components/vacancy-card/vacancy-card.js
@@ -1,22 +1,56 @@
-import ButtonCleanLink from '../buttons/button-clean/button-clean-link'
+import React from 'react';
 
-const VacancyCard = ({ data }) => (
-	<div className="vacancy-card container">
-    <div className="vacancy-card-image">
-      <img src={data.image.url} />
-    </div>
+import setImageParams from '../_helpers/setImageParameters';
 
-		<h2 className="vacancy-card-title">{data.title}</h2>
+import ButtonSecondaryLink from '../buttons/button-secondary/button-secondary-link'
+import Icon from '../icon/icon';
 
-		<p className="vacancy-card-tagline">{data.content}</p>
+const VacancyCard = ({ data }) => {
+	
+	const imageParameters = { fit: 'crop', fm: 'pjpg', q: 85 };
+	
+	const style ={__html:
+		`<style>
+			.vacancy-card-image {
+				background-image: url("${setImageParams(data.image.url, { ...imageParameters, w: 450, h:200 })}");
+			}
 
-		<ButtonCleanLink
-			href={data.callToActionUrl}
-			target="_blank"
-			classes="btn-large content" >
-			{data.callToActionTitle}
-		</ButtonCleanLink>
-	</div>
-)
+			@media only screen and (min-width: 768px) {
+				.vacancy-card-image {
+					background-image: url("${setImageParams(data.image.url, { ...imageParameters, w: 400, h:339 })}");
+				}
+			}
+
+			@media only screen and (min-width: 1024px) {
+				.vacancy-card-image {
+					background-image: url("${setImageParams(data.image.url, { ...imageParameters, w: 450, h:360 })}");
+				}
+			}
+		</style>`};
+
+	return (
+		<div className="container">
+			<div className="vacancy-card container-inner">
+				<div className="vacancy-card-image"></div>
+
+				<div className="vacancy-card-content">
+					<h2 className="vacancy-card-title">{data.title}</h2>
+
+					<p className="vacancy-card-tagline">{data.content}</p>
+
+					<ButtonSecondaryLink
+						href={data.callToActionUrl}
+						target="_blank"
+						classes="vacancies-btn"
+						icon="arrowRight">
+						{data.callToActionTitle}
+					</ButtonSecondaryLink>
+				</div>
+			</div>
+
+			<div dangerouslySetInnerHTML={style}></div>
+		</div>
+		)
+	}
 
 export default VacancyCard

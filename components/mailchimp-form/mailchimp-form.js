2673feb6f3f6cc4e6912b470f0f0ad0138fe8b02
bruna <brunadafonseca@gmail.com>
2018-11-01 17:05:12 +0100

    Restyle component

--- components/mailchimp-form/mailchimp-form.js
+++ components/mailchimp-form/mailchimp-form.js
@@ -1,26 +1,18 @@
 import React from 'react'
 
-const MailchimpForm = () => (
-  <div id="mc_embed_signup" className="mailchimp-form">
+const MailchimpForm = ({ title='', description='', buttonLabel }) => (
+  <div id="mc_embed_signup" className="mailchimp-form background-color">
+    <p class="form-title">{ title }</p>
+    <p class="form-description">{ description }</p>
+
     <form
       action='https://unitid.us9.list-manage.com/subscribe/post?u=9fcf53aac8cfc03a445bd4e2f&amp;id=5088a72738'
       method="POST"
       id="mc-embedded-subscribe-form"
       name="mc-embedded-subscribe-form"
-      className="validate" target="_blank"
+      className="validate form subscription-form" target="_blank"
       noValidate="">
-
-      <div id="mc_embed_signup_scroll" className="form">
-        <div className="mc-field-group input-field">
-          <label htmlFor="mce-FNAME" className="label">First Name</label>
-          <input type="text" name="FNAME" className="input" id="mce-FNAME" />
-        </div>
-
-        <div className="mc-field-group input-field">
-          <label htmlFor="mce-LNAME" className="label">Last Name </label>
-          <input type="text" name="LNAME" className="input" id="mce-LNAME" />
-        </div>
-
+      {/* <div id="mc_embed_signup_scroll" className="form"> */}
         <div className="mc-field-group input-field input-field-full-width">
           <label htmlFor="mce-EMAIL" className="label">Email Address <span className="asterisk">*</span></label>
           <input type="email" name="EMAIL" className="required email input" id="mce-EMAIL" />
@@ -32,8 +24,8 @@ const MailchimpForm = () => (
         </div>
         {/* <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups--> */}
         <div style={{ position: 'absolute', left: '-5000px' }} aria-hidden="true"><input type="text" name="b_9fcf53aac8cfc03a445bd4e2f_5088a72738" value="" tabIndex="-1" /></div>
-        <div className="clear btn-right-aligned"><input type="submit" value="Get the free toolkit" name="subscribe" id="mc-embedded-subscribe" className="button btn-primary submit-btn btn-large" /></div>
-      </div>
+        <div className="clear btn-right-aligned"><input type="submit" value={buttonLabel} name="subscribe" id="mc-embedded-subscribe" className="button btn-primary submit-btn btn-large" /></div>
+      {/* </div> */}
     </form>
   </div>
 )
--- pages/topic.js
+++ pages/topic.js
@@ -24,156 +24,164 @@ import FiftyFifty from '../components/50-50/50-50';
 import MailchimpForm from '../components/mailchimp-form/mailchimp-form'
 
 const Topic = ({Data, fontsLoaded, fullUrl}) => (
-	<Layout title={`Hike One - ${Data.title}`}
-			fontsLoaded={fontsLoaded}
-			seo={Data.seo}
-			url={fullUrl} >
-		<main className="main js-main">
-			<MenuBar color="white" />
-			<article className="article">
-				<FullWidthHeader
-					headerImage={Data.headerImage.url}
-					color={Data.color.hex}
-					title={Data.title}
-					titleOnly={true} />
+  <Layout title={`Hike One - ${Data.title}`}
+      fontsLoaded={fontsLoaded}
+      seo={Data.seo}
+      url={fullUrl} >
+    <main className="main js-main">
+      <MenuBar color="white" />
+      <article className="article">
+        <FullWidthHeader
+          headerImage={Data.headerImage.url}
+          color={Data.color.hex}
+          title={Data.title}
+          titleOnly={true} />
 
-				{ Data.content.map((component, index) => {
-					switch (component.itemType) {
-						case 'rich_body_text':
-							return (
-								<RichBodyText key={index} content={component.content}/>
-							);
-						case 'body_quote':
-							return <BodyQuote key={index} quote={component.quote}/>;
+        { Data.content.map((component, index) => {
+          switch (component.itemType) {
+            case 'rich_body_text':
+              return (
+                <RichBodyText key={index} content={component.content}/>
+              );
+            case 'body_quote':
+              return <BodyQuote key={index} quote={component.quote}/>;
 
-						case '50_50_text_right':
-							return (
-								<FiftyFifty
-									key={index}
-									title={component.title}
-									text={component.text}
-									image={component.image.url}>
-								</FiftyFifty>
-							);
-						case '50_50_text_left':
-							return (
-								<FiftyFifty
-									key={index}
-									contentLeft="true"
-									title={component.title}
-									text={component.text}
-									image={component.image.url}>
-								</FiftyFifty>
-							);
-						case 'inline_image':
-							const image = component.image ? component.image.url : undefined;
-							return (
-								<InlineImage
-									key={index}
-									image={image}
-									caption={component.caption} />
-							);
-						case 'inline_image_large':
-							const imageLarge = component.image ? component.image.url : undefined;
-							return (
-								<InlineImage
-									key={index}
-									large={true}
-									image={imageLarge}
-									caption={component.caption} />
-							);
-						case 'full_width_image_small':
-							return (
-								<FullWidthImageSmall
-									key={index}
-									index={index}
-									image={component.image.url} />
-							);
-						case 'logo_carousel':
-							return (
-								<LogoCarousel
-									key={index}
-									title={component.title}
-									companies={component.companies}
-									animationSpeed={component.animationSpeed}/>
-							);
-						case 'call_to_action':
-								return (
-									<CallToAction
-										key={index}
-										title={component.title}
-										url={component.url} />
-								);
+            case '50_50_text_right':
+              return (
+                <FiftyFifty
+                  key={index}
+                  title={component.title}
+                  text={component.text}
+                  image={component.image.url}>
+                </FiftyFifty>
+              );
+            case '50_50_text_left':
+              return (
+                <FiftyFifty
+                  key={index}
+                  contentLeft="true"
+                  title={component.title}
+                  text={component.text}
+                  image={component.image.url}>
+                </FiftyFifty>
+              );
+            case 'inline_image':
+              const image = component.image ? component.image.url : undefined;
+              return (
+                <InlineImage
+                  key={index}
+                  image={image}
+                  caption={component.caption} />
+              );
+            case 'inline_image_large':
+              const imageLarge = component.image ? component.image.url : undefined;
+              return (
+                <InlineImage
+                  key={index}
+                  large={true}
+                  image={imageLarge}
+                  caption={component.caption} />
+              );
+            case 'full_width_image_small':
+              return (
+                <FullWidthImageSmall
+                  key={index}
+                  index={index}
+                  image={component.image.url} />
+              );
+            case 'logo_carousel':
+              return (
+                <LogoCarousel
+                  key={index}
+                  title={component.title}
+                  companies={component.companies}
+                  animationSpeed={component.animationSpeed}/>
+              );
+            case 'call_to_action':
+              return (
+                <CallToAction
+                  key={index}
+                  title={component.title}
+                  url={component.url} />
+              );
+            case 'subscription_form':
+              return (
+                <MailchimpForm
+                  key={index}
+                  title={component.form.title}
+                  description={component.form.description}
+                  buttonLabel={component.form.button} />
+              );
 
-					}
-				})}
-				{ Data.hasSubscriptionForm && <MailchimpForm /> }
+          }
+        })}
+        { Data.hasSubscriptionForm && <MailchimpForm /> }
 
-				<SocialShare
-					facebookLink={`https://www.facebook.com/sharer/sharer.php?u=${fullUrl}`}
-					linkedinLink={`https://www.linkedin.com/shareArticle?mini=true&url=${fullUrl}&title=${Data.title}&summary=${Data.seo.description}&source=Hike&20One`}
-					twitterLink={`https://twitter.com/intent/tweet?text=${Data.title}&url=${fullUrl}`}
-				/>
+        <SocialShare
+          facebookLink={`https://www.facebook.com/sharer/sharer.php?u=${fullUrl}`}
+          linkedinLink={`https://www.linkedin.com/shareArticle?mini=true&url=${fullUrl}&title=${Data.title}&summary=${Data.seo.description}&source=Hike&20One`}
+          twitterLink={`https://twitter.com/intent/tweet?text=${Data.title}&url=${fullUrl}`}
+        />
 
-				<Contact
-					title={Data.contact.title}
-					button={Data.contact.button}
-					link={Data.contact.externalLink}
-					target='_blank' >
-					<ContactShapes.variation1Front position="front" />
-				</Contact>
+        <Contact
+          title={Data.contact.title}
+          button={Data.contact.button}
+          link={Data.contact.externalLink}
+          target='_blank' >
+          <ContactShapes.variation1Front position="front" />
+        </Contact>
 
-				<TextCenter title={Data.caseLinksTitle} />
+        <TextCenter title={Data.caseLinksTitle} />
 
-				<WorkOverview>
-					{ Data.caseLinks.map((item, index) => (
-						<CaseExtractSmall
-							key={index}
-							title={item.header.title}
-							subtitle={item.header.subtitle}
-							image={item.header.backgroundImage}
-							companyName={item.companyName}
-							color={item.caseThemeColor.hex}
-							slug={item.slug} />
-					))}
-				</WorkOverview>
-					{ Data.updateLinks.length > 0 &&
-					<div>
-						<TextCenter title={Data.updateLinksTitle} />
+        <WorkOverview>
+          { Data.caseLinks.map((item, index) => (
+            <CaseExtractSmall
+              key={index}
+              title={item.header.title}
+              subtitle={item.header.subtitle}
+              image={item.header.backgroundImage}
+              companyName={item.companyName}
+              color={item.caseThemeColor.hex}
+              slug={item.slug} />
+          ))}
+        </WorkOverview>
+          { Data.updateLinks.length > 0 &&
+          <div>
+            <TextCenter title={Data.updateLinksTitle} />
 
-						<UpdateOverviewSmall>
-							{ Data.updateLinks.map((item, index) => (
-								<UpdateExtractSmall
-									key={index}
-									index={index}
-									title={item.title}
-									date={item.date}
-									authors={item.authors}
-									target={item.link}
-									image={item.image.url}
-									category={item.category.name}
-									color={item.themeColor.hex}
-									external={item.isExternalLink} />
-							))}
-						</UpdateOverviewSmall>
-					</div>
-				}
-			</article>
-			<Footer
-				callToActionLabel={Data.footer.callToActionLabel}
-				callToActionUrl={Data.footer.callToActionUrl} />
-		</main>
-	</Layout>
+            <UpdateOverviewSmall>
+              { Data.updateLinks.map((item, index) => (
+                <UpdateExtractSmall
+                  key={index}
+                  index={index}
+                  title={item.title}
+                  date={item.date}
+                  authors={item.authors}
+                  target={item.link}
+                  image={item.image.url}
+                  category={item.category.name}
+                  color={item.themeColor.hex}
+                  external={item.isExternalLink} />
+              ))}
+            </UpdateOverviewSmall>
+          </div>
+        }
+      </article>
+      <Footer
+        callToActionLabel={Data.footer.callToActionLabel}
+        callToActionUrl={Data.footer.callToActionUrl} />
+    </main>
+  </Layout>
 );
 
 Topic.getInitialProps = async ({req, query, asPath}) => {
-	const baseUrl = req ? `${req.protocol}://${req.get('Host')}` : '';
-	const fullUrl = `${baseUrl}${asPath}`;
-	const res = await fetch(`${baseUrl}/api/topics/${query.slug}`);
-	const json = await res.json();
+  const baseUrl = req ? `${req.protocol}://${req.get('Host')}` : '';
+  const fullUrl = `${baseUrl}${asPath}`;
+  const res = await fetch(`${baseUrl}/api/topics/${query.slug}`);
+  const json = await res.json();
 
-	const fontsLoaded = req ? req.cookies['fonts-loaded'] : cookie('fonts-loaded');
-	return { Data: json, fontsLoaded, fullUrl};
+  const fontsLoaded = req ? req.cookies['fonts-loaded'] : cookie('fonts-loaded');
+  return { Data: json, fontsLoaded, fullUrl};
 };
 
 export default Topic;

257fe38c9767d791b8a97d7117f1704a45fd85e7
Thadee Trompetter <thadee@voorhoede.nl>
2018-11-07 12:00:09 +0100

    configure plek

---
+++ scripts/install-dependencies.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Install jmespath
+sudo wget https://github.com/jmespath/jp/releases/download/0.1.3/jp-linux-amd64 -O /usr/local/bin/jp \
+	&& sudo chmod +x /usr/local/bin/jp
+
+# install plek
+npm install plek@3;

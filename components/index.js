fa963c539a9799c609e1316a46698f8de49f3872
Bruna da Fonseca <brunadafonseca@gmail.com>
2018-11-19 12:46:11 +0100

    Feat/mailchimp (#362)
    * fix weird error
    
    * feat: create component
    
    * Clean up
    
    * Restyle component
    
    * Add specific styles
    
    * refactor: update props
    
    * style: add missing styles
    
    * fix form display and styles and clean up
    
    * Add box shadow, clean up file
    
    * refactor: import components alphabetically
    
    * feat: add thank you page

---
+++ components/index.js
@@ -0,0 +1,47 @@
+import BodyQuote from './body-quote/body-quote'
+import ButtonPrimaryLink from './buttons/button-primary/button-primary-link'
+import CallToAction from './call-to-action/call-to-action'
+import CaseExtractSmall from './case-extract-small/case-extract-small'
+import Contact from './contact/contact'
+import * as ContactShapes from './contact/contact-shapes'
+import FiftyFifty from './50-50/50-50'
+import Footer from './footer/footer'
+import FullWidthHeader from './full-width-header/full-width-header'
+import FullWidthImageSmall from './full-width-image-small/full-width-image-small'
+import InlineImage from './inline-image/inline-image'
+import Layout from './layout/layout'
+import LogoCarousel from './logo-carousel/logo-carousel'
+import MailchimpForm from './mailchimp-form/mailchimp-form'
+import MenuBar from './menu-bar/menu-bar'
+import RichBodyText from './rich-body-text/rich-body-text'
+import SocialShare from './social-share/social-share'
+import TextCenter from './text-center/text-center'
+import * as TextCenterShapes from './text-center/text-center-shapes'
+import UpdateExtractSmall from './update-extract-small/update-extract-small'
+import UpdateOverviewSmall from './update-overview-small/update-overview-small'
+import WorkOverview from './work-overview/work-overview'
+
+export {
+  BodyQuote,
+  ButtonPrimaryLink,
+  CallToAction,
+  CaseExtractSmall,
+  Contact,
+  ContactShapes,
+  FiftyFifty,
+  Footer,
+  FullWidthHeader,
+  FullWidthImageSmall,
+  InlineImage,
+  Layout,
+  LogoCarousel,
+  MailchimpForm,
+  MenuBar,
+  RichBodyText,
+  SocialShare,
+  TextCenter,
+  TextCenterShapes,
+  UpdateExtractSmall,
+  UpdateOverviewSmall,
+  WorkOverview,
+}

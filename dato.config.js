cb77df7e2c254fad5aa621d118278d8c842e59b5
bruna <brunadafonseca@gmail.com>
2018-11-19 14:07:13 +0100

    Change spaces

--- dato.config.js
+++ dato.config.js
@@ -1,115 +1,115 @@
 const { uniq } = require('lodash')
 
 module.exports = (dato, root) => {
-  root.directory('data/current', dir => {
-    dir.createDataFile('team.json', 'json', dato.team.toMap())
+	root.directory('data/current', dir => {
+		dir.createDataFile('team.json', 'json', dato.team.toMap())
 
-    dir.createDataFile('tags.json', 'json', mapCollection(dato.tags))
+		dir.createDataFile('tags.json', 'json', mapCollection(dato.tags))
 
-    dir.createDataFile('people-tab.json', 'json', dato.peopleTab.toMap())
+		dir.createDataFile('people-tab.json', 'json', dato.peopleTab.toMap())
 
-    const teamImages34 = dato.collectionsByType.teamImage34S
-    dir.createDataFile('teamImages34.json', 'json', teamImages34[0].toMap())
+		const teamImages34 = dato.collectionsByType.teamImage34S
+		dir.createDataFile('teamImages34.json', 'json', teamImages34[0].toMap())
 
-    const teamImages21 = dato.collectionsByType.teamImage21S
-    dir.createDataFile('teamImages21.json', 'json', teamImages21[0].toMap())
+		const teamImages21 = dato.collectionsByType.teamImage21S
+		dir.createDataFile('teamImages21.json', 'json', teamImages21[0].toMap())
 
-    const peopleData = dato.collectionsByType.people
-    dir.createDataFile('people.json', 'json',  mapCollection(peopleData))
+		const peopleData = dato.collectionsByType.people
+		dir.createDataFile('people.json', 'json',	mapCollection(peopleData))
 
-    dir.createDataFile(`service-overview.json`, 'json', dato.serviceOverview.toMap())
+		dir.createDataFile(`service-overview.json`, 'json', dato.serviceOverview.toMap())
 
-    dir.createDataFile(`home.json`, 'json', dato.home.toMap())
+		dir.createDataFile(`home.json`, 'json', dato.home.toMap())
 
-    dir.createDataFile('cases.json', 'json', mapCollection(dato.cases))
+		dir.createDataFile('cases.json', 'json', mapCollection(dato.cases))
 
-    dir.createDataFile('component-guide.json', 'json', dato.componentGuide.toMap())
+		dir.createDataFile('component-guide.json', 'json', dato.componentGuide.toMap())
 
-    dir.createDataFile(`contact.json`, 'json', dato.contactPage.toMap())
+		dir.createDataFile(`contact.json`, 'json', dato.contactPage.toMap())
 
-    dir.createDataFile('work.json', 'json', dato.work.toMap())
+		dir.createDataFile('work.json', 'json', dato.work.toMap())
 
-    dir.createDataFile('update-overview.json', 'json', dato.updateOverview.toMap())
+		dir.createDataFile('update-overview.json', 'json', dato.updateOverview.toMap())
 
-    dir.createDataFile('update-extracts.json', 'json', mapCollection(dato.updateExtracts))
+		dir.createDataFile('update-extracts.json', 'json', mapCollection(dato.updateExtracts))
 
-    dir.createDataFile('updates.json', 'json', mapUpdates(dato.tags, dato.updates))
+		dir.createDataFile('updates.json', 'json', mapUpdates(dato.tags, dato.updates))
 
-    dir.createDataFile('redirects.json', 'json', redirectsToJson(dato.redirects))
+		dir.createDataFile('redirects.json', 'json', redirectsToJson(dato.redirects))
 
-    dir.createDataFile('services.json', 'json', mapCollection(dato.services))
+		dir.createDataFile('services.json', 'json', mapCollection(dato.services))
 
-    dir.createDataFile('topics.json', 'json', mapCollection(dato.topics))
+		dir.createDataFile('topics.json', 'json', mapCollection(dato.topics))
 
-    dir.createDataFile('vacancies.json', 'json', mapCollection(dato.vacancies))
+		dir.createDataFile('vacancies.json', 'json', mapCollection(dato.vacancies))
 
-    dir.createDataFile('vacancies-overview.json', 'json', dato.vacancyOverview.toMap())
-  })
+		dir.createDataFile('vacancies-overview.json', 'json', dato.vacancyOverview.toMap())
+	})
 
-  function mapCollection(collection) {
-    return collection.reduce((acc,item) => {
-      const mappedData = item.toMap()
-      acc.push(mappedData)
-      return acc
-    }, [])
-  }
+	function mapCollection(collection) {
+		return collection.reduce((acc,item) => {
+			const mappedData = item.toMap()
+			acc.push(mappedData)
+			return acc
+		}, [])
+	}
 
-  function mapUpdates(tags, updates) {
-    const mappedUpdates = mapCollection(updates)
-    const mappedTags = mapCollection(tags)
-    const updatesByTag = getUpdatesByTagId(mappedTags, mappedUpdates)
+	function mapUpdates(tags, updates) {
+	const mappedUpdates = mapCollection(updates)
+	const mappedTags = mapCollection(tags)
+	const updatesByTag = getUpdatesByTagId(mappedTags, mappedUpdates)
 
-    return mappedUpdates.reduce((arr, update) => {
-      const ids = update.tags.reduce((acc, tag) => {
-        acc.push(...updatesByTag[tag.id])
-        return uniq(acc)
-      }, []).filter(updateId => updateId !== update.id)
+	return mappedUpdates.reduce((arr, update) => {
+		const ids = update.tags.reduce((acc, tag) => {
+			acc.push(...updatesByTag[tag.id])
+			return uniq(acc)
+		}, []).filter(updateId => updateId !== update.id)
 
-      const updateExtracts = ids.map(id => {
-        const data = mappedUpdates.find(item => item.id === id)
+		const updateExtracts = ids.map(id => {
+			const data = mappedUpdates.find(item => item.id === id)
 
-        return data.updateExtract
-      }).slice(0, 3)
+			return data.updateExtract
+		}).slice(0, 3)
 
-      arr.push({ ...update, relatedUpdates: updateExtracts })
+		arr.push({ ...update, relatedUpdates: updateExtracts })
 
-      return arr
-    }, [])
-  }
+		return arr
+	}, [])
+	}
 
-  function getUpdatesByTagId(tags, updates) {
-    const updatesByTag = tags.reduce((obj, tag) => {
-      const filteredUpdates = updates.filter(update => {
-        const ids = update.tags.map(item => item.id)
+	function getUpdatesByTagId(tags, updates) {
+		const updatesByTag = tags.reduce((obj, tag) => {
+			const filteredUpdates = updates.filter(update => {
+				const ids = update.tags.map(item => item.id)
 
-        if (ids.includes(tag.id)) {
-          return update
-        }
-      }).sort((a, b) => {
-        if (a.date > b.date) { return -1 }
-        if (a.date < b.date) { return 1 }
-        return 0
-      }).slice(0, 3)
+				if (ids.includes(tag.id)) {
+					return update
+				}
+			}).sort((a, b) => {
+				if (a.date > b.date) { return -1 }
+				if (a.date < b.date) { return 1 }
+				return 0
+			}).slice(0, 3)
 
-      obj[tag.id] = filteredUpdates.map(item => item.id)
+			obj[tag.id] = filteredUpdates.map(item => item.id)
 
-      return obj
-    }, {})
+			return obj
+		}, {})
 
-    return updatesByTag
-  }
+		return updatesByTag
+	}
 
-  function redirectsToJson(redirects) {
-    return redirects.reduce((urls, redirect) => {
-      const { from, to, statusCode } = redirect
+	function redirectsToJson(redirects) {
+		return redirects.reduce((urls, redirect) => {
+			const { from, to, statusCode } = redirect
 
-      return {
-        ...urls,
-        [from]: {
-          to,
-          statusCode
-        }
-      }
-    }, {})
-  }
+			return {
+			...urls,
+			[from]: {
+				to,
+				statusCode
+			}
+			}
+		}, {})
+	}
 }
--- pages/update.js
+++ pages/update.js
@@ -4,141 +4,141 @@ import "isomorphic-fetch"
 import cookie from '../components/_helpers/cookie'
 
 import {
-  Author,
-  BodyQuote,
-  CallToAction,
-  Footer,
-  FullWidthHeader,
-  FullWidthImageSmall,
-  InlineImage,
-  Layout,
-  MenuBar,
-  RichBodyText,
-  SocialShare,
-  TextCenter,
-  UpdateExtractSmall,
-  UpdateOverviewSmall,
+	Author,
+	BodyQuote,
+	CallToAction,
+	Footer,
+	FullWidthHeader,
+	FullWidthImageSmall,
+	InlineImage,
+	Layout,
+	MenuBar,
+	RichBodyText,
+	SocialShare,
+	TextCenter,
+	UpdateExtractSmall,
+	UpdateOverviewSmall,
 } from '../components'
 
 const Update = ({ Data, fontsLoaded, fullUrl }) => (
-  <Layout
-    title={`Hike One - ${Data.title}`}
-    fontsLoaded={fontsLoaded}
-    seo={Data.seo}
-    url={fullUrl}
-  >
-    <main className="main js-main">
-      <MenuBar color="white" />
+	<Layout
+		title={`Hike One - ${Data.title}`}
+		fontsLoaded={fontsLoaded}
+		seo={Data.seo}
+		url={fullUrl}
+	>
+		<main className="main js-main">
+			<MenuBar color="white" />
 
-      <article className="article">
-        <FullWidthHeader
-          headerImage={Data.headerImage.url}
-          color={Data.color.hex}
-          title={Data.title}
-          authorName={Data.authors.map(author => author.name).join(', ')}
-          updatedDate={Data.date}
-        />
+			<article className="article">
+				<FullWidthHeader
+					headerImage={Data.headerImage.url}
+					color={Data.color.hex}
+					title={Data.title}
+					authorName={Data.authors.map(author => author.name).join(', ')}
+					updatedDate={Data.date}
+				/>
 
-        {Data.content.map((component, index) => {
-          switch (component.itemType) {
-            case 'rich_body_text':
-              return <RichBodyText key={index} content={component.content} />
+				{Data.content.map((component, index) => {
+					switch (component.itemType) {
+						case 'rich_body_text':
+							return <RichBodyText key={index} content={component.content} />
 
-            case 'body_quote':
-              return <BodyQuote key={index} quote={component.quote} quotee={component.quotee} />
+						case 'body_quote':
+							return <BodyQuote key={index} quote={component.quote} quotee={component.quotee} />
 
-            case 'inline_image':
-              const image = component.image ? component.image.url : undefined
-              return (
-                <InlineImage
-                  key={index}
-                  image={image}
-                  videoControls={true}
-                  video={component.inlineVideoSrc}
-                  caption={component.caption}
-                />
-              )
+						case 'inline_image':
+							const image = component.image ? component.image.url : undefined
+							return (
+								<InlineImage
+									key={index}
+									image={image}
+									videoControls={true}
+									video={component.inlineVideoSrc}
+									caption={component.caption}
+								/>
+							)
 
-            case 'inline_image_large':
-              const imageLarge = component.image ? component.image.url : undefined
-              return (
-                <InlineImage
-                  key={index}
-                  large={true}
-                  image={imageLarge}
-                  caption={component.caption}
-                />
-              )
-            case 'full_width_image_small':
-              return (
-                <FullWidthImageSmall
-                  key={index}
-                  index={index}
-                  image={component.image.url}
-                />
-              )
-            case 'call_to_action':
-              return <CallToAction key={index} title={component.title} url={component.url} />
-          }
-        })}
+						case 'inline_image_large':
+							const imageLarge = component.image ? component.image.url : undefined
+							return (
+								<InlineImage
+									key={index}
+									large={true}
+									image={imageLarge}
+									caption={component.caption}
+								/>
+							)
+						case 'full_width_image_small':
+							return (
+								<FullWidthImageSmall
+									key={index}
+									index={index}
+									image={component.image.url}
+								/>
+							)
+						case 'call_to_action':
+							return <CallToAction key={index} title={component.title} url={component.url} />
+					}
+				})}
 
-        <SocialShare
-          facebookLink={`https://www.facebook.com/sharer/sharer.php?u=${fullUrl}`}
-          linkedinLink={`https://www.linkedin.com/shareArticle?mini=true&url=${fullUrl}&title=${Data.title}&summary=${Data.seo.description}&source=Hike&20One`}
-          twitterLink={`https://twitter.com/intent/tweet?text=${Data.title}&url=${fullUrl}`}
-        />
+				<SocialShare
+					facebookLink={`https://www.facebook.com/sharer/sharer.php?u=${fullUrl}`}
+					linkedinLink={`https://www.linkedin.com/shareArticle?mini=true&url=${fullUrl}&title=${Data.title}&summary=${Data.seo.description}&source=Hike&20One`}
+					twitterLink={`https://twitter.com/intent/tweet?text=${Data.title}&url=${fullUrl}`}
+				/>
 
-        <div className="authors">
-          {Data.authors.map((author, index) => (
-            <Author
-              key={index}
-              name={author.name}
-              role={author.role}
-              photoUrl={author.photo.url}
-              summary={author.summary}
-            />
-          ))}
-        </div>
+				<div className="authors">
+					{Data.authors.map((author, index) => (
+						<Author
+							key={index}
+							name={author.name}
+							role={author.role}
+							photoUrl={author.photo.url}
+							summary={author.summary}
+						/>
+					))}
+				</div>
 
-        <TextCenter
-          classes="text-center-font-medium text-center-spacing-small"
-          title={Data.updateLinksTitle}
-        />
+				<TextCenter
+					classes="text-center-font-medium text-center-spacing-small"
+					title={Data.updateLinksTitle}
+				/>
 
-        <UpdateOverviewSmall>
-          {Data.relatedUpdates.map((item, index) => (
-            <UpdateExtractSmall
-              key={index}
-              index={index}
-              title={item.title}
-              date={item.date}
-              authors={item.authors}
-              target={item.link}
-              image={item.image.url}
-              category={item.category.name}
-              color={item.themeColor.hex}
-              external={item.isExternalLink}
-            />
-          ))}
-        </UpdateOverviewSmall>
-      </article>
+				<UpdateOverviewSmall>
+					{Data.relatedUpdates.map((item, index) => (
+						<UpdateExtractSmall
+							key={index}
+							index={index}
+							title={item.title}
+							date={item.date}
+							authors={item.authors}
+							target={item.link}
+							image={item.image.url}
+							category={item.category.name}
+							color={item.themeColor.hex}
+							external={item.isExternalLink}
+						/>
+					))}
+				</UpdateOverviewSmall>
+			</article>
 
-      <Footer
-        callToActionLabel={Data.footer.callToActionLabel}
-        callToActionUrl={Data.footer.callToActionUrl}
-      />
-    </main>
-  </Layout>
+			<Footer
+				callToActionLabel={Data.footer.callToActionLabel}
+				callToActionUrl={Data.footer.callToActionUrl}
+			/>
+		</main>
+	</Layout>
 )
 
 Update.getInitialProps = async ({ req, query, asPath }) => {
-  const baseUrl = req ? `${req.protocol}://${req.get('Host')}` : ''
-  const fullUrl = `${baseUrl}${asPath}`
-  const res = await fetch(`${baseUrl}/api/updates/${query.slug}`)
-  const json = await res.json()
-  const fontsLoaded = req ? req.cookies['fonts-loaded'] : cookie('fonts-loaded')
+	const baseUrl = req ? `${req.protocol}://${req.get('Host')}` : ''
+	const fullUrl = `${baseUrl}${asPath}`
+	const res = await fetch(`${baseUrl}/api/updates/${query.slug}`)
+	const json = await res.json()
+	const fontsLoaded = req ? req.cookies['fonts-loaded'] : cookie('fonts-loaded')
 
-  return { Data: json, fontsLoaded, fullUrl}
+	return { Data: json, fontsLoaded, fullUrl}
 }
 
 export default Update

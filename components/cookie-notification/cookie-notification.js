1ff1b2926892689090fb717c92be0925ab93b0a2
bruna <brunadafonseca@gmail.com>
2018-12-07 09:46:20 +0100

    feat: create cookie notification

--- components/cookie-notification/cookie-notification.js
+++ components/cookie-notification/cookie-notification.js
@@ -1,11 +1,41 @@
-const CookieNotification = ({ text, callToActionLabel, callToActionUrl, saveCookie }) => (
-  <div className="cookie-notification">
-    <span>{text}</span>
-    <Link href={callToActionUrl}>
-      <a>{callToActionLabel}</a>
-    </Link>
-    <button onClick={saveCookie}>Ok</button>
-  </div>
-)
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import Link from 'next/link'
+import cookie from '../_helpers/cookie'
+
+class CookieNotification extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      accepted: false,
+    }
+  }
+  setCookie = () => {
+    cookie('accepted-cookies', true, 100)
+    this.setState({ accepted: true })
+  }
+
+  componentDidMount() {
+    const element = ReactDOM.findDOMNode(this.content);
+    console.log(element.offsetHeight)
+  }
+
+  render() {
+    const { text, callToActionLabel, callToActionUrl, button } = this.props
+
+    return (
+      <div className={`cookie-notification ${ this.state.accepted ? 'hidden' : '' }`} ref={r => this.content = r}>
+        <span>{text + ' '}</span>
+        <Link href={callToActionUrl}>
+          <a>{callToActionLabel}</a>
+        </Link>
+        <button onClick={this.setCookie}>
+          {button}
+        </button>
+      </div>
+    )
+  }
+}
 
 export default CookieNotification

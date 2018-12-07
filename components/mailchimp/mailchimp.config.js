384ab15cca0ab9b4074445414f2c195a57d4ab49
bruna <brunadafonseca@gmail.com>
2018-12-07 15:16:20 +0100

    fix: align ids with mailchimp tag fields

--- components/mailchimp/mailchimp.config.js
+++ components/mailchimp/mailchimp.config.js
@@ -1,8 +1,8 @@
 export default {
-	'company-name': { id: 'MMERGE6', type: 'text' },
+	'company-name': { id: 'COMPANY', type: 'text' },
 	'email': { id: 'EMAIL', type: 'email' },
 	'first-name': { id: 'FNAME', type: 'text' },
-	'job-title': { id: 'MMERGE5', type: 'text' },
+	'job-title': { id: 'JOBTITLE', type: 'text' },
 	'last-name': { id: 'LNAME', type: 'text' },
 	'phone-number': { id: 'PHONE', type: 'phone' },
 }

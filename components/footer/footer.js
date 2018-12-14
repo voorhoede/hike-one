63217acdae616144d08c8f588aaf812a7cfccf35
bruna <brunadafonseca@gmail.com>
2018-12-14 14:34:30 +0100

    feat: add link to privacy and cookie statement pages

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,107 +1,113 @@
-import React from 'react';
-import Link from 'next/link';
+import React from 'react'
+import Link from 'next/link'
 
-import Logo from '../logo/logo';
-import FooterLocations from '../footer-locations/footer-locations';
-import SocialMedia from '../social-media/social-media';
-import Icon from '../icon/icon';
+import { Logo, FooterLocations, SocialMedia, Icon } from '../'
 
 class Footer extends React.Component {
-  constructor() {
-    super();
-    this.ticking = false;
-    this.onResize = this.onResize.bind(this);
-    this.setFixedState = this.setFixedState.bind(this);
-  }
+	constructor() {
+		super()
+		this.ticking = false
+		this.onResize = this.onResize.bind(this)
+		this.setFixedState = this.setFixedState.bind(this)
+	}
 
-  componentDidMount() {
-    const { disableParallax } = this.props;
-    this.mainContainer = document.querySelector('.js-main');
+	componentDidMount() {
+		const { disableParallax } = this.props
+		this.mainContainer = document.querySelector('.js-main')
 
-    if (typeof window.requestAnimationFrame !== 'undefined' && this.mainContainer && !disableParallax) {
-      this.footerHeight = this.footer.getBoundingClientRect().height;
-      this.setFixedState();
-      window.addEventListener('resize', this.onResize);
-    }
-  }
+		if (typeof window.requestAnimationFrame !== 'undefined' && this.mainContainer && !disableParallax) {
+			this.footerHeight = this.footer.getBoundingClientRect().height
+			this.setFixedState()
+			window.addEventListener('resize', this.onResize)
+		}
+	}
 
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.onResize);
-  }
+	componentWillUnmount() {
+		window.removeEventListener('resize', this.onResize)
+	}
 
-  setFixedState() {
-    if (window.innerHeight > this.footerHeight) {
-      this.footer.classList.add('is-fixed');
-      this.mainContainer.style.paddingBottom = `${this.footerHeight}px`;
-    } else {
-      this.footer.classList.remove('is-fixed');
-      this.mainContainer.style.paddingBottom = `0px`;
-    }
-  }
+	setFixedState() {
+		if (window.innerHeight > this.footerHeight) {
+			this.footer.classList.add('is-fixed')
+			this.mainContainer.style.paddingBottom = `${this.footerHeight}px`
+		} else {
+			this.footer.classList.remove('is-fixed')
+			this.mainContainer.style.paddingBottom = `0px`
+		}
+	}
 
-  onResize() {
-    if (!this.ticking) {
-      window.requestAnimationFrame(() => {
-        this.setFixedState();
-        this.ticking = false;
-      });
-    }
+	onResize() {
+		if (!this.ticking) {
+			window.requestAnimationFrame(() => {
+				this.setFixedState()
+				this.ticking = false
+			})
+		}
 
-    this.ticking = true;
-  }
+		this.ticking = true
+	}
 
-  render() {
-    const { callToActionUrl, callToActionLabel } = this.props;
-    return (
-      <footer ref={node => this.footer = node} className="footer">
-        <div className="container-inner">
-          <Link href="/">
-            <a className="footer-logo">
-              <Logo color="white"/>
-              <h1 className="a11y-sr-only">Hike one</h1>
-            </a>
-          </Link>
+	render() {
+		const { callToActionUrl, callToActionLabel } = this.props
 
-          <div className="footer-main-content">
-            <div className="footer-left">
-              <div className="footer-links">
-                <ul className="footer-link-list">
-                  <li><Link href="/team?slug=culture"
-                        as="/team/culture">
-                    <a>Team</a></Link>
-                  </li>
-                  <li><Link href='/service?slug=new-product-design'
-                        as="/service/new-product-design">
-                    <a>Services</a></Link>
-                  </li>
-                  <li><Link href="/work"><a>Work</a></Link></li>
-                  <li><Link href="/contact"><a>Contact</a></Link></li>
-                  <li><Link href="/updates"><a>Updates</a></Link></li>
-                </ul>
-              </div>
-              <a className="footer-join-link" href={ callToActionUrl } target="_blank">
-                { callToActionLabel } <Icon icon="arrowRightCircle" />
-              </a>
-            </div>
+		return (
+			<footer ref={node => this.footer = node} className="footer">
+				<div className="container-inner">
+					<Link href="/">
+						<a className="footer-logo">
+							<Logo color="white"/>
+							<h1 className="a11y-sr-only">Hike one</h1>
+						</a>
+					</Link>
 
-            <div className="footer-right">
-              <div className="footer-contact">
-                <a href="tel:+31-202044577" className="footer-contact-tel">+31 20 204 45 77</a>
-                <a href="mailto:hello@hike.one" className="footer-contact-email">hello@hike.one</a>
-              </div>
+					<div className="footer-main-content">
+						<div className="footer-left">
+							<div className="footer-links">
+								<ul className="footer-link-list">
+									<li>
+										<Link href="/team?slug=culture" as="/team/culture">
+											<a>Team</a>
+										</Link>
+									</li>
+									<li>
+										<Link href='/service?slug=new-product-design' as="/service/new-product-design">
+											<a>Services</a>
+										</Link>
+									</li>
+									<li>
+										<Link href="/work"><a>Work</a></Link>
+									</li>
+									<li>
+										<Link href="/contact"><a>Contact</a></Link>
+									</li>
+									<li>
+										<Link href="/updates"><a>Updates</a></Link>
+									</li>
+								</ul>
+							</div>
+							<a className="footer-join-link" href={callToActionUrl} target="_blank">
+								{ callToActionLabel } <Icon icon="arrowRightCircle" />
+							</a>
+						</div>
 
-              <FooterLocations />
-              <SocialMedia />
-            </div>
-          </div>
+						<div className="footer-right">
+							<div className="footer-contact">
+								<a href="tel:+31-202044577" className="footer-contact-tel">+31 20 204 45 77</a>
+								<a href="mailto:hello@hike.one" className="footer-contact-email">hello@hike.one</a>
+							</div>
 
-          <p className="footer-copyright">@ Hike One 2017</p>
-          <a className="footer-copyright" href="/static/root/hike-one-privacy-statement-en.pdf">privacy statement</a>
-          <a className="footer-copyright" href="/static/root/hike-one-cookie-statement-nl.pdf">cookie statement</a>
-        </div>
-      </footer>
-    );
-  }
+							<FooterLocations />
+							<SocialMedia />
+						</div>
+					</div>
+
+					<p className="footer-copyright">@ Hike One 2017</p>
+					<a className="footer-copyright" href="/topic/privacy-statement">privacy statement</a>
+					<a className="footer-copyright" href="/topic/cookie-statement">cookie statement</a>
+				</div>
+			</footer>
+		)
+	}
 }
 
-export default Footer;
+export default Footer
--- components/index.js
+++ components/index.js
@@ -7,14 +7,18 @@ import Contact from './contact/contact'
 import * as ContactShapes from './contact/contact-shapes'
 import FiftyFifty from './50-50/50-50'
 import Footer from './footer/footer'
+import FooterLocations from './footer-locations/footer-locations'
 import FullWidthHeader from './full-width-header/full-width-header'
 import FullWidthImageSmall from './full-width-image-small/full-width-image-small'
+import Icon from './icon/icon'
 import InlineImage from './inline-image/inline-image'
 import Layout from './layout/layout'
+import Logo from './logo/logo'
 import LogoCarousel from './logo-carousel/logo-carousel'
 import MailchimpForm from './mailchimp/mailchimp-form'
 import MenuBar from './menu-bar/menu-bar'
 import RichBodyText from './rich-body-text/rich-body-text'
+import SocialMedia from './social-media/social-media'
 import SocialShare from './social-share/social-share'
 import TextCenter from './text-center/text-center'
 import * as TextCenterShapes from './text-center/text-center-shapes'
@@ -32,14 +36,18 @@ export {
 	ContactShapes,
 	FiftyFifty,
 	Footer,
+	FooterLocations,
 	FullWidthHeader,
 	FullWidthImageSmall,
+	Icon,
 	InlineImage,
 	Layout,
+	Logo,
 	LogoCarousel,
 	MailchimpForm,
 	MenuBar,
 	RichBodyText,
+	SocialMedia,
 	SocialShare,
 	TextCenter,
 	TextCenterShapes,

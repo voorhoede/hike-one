93357f10ec807b7dacd40c29005f536b7cc88695
Bruna da Fonseca <brunadafonseca@gmail.com>
2018-12-17 16:30:42 +0100

    Feat/notification bar (#371)
    * feat: create component
    
    * feat: add styling
    
    * feat: add event listeners, style for mobile
    
    * Add notification bar on top of content page
    
    * refactor: make it a functional component
    
    * refactor
    
    * refactor: clean up
    
    * refactor: remove comment, conditionally render component

--- components/icon/icon.js
+++ components/icon/icon.js
@@ -1,22 +1,22 @@
-import ArrowDown from '../icons/arrow-down';
-import ArrowDownCircle from '../icons/arrow-down-circle';
-import ArrowLeft from '../icons/arrow-left';
-import ArrowRightCircle from '../icons/arrow-right-circle';
-import ArrowRight from '../icons/arrow-right';
-import ArrowUp from '../icons/arrow-up';
-import Dribble from '../icons/dribble';
-import FaceBook from '../icons/facebook-circle';
-import Instagram from '../icons/instagram-circle';
-import Linkedin from '../icons/linkedin-circle';
-import LinkedinNoCircle from '../icons/linkedin';
-import Location from '../icons/location';
-import Medium from '../icons/medium-circle';
-import Twitter from '../icons/twitter';
-import TwitterCircle from '../icons/twitter-circle';
-import Triangle from '../icons/triangle';
+import ArrowDown from '../icons/arrow-down'
+import ArrowDownCircle from '../icons/arrow-down-circle'
+import ArrowLeft from '../icons/arrow-left'
+import ArrowRightCircle from '../icons/arrow-right-circle'
+import ArrowRight from '../icons/arrow-right'
+import ArrowUp from '../icons/arrow-up'
+import Dribble from '../icons/dribble'
+import FaceBook from '../icons/facebook-circle'
+import Instagram from '../icons/instagram-circle'
+import Linkedin from '../icons/linkedin-circle'
+import LinkedinNoCircle from '../icons/linkedin'
+import Location from '../icons/location'
+import Medium from '../icons/medium-circle'
+import Twitter from '../icons/twitter'
+import TwitterCircle from '../icons/twitter-circle'
+import Triangle from '../icons/triangle'
 import Spinner from '../icons/spinner'
 
-const Icon = ({icon = 'arrowDown', classes = ''}) => {
+const Icon = ({ icon='arrowDown', classes='' }) => {
 	const icons = {
 		arrowDown: <ArrowDown />,
 		arrowDownCircle: <ArrowDownCircle />,
@@ -35,9 +35,9 @@ const Icon = ({icon = 'arrowDown', classes = ''}) => {
 		twitterCircle: <TwitterCircle/>,
 		triangle: <Triangle />,
 		spinner: <Spinner />,
-	};
+	}
 
-	return (<span className={classes}>{icons[icon]}</span>);
-};
+	return (<span className={classes}>{icons[icon]}</span>)
+}
 
-export default Icon;
+export default Icon
--- components/index.js
+++ components/index.js
@@ -2,6 +2,7 @@ import Author from './author/author'
 import BodyQuote from './body-quote/body-quote'
 import ButtonPrimaryLink from './buttons/button-primary/button-primary-link'
 import CallToAction from './call-to-action/call-to-action'
+import CaseExtract from './case-extract/case-extract'
 import CaseExtractSmall from './case-extract-small/case-extract-small'
 import Collage from '../components/collage/collage'
 import * as CollageShapes from '../components/collage/collage-shapes'
@@ -14,6 +15,7 @@ import FullWidthHeader from './full-width-header/full-width-header'
 import FullWidthImage from '../components/full-width-image/full-width-image'
 import FullWidthImageSmall from './full-width-image-small/full-width-image-small'
 import FullWidthImageStatic from '../components/full-width-image-static/full-width-image-static'
+import Icon from './icon/icon'
 import ImageCombo from '../components/image-combo/image-combo'
 import * as ImageComboShapes from '../components/image-combo/image-combo-shapes'
 import InlineImage from './inline-image/inline-image'
@@ -22,9 +24,12 @@ import LogoCarousel from './logo-carousel/logo-carousel'
 import LogoList from '../components/logo-list/logo-list'
 import MailchimpForm from './mailchimp/mailchimp-form'
 import MenuBar from './menu-bar/menu-bar'
-import PageHeader from '../components/page-header/page-header'
+import NotificationBar from './notification-bar/notification-bar'
+import PageHeader from './page-header/page-header'
+import * as PageHeaderShapes from './page-header/page-header-shapes'
 import QuoteBlock from '../components/quote-block/quote-block'
 import RichBodyText from './rich-body-text/rich-body-text'
+import ServicesOverviewSmall from './services-overview-small/services-overview-small'
 import SocialShare from './social-share/social-share'
 import TextCard from '../components/text-card/text-card'
 import TextCenter from './text-center/text-center'
@@ -42,6 +47,7 @@ export {
 	BodyQuote,
 	ButtonPrimaryLink,
 	CallToAction,
+	CaseExtract,
 	CaseExtractSmall,
 	Collage,
 	CollageShapes,
@@ -54,6 +60,7 @@ export {
 	FullWidthImage,
 	FullWidthImageSmall,
 	FullWidthImageStatic,
+	Icon,
 	ImageCombo,
 	ImageComboShapes,
 	InlineImage,
@@ -62,9 +69,12 @@ export {
 	LogoList,
 	MailchimpForm,
 	MenuBar,
+	NotificationBar,
 	PageHeader,
+	PageHeaderShapes,
 	QuoteBlock,
 	RichBodyText,
+	ServicesOverviewSmall,
 	SocialShare,
 	TextCard,
 	TextCenter,

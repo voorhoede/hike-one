d960b0b69676ee1cf5113977b5d0ea0080509374
bruna <brunadafonseca@gmail.com>
2019-01-21 14:55:07 +0100

    feat: customised text color and width

--- components/call-to-action/call-to-action.js
+++ components/call-to-action/call-to-action.js
@@ -1,7 +1,13 @@
-const CallToAction = ({ title='', url='', bgColor, buttonText='' }) => {
+const CallToAction = ({ title='', url='', bgColor='', buttonText='', fullWidth=false, titleWhite=false }) => {
+const textColorClass = titleWhite ? 'title-white' : ''
+
 	return (
-		<div className="call-to-action container-inner" style={{ backgroundColor: bgColor }}>
-			{title && <p className="call-to-action-title">{title}</p>}
+		<div className={`call-to-action ${fullWidth ? 'container' : 'container-inner'}`} style={{ backgroundColor: bgColor }}>
+			{title &&
+				<p className={`call-to-action-title ${textColorClass}`}>
+					{title}
+				</p>
+			}
 			<a href={url} className="call-to-action-button btn-primary btn-large content" target="_blank">{buttonText}</a>
 		</div>
 	)

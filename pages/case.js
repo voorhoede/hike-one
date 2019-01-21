fead819947120b7021f5e4c0cc2b5e1e1a67c810
Bruna da Fonseca <brunadafonseca@gmail.com>
2019-01-21 13:30:59 +0100

    Feat/add video to 5050 (#388)
    * feat: add video to 5050 component

--- pages/case.js
+++ pages/case.js
@@ -146,7 +146,8 @@ const Case = ({ Data, fontsLoaded, fullUrl }) => (
 										contentLeft={component.textLeft}
 										title={component.title}
 										text={component.text}
-										image={component.image.url}
+										image={component.image && component.image.url}
+										video={component.videoSrc}
 									>
 										{ parallaxLayers }
 									</FiftyFifty>

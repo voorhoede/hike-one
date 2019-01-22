15a316063ae1f265fac3070f395a0b20f252b04e
Bruna da Fonseca <brunadafonseca@gmail.com>
2019-01-22 16:20:34 +0100

    add anchor link to subscription form (#387)
    * add anchor link to subscription form

--- components/call-to-action/call-to-action.js
+++ components/call-to-action/call-to-action.js
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 const CallToAction = ({ title='', url='', bgColor='', buttonText='', fullWidth=false, titleWhite=false }) => {
 const textColorClass = titleWhite ? 'title-white' : ''
 const containerClass = fullWidth ? 'container' : 'container-inner'
@@ -10,7 +12,9 @@ const bgClass = bgColor ? 'call-to-action-bg-color' : ''
 					{title}
 				</p>
 			}
-			<a href={url} className="call-to-action-button btn-primary btn-large content" target="_blank">{buttonText}</a>
+			<Link href={url}>
+				<a className="call-to-action-button btn-primary btn-large content">{buttonText}</a>
+			</Link>
 		</div>
 	)
 }

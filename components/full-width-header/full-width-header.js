2ba4917cf0cdd8b946009365298e4895143bafaf
bruna <brunadafonseca@gmail.com>
2019-01-23 11:57:18 +0100

    Make header height configurable via dato

--- components/full-width-header/full-width-header.js
+++ components/full-width-header/full-width-header.js
@@ -1,13 +1,14 @@
-import Icon from '../icon/icon';
-import Link from 'next/link';
-import getDateFormat from '../_helpers/getDateFormat';
-import setImageParams from '../_helpers/setImageParameters';
+import Icon from '../icon/icon'
+import Link from 'next/link'
+import getDateFormat from '../_helpers/getDateFormat'
+import setImageParams from '../_helpers/setImageParameters'
 
-const FullWidthHeader = ({headerImage='', color='', title='', authorName='', updatedDate='', titleOnly = false}) => {
-	const imageParameters = { fit: 'max', fm: 'pjpg', q: 85 };
-	const heroImageSmall = `${setImageParams(headerImage, {...imageParameters, w: 1000} )}`;
-	const heroImageMedium = `${setImageParams(headerImage, {...imageParameters, w: 1500} )}`;
-	const heroImageLarge = `${setImageParams(headerImage, {...imageParameters, w: 2000} )}`;
+const FullWidthHeader = ({ headerImage='', color='', title='', authorName='', updatedDate='', titleOnly=false, headerImageLarger=false }) => {
+	const imageParameters = { fit: 'max', fm: 'pjpg', q: 85 }
+	const heroImageSmall = `${setImageParams(headerImage, {...imageParameters, w: 1000} )}`
+	const heroImageMedium = `${setImageParams(headerImage, {...imageParameters, w: 1500} )}`
+	const heroImageLarge = `${setImageParams(headerImage, {...imageParameters, w: 2000} )}`
+	const headerImageClass = headerImageLarger ? 'image-large' : ''
 
 	const style ={__html:
 		`<style>
@@ -24,15 +25,15 @@ const FullWidthHeader = ({headerImage='', color='', title='', authorName='', upd
 					background-image: url(${heroImageLarge});
 				}
 			}
-		</style>`};
+		</style>`}
 
 	return (
 		<div className="full-width-header">
 			<div dangerouslySetInnerHTML={style}></div>
-			<div className="full-width-header-image"></div>
+			<div className={`full-width-header-image ${headerImageClass}`}></div>
 			<div className="full-width-header-banner" style={{backgroundColor:`${color}`}}>
 				<div className="full-width-header-back" style={{backgroundColor:`${ color}`}}>
-					{ !titleOnly &&
+					{!titleOnly &&
 					<Link href={`/updates`}  as={`/updates`}>
 						<a className="full-width-header-link">
 							<Icon icon="arrowLeft" classes="icon"/>
@@ -51,6 +52,8 @@ const FullWidthHeader = ({headerImage='', color='', title='', authorName='', upd
 				</div>
 				<div></div>
 			</div>
-		</div> );
-};
-export default FullWidthHeader;
+		</div>
+	)
+}
+
+export default FullWidthHeader

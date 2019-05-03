e200b9b5e260c87b7dfb5eccb220314787d6f069
Vincent van Dijck <vincent@voorhoede.nl>
2019-05-03 15:18:48 +0200

    chore(project) removed Storybook

--- .storybook/config.js
+++
@@ -1,8 +0,0 @@
-import { configure } from '@storybook/react';
-const req = require.context(`../components`, true, /\.stories\.js$/);
-
-function loadStories() {
-	req.keys().map(path => req(path));
-}
-
-configure(loadStories, module);

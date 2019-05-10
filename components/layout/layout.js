281194c05887d04373575fb89ba1f2f75d22564c
Vincent van Dijck <vincent@voorhoede.nl>
2019-05-10 13:18:47 +0200

    feat(pages) create _documents and move scripts to it

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -2,7 +2,6 @@ import 'core-js/fn/array/find'
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Head from 'next/head'
-import { GoogleTagManagerConfig } from '../'
 import loadFonts from '../_helpers/fontLoader'
 
 class Layout extends Component {
@@ -29,7 +28,8 @@ class Layout extends Component {
   }
 
   render() {
-    const { title = 'Hike One', children, seo, classes, url = 'https://hike.one' } = this.props
+    const { fontsLoaded } = this.state
+    const { title = 'Hike One', children, seo, classes = '', url = 'https://hike.one' } = this.props
     const socialImage = seo && seo.image
       ? seo.image.url
       : '/static/images/hikeone-default-social.jpg'
@@ -41,11 +41,9 @@ class Layout extends Component {
       : title
 
     return (
-      <div>
+      <React.Fragment>
         <Head>
           <title>{seoTitle}</title>
-          <meta charSet="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0, minimal-ui" />
           <meta name="msapplication-TileColor" content="#ffffff" />
           <meta name="msapplication-TileImage" content="/static/icons/mstile-150x150.png" />
           <meta name="apple-mobile-web-app-title" content="Hike One" />
@@ -65,30 +63,9 @@ class Layout extends Component {
           <link rel="apple-touch-icon" sizes="180x180" href="/static/icons/apple-touch-icon.png" />
           <link rel="icon" type="image/png" href="/static/icons/favicon-32x32.png" sizes="32x32" />
           <link rel="icon" type="image/png" href="/static/icons/favicon-16x16.png" sizes="16x16" />
-          <link rel="stylesheet" href="/static/styles/index.css" />
-          <link rel="manifest" href="/manifest.json" />
-          <GoogleTagManagerConfig />
         </Head>
-        <div className={`${this.state.fontsLoaded} ${classes}`}>{children}</div>
-        <script id="mcjs" dangerouslySetInnerHTML={{
-          __html:
-            '!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/9fcf53aac8cfc03a445bd4e2f/c7ff7fa29349202fda8890cac.js");',
-        }} />
-        <script dangerouslySetInnerHTML={{
-          __html:
-            '_linkedin_partner_id = "361124"; window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || []; window._linkedin_data_partner_ids.push(_linkedin_partner_id);',
-        }} />
-        <script dangerouslySetInnerHTML={{
-          __html:
-            '(function(){var s = document.getElementsByTagName("script")[0]; var b = document.createElement("script"); b.type = "text/javascript";b.async = true; b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js"; s.parentNode.insertBefore(b, s);})();',
-        }} />
-        <noscript>
-          <img height="1" width="1" style={{ display: 'none' }} alt="img" src="https://dc.ads.linkedin.com/collect/?pid=361124&fmt=gif" />
-        </noscript>
-        <noscript>
-          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5KJ6PKN" height="0" width="0" style={{ display: 'none', visibility: 'hidden' }} />
-        </noscript>
-      </div>
+        <div className={`${fontsLoaded} ${classes}`}>{children}</div>
+      </React.Fragment>
     )
   }
 }

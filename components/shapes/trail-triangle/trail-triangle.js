15a6504d13371c7e5e566595f953a06324a41637
Vincent van Dijck <vincent@voorhoede.nl>
2019-06-20 15:19:26 +0200

    fix(shapes) correctly align shapes vertically

--- components/shapes/trail-triangle/trail-triangle.js
+++ components/shapes/trail-triangle/trail-triangle.js
@@ -3,25 +3,22 @@ import PropTypes from 'prop-types'
 
 const TrailTraingle = ({ shadow = false }) => (
   <div className="shape">
-    <svg className="shape-trail-triangle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 399 399">
+    <svg className="shape-trail-triangle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 400">
       { shadow &&
-      <defs>
-        <filter id="shape-trail-triangle-shadow" width="160%" height="160%" x="-30%" y="-20%" filterUnits="objectBoundingBox">
-          <feOffset dy="30" in="SourceAlpha" result="shadowOffsetOuter1"/>
-          <feGaussianBlur in="shadowOffsetOuter1" result="shadowBlurOuter1" stdDeviation="25"/>
-          <feColorMatrix in="shadowBlurOuter1" result="shadowMatrixOuter1" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.2 0"/>
-          <feMerge>
-            <feMergeNode in="shadowMatrixOuter1"/>
-            <feMergeNode in="SourceGraphic"/>
-          </feMerge>
-        </filter>
-      </defs>
+        <defs>
+          <filter id="shape-trail-triangle-shadow" width="160%" height="160%" x="-30%" y="-20%" filterUnits="objectBoundingBox">
+            <feOffset dy="30" in="SourceAlpha" result="shadowOffsetOuter1"/>
+            <feGaussianBlur in="shadowOffsetOuter1" result="shadowBlurOuter1" stdDeviation="25"/>
+            <feColorMatrix in="shadowBlurOuter1" result="shadowMatrixOuter1" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.2 0"/>
+            <feMerge>
+              <feMergeNode in="shadowMatrixOuter1"/>
+              <feMergeNode in="SourceGraphic"/>
+            </feMerge>
+          </filter>
+        </defs>
       }
-      <g fill="none" fillRule="evenodd">
-        <rect className="shape-trail-triangle-bg" filter={shadow == "true" ? 'url(#shape-trail-triangle-shadow)' : ''}
-            width="299.511" height="299.824" fill="#FFE044" rx="4" transform="translate(50 20)"/>
-        <path className="shape-trail-triangle-front" fill="#8415BC" d="M304.768 249.844l-213.275-.18a1.494 1.494 0 0 1-1.276-2.27L198.31 75.718a1.494 1.494 0 0 1 2.552-.004L309.78 247.57c.43.704.21 1.623-.494 2.054-.236.144-4.242.22-4.518.22z"/>
-      </g>
+      <path className="shape-trail-triangle-bg shape-bg" filter={shadow == "true" ? 'url(#shape-trail-triangle-shadow)' : ''} d="M54.1 50h291.8c2.2 0 4 1.8 4 4v292c0 2.2-1.8 4-4 4H54.1c-2.2 0-4-1.8-4-4V54c0-2.2 1.8-4 4-4z" fill="#FFE044" />
+      <path className="shape-trail-triangle-front shape-front" fill="#8415BC" d="M304.8,287.4l-213.3-0.2c-0.8,0-1.5-0.7-1.5-1.5c0-0.3,0.1-0.5,0.2-0.8l108.1-171.7c0.4-0.7,1.3-0.9,2.1-0.5c0.2,0.1,0.4,0.3,0.5,0.5l108.9,171.9c0.4,0.7,0.2,1.6-0.5,2.1C309.1,287.3,305,287.4,304.8,287.4L304.8,287.4z" />
     </svg>
   </div>
 )

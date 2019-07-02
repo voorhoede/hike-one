cb40c72de5eacf9cc8a1c4e85483b6f8cdd0a9c3
Vincent van Dijck <vincent@voorhoede.nl>
2019-07-02 10:57:18 +0200

    chore(shapes) edit svg’s

--- components/shapes/animated/trail-diamond/trail-diamond.js
+++ components/shapes/animated/trail-diamond/trail-diamond.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const TrailDiamond = () => (
-  <svg className="shape animated-shape-trail-diamond" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 400">
-    <path className="shape-trail-diamond-bg shape-bg" fill="#fe595b" d="M54.1 50h291.8c2.2 0 4 1.8 4 4v292c0 2.2-1.8 4-4 4H54.1c-2.2 0-4-1.8-4-4V54c0-2.2 1.8-4 4-4z" />
-    <path className="shape-trail-diamond-front shape-front" fill="#00aae9" d="M304.8,287.4l-213.3-0.2c-0.8,0-1.5-0.7-1.5-1.5c0-0.3,0.1-0.5,0.2-0.8l108.1-171.7c0.4-0.7,1.3-0.9,2.1-0.5c0.2,0.1,0.4,0.3,0.5,0.5l108.9,171.9c0.4,0.7,0.2,1.6-0.5,2.1C309.1,287.3,305,287.4,304.8,287.4L304.8,287.4z" />
+  <svg className="shape animated-shape-trail-diamond" xmlns="http://www.w3.org/2000/svg" width="400" height="400" viewBox="0 0 400 400">
+    <rect className="shape-trail-diamond-bg shape-bg" width="400" height="400" fill="#fe595b"/>
+    <rect className="shape-trail-diamond-front shape-front" fill="#00aae9" x="126.1" y="126.1" width="147.7" height="147.7" transform="matrix(0.7071 0.7071 -0.7071 0.7071 200 -82.8427)" />
   </svg>
 )
 
--- components/shapes/animated/trail-double-diamond/trail-double-diamond.js
+++ components/shapes/animated/trail-double-diamond/trail-double-diamond.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
 const TrailDoubleDiamond = () => (
-  <svg className="shape animated-shape-trail-double-diamond" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 400">
-    <path className="shape-trail-double-diamond-bg shape-bg" fill="#45d33c" d="M54.1 50h291.8c2.2 0 4 1.8 4 4v292c0 2.2-1.8 4-4 4H54.1c-2.2 0-4-1.8-4-4V54c0-2.2 1.8-4 4-4z" />
-    <path className="shape-trail-double-diamond-back shape-back" fill="#ffe044" d="M164.1 125.1l72 72c1.6 1.6 1.6 4.1 0 5.7L163.8 275c-1.6 1.6-4.1 1.6-5.7 0l-72-72c-1.6-1.6-1.6-4.1 0-5.7l72.3-72.3c1.6-1.5 4.1-1.5 5.7.1z" />
-    <path className="shape-trail-double-diamond-front shape-front" fill="#fe595b" d="M241.8 125l72 72c1.6 1.6 1.6 4.1 0 5.7L241.5 275c-1.6 1.6-4.1 1.6-5.7 0l-72-72c-1.6-1.6-1.6-4.1 0-5.7l72.4-72.3c1.5-1.5 4.1-1.5 5.6 0z" />
+  <svg className="shape animated-shape-trail-double-diamond" xmlns="http://www.w3.org/2000/svg" width="400" height="400" viewBox="0 0 400 400">
+    <rect className="shape-trail-double-diamond-bg shape-bg" width="400" height="400" fill="#45d33c" />
+    <rect className="shape-trail-double-diamond-back shape-back" x="82.2" y="129.5" transform="matrix(0.7071 0.7071 -0.7071 0.7071 186.1439 -49.3912)" width="141" height="141"/>
+    <rect className="shape-trail-double-diamond-front shape-front" x="176.8" y="129.5" transform="matrix(0.7071 0.7071 -0.7071 0.7071 213.8561 -116.2942)" width="141" height="141"/>
   </svg>
 )
 

a423724d3acade3c941248ce81c1e0f8afead5bd
Vincent van Dijck <vincent@voorhoede.nl>
2019-07-02 09:17:08 +0200

    feat(services-overview-small) prepare for animations

--- components/services-overview-small/services-overview-small.js
+++ components/services-overview-small/services-overview-small.js
@@ -1,12 +1,11 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Link from 'next/link'
 import { ButtonSecondaryMock } from '../'
 
-import TrailDiamond from '../shapes/trail-diamond/trail-diamond.js'
-import TrailDoubleDiamond from '../shapes/trail-double-diamond/trail-double-diamond.js'
-import Triangle from '../shapes/triangle/triangle.js'
-import TrailTriangle from '../shapes/trail-triangle/trail-triangle.js'
+import TrailDiamond from '../shapes/animated/trail-diamond/trail-diamond.js'
+import TrailDoubleDiamond from '../shapes/animated/trail-double-diamond/trail-double-diamond.js'
+import TrailTriangle from '../shapes/animated/trail-triangle/trail-triangle.js'
 
 const shapesList = ['diamond', 'doubleDiamond', 'triangle']
 const shapes = {
@@ -15,24 +14,82 @@ const shapes = {
   doubleDiamond: TrailDoubleDiamond,
 }
 
-const ServicesOverviewSmall = ({ services = [], title = '', classes = '' }) => (
-  <div className={`services-overview-small container clearfix ${classes}`}>
-    <div className="service-overview-small-triangle">
-      <Triangle />
-    </div>
-    <div className="services-overview-small-header">{title}</div>
-    <div className="container-inner">
-      {Object.values(services).map((item, index) => {
-        const Component = shapes[shapesList[index]]
-        const hasLink = item.link && item.link.slug
+class ServicesOverviewSmall extends Component {
+  constructor(props) {
+    super(props)
+    this.onResize = this.onResize.bind(this)
+    this.onMouseOver = this.onMouseOver.bind(this)
+    this.onMouseLeave = this.onMouseLeave.bind(this)
+    this.setInitialValues = this.setInitialValues.bind(this)
+  }
 
-        return hasLink
-          ? <ServicesItemLink key={index} item={item} Component={Component} />
-          : <ServicesItem key={index} item={item} Component={Component} />
-      })}
-    </div>
-  </div>
-)
+  componentDidMount() {
+    this.setInitialValues()
+    window.addEventListener('resize', this.onResize)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.onResize)
+  }
+
+  setInitialValues() {
+    this.windowWidth = document.body.clientWidth || document.documentElement.clientWidth || 0
+  }
+
+  onMouseOver(item) {
+    console.log({ item })
+  }
+
+  onMouseLeave(item) {
+    console.log({ item })
+  }
+
+  onResize() {
+    const newWindowWidth = document.body.clientWidth || document.documentElement.clientWidth || 0
+
+    if (this.windowWidth !== newWindowWidth) {
+      clearTimeout(this.resizeTimer)
+      this.resizeTimer = setTimeout(() => {
+        this.setInitialValues()
+      }, 250)
+    }
+  }
+
+  render() {
+    const { services = [], title = '', classes = '' } = this.props
+
+    return (
+      <div className={`services-overview-small container clearfix ${classes}`}>
+        <div className="services-overview-small-header">{title}</div>
+        <div className="container-inner">
+          {Object.values(services).map((item, index) => {
+            const Component = shapes[shapesList[index]]
+            const hasLink = item.link && item.link.slug
+
+            return hasLink
+              ? <ServicesItemLink
+                  key={index}
+                  item={item}
+                  Component={Component}
+                  onMouseOver={this.onMouseOver}
+                  onMouseLeave={this.onMouseLeave} />
+              : <ServicesItem
+                  key={index}
+                  item={item}
+                  Component={Component} />
+          })}
+        </div>
+      </div>
+    )
+  }
+}
+
+ServicesOverviewSmall.propTypes = {
+  childRef: PropTypes.func,
+  services: PropTypes.array,
+  title: PropTypes.string,
+  classes: PropTypes.string,
+}
 
 const ServicesItem = ({ item = {}, Component = null }) => (
   <div className="services-item-small">
@@ -54,10 +111,19 @@ ServicesItem.propTypes = {
   Component: PropTypes.func,
 }
 
-const ServicesItemLink = ({ item = {}, Component = null }) => (
+const ServicesItemLink = ({
+  item = {},
+  Component = null,
+  onMouseOver = null,
+  onMouseLeave = null,
+}) => (
   <Link href={`/service?slug=${item.link.slug}`} as={`/service/${item.link.slug}`}>
-    <a className="services-item-small">
-      <div className={`services-item-small-shape ${item.iconColor.color}`}>
+    <a
+      id={item.link.slug}
+      className="services-item-small"
+      onMouseOver={() => onMouseOver(item.link.slug)}
+      onMouseLeave={() => onMouseLeave(item.link.slug)}>
+      <div className="services-item-small-shape">
         <Component />
       </div>
       <div className="services-item-small-content">
@@ -74,12 +140,8 @@ const ServicesItemLink = ({ item = {}, Component = null }) => (
 ServicesItemLink.propTypes = {
   item: PropTypes.object,
   Component: PropTypes.func,
-}
-
-ServicesOverviewSmall.propTypes = {
-  services: PropTypes.array,
-  title: PropTypes.string,
-  classes: PropTypes.string,
+  onMouseOver: PropTypes.func,
+  onMouseLeave: PropTypes.func,
 }
 
 export default ServicesOverviewSmall

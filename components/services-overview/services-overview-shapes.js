7a4e8a83884fd92174655ce091ee5232d7a21b94
Vincent van Dijck <vincent@voorhoede.nl>
2019-07-03 11:26:40 +0200

    feat(services-overview) adjust svg shapes

--- components/services-overview/services-overview-shapes.js
+++ components/services-overview/services-overview-shapes.js
@@ -1,23 +1,23 @@
 import React from 'react'
 
 export const TrailDiamond = () => (
-  <svg className="animated-shape" xmlns="http://www.w3.org/2000/svg" width="400" height="400" viewBox="0 0 400 400">
-    <rect className="shape-bg" width="400" height="400" fill="#fe595b"/>
-    <rect className="shape-front" fill="#00aae9" x="126.1" y="126.1" width="147.7" height="147.7" transform="matrix(0.7071 0.7071 -0.7071 0.7071 200 -82.8427)" />
+  <svg className="animated-shape diamond" xmlns="http://www.w3.org/2000/svg" width="400" height="400" viewBox="0 0 400 400">
+    <rect className="shape-bg" fill="#fe595b" width="400" height="400" />
+    <rect className="shape-front" fill="#00aae9" width="147.7" height="147.7" x="126.1" y="126.1" rx="12" ry="12" transform="matrix(0.7071 0.7071 -0.7071 0.7071 200 -82.8427)" />
   </svg>
 )
 
 export const TrailDoubleDiamond = () => (
-  <svg className="animated-shape" xmlns="http://www.w3.org/2000/svg" width="400" height="400" viewBox="0 0 400 400">
-    <rect className="shape-bg" width="400" height="400" fill="#45d33c" />
-    <rect className="shape-back" x="82.2" y="129.5" fill="#FFE044" transform="matrix(0.7071 0.7071 -0.7071 0.7071 186.1439 -49.3912)" width="141" height="141"/>
-    <rect className="shape-front" x="176.8" y="129.5" fill="#FE595B" transform="matrix(0.7071 0.7071 -0.7071 0.7071 213.8561 -116.2942)" width="141" height="141"/>
+  <svg className="animated-shape double-diamond" xmlns="http://www.w3.org/2000/svg" width="400" height="400" viewBox="0 0 400 400">
+    <rect className="shape-bg" fill="#45d33c" width="400" height="400" />
+    <rect className="shape-back" fill="#FFE044" width="141" height="141" x="82.2" y="129.5" rx="12" ry="12" transform="matrix(0.7071 0.7071 -0.7071 0.7071 186.1439 -49.3912)" />
+    <rect className="shape-front" fill="#FE595B" width="141" height="141" x="176.8" y="129.5" rx="12" ry="12" transform="matrix(0.7071 0.7071 -0.7071 0.7071 213.8561 -116.2942)" />
   </svg>
 )
 
 export const TrailTriangle = () => (
-  <svg className="animated-shape" xmlns="http://www.w3.org/2000/svg" width="400" height="400" viewBox="0 0 400 400">
-    <rect className="shape-bg" width="400" height="400" fill="#FFE044"/>
+  <svg className="animated-shape triangle" xmlns="http://www.w3.org/2000/svg" width="400" height="400" viewBox="0 0 400 400">
+    <rect className="shape-bg" fill="#FFE044" width="400" height="400" />
     <path className="shape-front" fill="#8314BB" d="M304.8,287.4l-213.3-0.2c-0.8,0-1.5-0.7-1.5-1.5c0-0.3,0.1-0.5,0.2-0.8l108.1-171.7c0.4-0.7,1.3-0.9,2.1-0.5c0.2,0.1,0.4,0.3,0.5,0.5l108.9,171.9c0.4,0.7,0.2,1.6-0.5,2.1C309.1,287.3,305,287.4,304.8,287.4L304.8,287.4z" />
   </svg>
 )

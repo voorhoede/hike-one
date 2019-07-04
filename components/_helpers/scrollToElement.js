bf6914ae60d07f0f4ee830f57e09754ea14358f8
Vincent van Dijck <vvandijck@users.noreply.github.com>
2019-07-04 13:00:40 +0200

    fix(components) gsap imports (#443)

--- components/_helpers/scrollToElement.js
+++ components/_helpers/scrollToElement.js
@@ -1,11 +1,12 @@
 import TweenLite from 'gsap'
+import 'gsap/umd/ScrollToPlugin' // Needed to enable TweenLite 'scrollTo' functionality.
 
 const scrollToElement = (elementClass) => {
   TweenLite.to(window, 0.3, {
     scrollTo: {
       y: `.${elementClass}`,
-      autoKill: false
-    }
+      autoKill: false,
+    },
   })
 }
 

4aec381df6a77c993a05c588d8b75e3279b25c5a
Vincent van Dijck <vincent@voorhoede.nl>
2019-07-05 16:27:26 +0200

    fix(service-item) single item with optional link

--- components/service-item/service-item.js
+++ components/service-item/service-item.js
@@ -4,42 +4,23 @@ import Link from 'next/link'
 import { ButtonSecondaryMock } from '..'
 
 const ServiceItem = ({ item = {}, Component = null, onMouseOver = null, onMouseLeave = null }) => {
-  const hasLink = item.link && item.link.slug
+  const { button, iconColor, link, text, title } = item
 
-  return hasLink
-    ? <WithLink {...item} Component={Component} onMouseOver={onMouseOver} onMouseLeave={onMouseLeave} />
-    : <Item {...item} Component={Component} />
-}
-
-ServiceItem.propTypes = {
-  item: PropTypes.object,
-  Component: PropTypes.func,
-  onMouseOver: PropTypes.func,
-  onMouseLeave: PropTypes.func,
-}
-
-const WithLink = ({
-  button = '',
-  iconColor = '',
-  link = '',
-  text = '',
-  title = '',
-  Component = null,
-  onMouseOver = null,
-  onMouseLeave = null,
-}) => (
-  <Link href={`/service?slug=${link.slug}`} as={`/service/${link.slug}`}>
-    <a
-      id={link.slug}
-      className="service-item"
-      onMouseOver={() => onMouseOver(link.slug)}
-      onMouseLeave={() => onMouseLeave(link.slug)}>
+  return (
+    <div className="service-item">
       <div className="service-item__icon">
         <div className={`service-item__icon-square ${iconColor.color}`}></div>
         <Component />
       </div>
       <div className="service-item__content">
-        <h3 className="service-item__content-title">{title}</h3>
+        { item.link && item.link.slug
+          ? <Link href={`/service?slug=${link.slug}`} as={`/service/${link.slug}`}>
+            <a id={link.slug} onMouseOver={() => onMouseOver(link.slug)} onMouseLeave={() => onMouseLeave(link.slug)}>
+              <h3 className="service-item__content-title">{title}</h3>
+            </a>
+          </Link>
+          : <h3 className="service-item__content-title">{title}</h3>
+        }
         <p className="service-item__content-text">{text}</p>
         <ButtonSecondaryMock
           icon="arrowRight"
@@ -47,51 +28,15 @@ const WithLink = ({
           {button}
         </ButtonSecondaryMock>
       </div>
-    </a>
-  </Link>
-)
+    </div>
+  )
+}
 
-WithLink.propTypes = {
-  button: PropTypes.string,
-  iconColor: PropTypes.object,
-  link: PropTypes.object,
-  text: PropTypes.string,
-  title: PropTypes.string,
+ServiceItem.propTypes = {
+  item: PropTypes.object,
   Component: PropTypes.func,
   onMouseOver: PropTypes.func,
   onMouseLeave: PropTypes.func,
-}
-
-const Item = ({
-  button = '',
-  iconColor = '',
-  text = '',
-  title = '',
-  Component = null
-}) => (
-  <div className="service-item">
-    <div className="service-item__icon">
-      <div className={`service-item__icon-square ${iconColor.color}`}></div>
-      <Component />
-    </div>
-    <div className="service-item__content">
-      <h3 className="service-item__content-title">{title}</h3>
-      <p className="service-item__content-text">{text}</p>
-      <ButtonSecondaryMock
-        icon="arrowRight"
-        classes="btn-red-border btn-wide">
-        {button}
-      </ButtonSecondaryMock>
-    </div>
-  </div>
-)
-
-Item.propTypes = {
-  button: PropTypes.string,
-  iconColor: PropTypes.object,
-  text: PropTypes.string,
-  title: PropTypes.string,
-  Component: PropTypes.func,
 }
 
 export default ServiceItem

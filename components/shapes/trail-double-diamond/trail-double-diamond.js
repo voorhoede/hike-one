fb25fd3b3e804bda6deb149f4ebdb88c961cf522
Vincent van Dijck <vvandijck@users.noreply.github.com>
2019-07-09 15:26:51 +0200

    Feature/424 home page services conversion upgrade (#439)
    * feat(components) export button mocks
    
    * fix(shapes) correctly align shapes vertically
    
    * feat(services-overview-small) each block is a link
    
    * feat(services-overview-small) new hover animation
    
    * fix(services-overview-small) check for item link
    
    * fix(services-overview-small) move check to const
    
    * feat(services-overview-small) tweak animation
    
    * fix(services-overview-small) fix styling on larger screens
    
    * fix(services-overview-small) decrease margin
    
    * fix(menu-bar) renamed parameter
    
    * feat(services-overview-small) prepare for animations
    
    * chore(shapes) edit svg’s
    
    * feat(services-overview-small) css only hover animation
    
    * chore(components) refactored service item and overview
    
    * feat(service-item) animation transitions
    
    * feat(services-overview) adjust svg shapes
    
    * feat(service-item) icon vertical offset + rounded corners
    
    * fix(service-item) speed-up transition
    
    * feat(service-item) add delays to transitions
    
    * feat(services-overview) add GSAP hover animations
    
    * chore(services-overview) removed old unused code
    
    * fix(services-overview) remove parentesis
    
    * chore(services-overview) put back parentesis
    
    * fix(service-item) single item with optional link

--- components/shapes/trail-double-diamond/trail-double-diamond.js
+++ components/shapes/trail-double-diamond/trail-double-diamond.js
@@ -3,28 +3,23 @@ import PropTypes from 'prop-types'
 
 const TrailDoubleDiamond = ({ shadow = false }) => (
   <div className="shape">
-    <svg className="shape-trail-double-diamond" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 399 399">
+    <svg className="shape-trail-double-diamond" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 400">
       { shadow &&
         <defs>
           <filter id="shape-trail-double-diamond-shadow" width="160%" height="160%" x="-30%" y="-20%" filterUnits="objectBoundingBox">
-            <feOffset dy="30" in="SourceAlpha" result="shadowOffsetOuter1"/>
-            <feGaussianBlur in="shadowOffsetOuter1" result="shadowBlurOuter1" stdDeviation="25"/>
-            <feColorMatrix in="shadowBlurOuter1" result="shadowMatrixOuter1" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.2 0"/>
-            <feMerge>
-              <feMergeNode in="shadowMatrixOuter1"/>
-              <feMergeNode in="SourceGraphic"/>
-            </feMerge>
+          <feOffset dy="30" in="SourceAlpha" result="shadowOffsetOuter1"/>
+          <feGaussianBlur in="shadowOffsetOuter1" result="shadowBlurOuter1" stdDeviation="25"/>
+          <feColorMatrix in="shadowBlurOuter1" result="shadowMatrixOuter1" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.2 0"/>
+          <feMerge>
+            <feMergeNode in="shadowMatrixOuter1"/>
+            <feMergeNode in="SourceGraphic"/>
+          </feMerge>
           </filter>
         </defs>
       }
-      <g filter={shadow ? 'url(#shape-trail-double-diamond-shadow)' : ''}
-        fill="none" fillRule="evenodd"  transform="translate(50 20)">
-        <rect className="shape-trail-double-diamond-bg" width="299.511" height="299.824" fill="#45D33C" rx="4"/>
-        <g transform="translate(33 71)">
-          <rect className="shape-trail-double-diamond-back" width="109.818" height="110.183" x="23.053" y="23.337" fill="#FFE044" rx="4" transform="rotate(45 77.961 78.429)"/>
-          <rect className="shape-trail-double-diamond-front" width="109.818" height="110.183" x="100.772" y="23.271" fill="#FE595B" rx="4" transform="rotate(45 155.68 78.362)"/>
-        </g>
-      </g>
+      <path className="shape-trail-double-diamond-bg shape-bg" filter={shadow ? 'url(#shape-trail-double-diamond-shadow)' : ''} d="M54.1 50h291.8c2.2 0 4 1.8 4 4v292c0 2.2-1.8 4-4 4H54.1c-2.2 0-4-1.8-4-4V54c0-2.2 1.8-4 4-4z" />
+      <path className="shape-trail-double-diamond-back shape-back" d="M164.1 125.1l72 72c1.6 1.6 1.6 4.1 0 5.7L163.8 275c-1.6 1.6-4.1 1.6-5.7 0l-72-72c-1.6-1.6-1.6-4.1 0-5.7l72.3-72.3c1.6-1.5 4.1-1.5 5.7.1z" fill="#ffe044"/>
+      <path className="shape-trail-double-diamond-front shape-front" d="M241.8 125l72 72c1.6 1.6 1.6 4.1 0 5.7L241.5 275c-1.6 1.6-4.1 1.6-5.7 0l-72-72c-1.6-1.6-1.6-4.1 0-5.7l72.4-72.3c1.5-1.5 4.1-1.5 5.6 0z" fill="#fe595b"/>
     </svg>
   </div>
 )

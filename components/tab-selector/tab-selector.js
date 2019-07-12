d54f0514882ca130b4d9d0be5088e8aac257f185
Vincent van Dijck <vincent@voorhoede.nl>
2019-07-12 16:14:23 +0200

    fix(tab-selector) change tab color

--- components/tab-selector/tab-selector.js
+++ components/tab-selector/tab-selector.js
@@ -1,32 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Link from 'next/link'
-<<<<<<< HEAD
-import { TrailDiamond, TrailDoubleDiamond, TrailTriangle } from '../'
-
-const TabSelector = ({ services = [], selectedItem = '' }) => (
-  <div className="tab-selector container shadow">
-    <Link href={`/service?slug=${services[0].slug}`} as={`/service/${services[0].slug}`} prefetch>
-      <a className={`tab-selector-item tab-selector-item-red ${selectedItem === `${services[0].slug}` ? 'is-selected' : ''}`}>
-        <div className="tab-selector-item-shape">
-          <TrailDiamond />
-        </div>
-        <h2 className="tab-selector-item-title">{services[0].title}</h2>
-      </a>
-    </Link>
-    <Link href={`/service?slug=${services[1].slug}`} as={`/service/${services[1].slug}`} prefetch>
-      <a className={`tab-selector-item tab-selector-item-green ${selectedItem === `${services[1].slug}` ? 'is-selected' : ''}`}>
-        <div className="tab-selector-item-shape">
-          <TrailDoubleDiamond />
-        </div>
-        <h2 className="tab-selector-item-title">{services[1].title}</h2>
-      </a>
-    </Link>
-    <Link href={`/service?slug=${services[2].slug}`} as={`/service/${services[2].slug}`} prefetch>
-      <a className={`tab-selector-item tab-selector-item-purple ${selectedItem === `${services[2].slug}` ? 'is-selected' : ''}`}>
-        <div className="tab-selector-item-shape">
-          <TrailTriangle />
-=======
 import { TrailDiamond, TrailDoubleDiamond, TrailTriangle } from './tab-selector-shapes'
 
 const shapes = [
@@ -58,7 +32,7 @@ class TabSelector extends Component {
   render() {
     const { services = [], selectedItem = '' } = this.props
     const { selectedTab } = this.state
-    const color = [ '#fe595b', '#45d33c', '#ffe044' ]
+    const color = [ '#fe595b', '#45d33c', '#8314bb' ]
     const style = {
       transform: `translateX(${selectedTab * 100}%)`,
       backgroundColor: color[selectedTab],
@@ -77,7 +51,6 @@ class TabSelector extends Component {
         ))}
         <div className="tab-selector__track">
           <span className="tab-selector__track-slider" style={style}></span>
->>>>>>> master
         </div>
       </div>
     )

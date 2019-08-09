b11987416883d4bd0cac71b95f17365037dd6722
Vincent van Dijck <vincent@voorhoede.nl>
2019-08-09 15:13:42 +0200

    feat(page-header-new) finish animations

--- components/page-header-new/page-header-new-animations.js
+++ components/page-header-new/page-header-new-animations.js
@@ -32,10 +32,10 @@ export function timelineDoubleDiamond() {
       { y: 500 },
       { y: 0, ease: Power2.easeIn })
     .fromTo('.animated-shape-double-diamond .shape-back', .9,
-      { x: '70vw', transformOrigin: 'center' },
+      { x: '100vw', transformOrigin: 'center' },
       { x: 0 }, '-=.1')
     .fromTo('.animated-shape-double-diamond .shape-front', .9,
-      { x: '-70vw', transformOrigin: 'center' },
+      { x: '-100vw', transformOrigin: 'center' },
       { x: 0 }, '-=.9')
     .fromTo('.animated-shape-double-diamond .shape-back', 1.1,
       { scale: 5, transformOrigin: 'center' },
@@ -54,5 +54,116 @@ export function timelineDoubleDiamond() {
 export function timelineTriangles() {
   return new TimelineLite()
     .pause()
-    .to('.animated-shape-traingles .shape-back', 3, { opacity: 0 })
+    .add('startAnimation')
+    .fromTo('.page-header-new__animation-background', .5,
+      { y: 500 },
+      { y: 0, ease: Power2.easeIn })
+    .fromTo('.shape-arrow-14', 1.2,
+      { y: 400 },
+      { y: 0, ease: Power2.easeInOut }, 'startAnimation')
+    .fromTo('.shape-arrow-8', 1,
+      { y: 300 },
+      { y: 0, ease: Power2.easeInOut }, 'startAnimation')
+    .fromTo('.shape-arrow-17', 1.2,
+      { y: 300 },
+      { y: 0, ease: Power2.easeInOut }, 'startAnimation')
+    .fromTo('.shape-arrow-13', 1.3,
+      { y: 300 },
+      { y: 0, ease: Power2.easeInOut }, 'startAnimation')
+    .fromTo('.shape-arrow-15', 1,
+      { y: 150 },
+      { y: 0, ease: Power2.easeInOut }, 'startAnimation')
+    .fromTo('.shape-arrow-10', 1.4,
+      { y: 200 },
+      { y: 0, ease: Power2.easeInOut }, 'startAnimation')
+    .fromTo('.shape-arrow-16', 1.6,
+      { y: 300 },
+      { y: 0, ease: Power2.easeInOut }, 'startAnimation')
+    .fromTo('.shape-arrow-11', 1.6,
+      { y: 350 },
+      { y: 0, ease: Power2.easeInOut }, 'startAnimation')
+    .fromTo('.shape-arrow-12', 1.5,
+      { y: 300 },
+      { y: 0, ease: Power2.easeInOut }, 'startAnimation')
+    .fromTo('.shape-arrow-1', 1.8,
+      { y: 250 },
+      { y: 0, ease: Power2.easeInOut }, 'startAnimation')
+    .fromTo('.shape-arrow-6', 2,
+      { y: 150 },
+      { y: 0, ease: Power2.easeInOut }, 'startAnimation')
+    .fromTo('.shape-arrow-4', 1.8,
+      { y: 350 },
+      { y: 0, ease: Power2.easeInOut }, 'startAnimation')
+    .fromTo('.shape-arrow-5', 1.8,
+      { y: 350 },
+      { y: 0, ease: Power2.easeInOut }, 'startAnimation')
+    .fromTo('.shape-arrow-3', 2,
+      { y: 300 },
+      { y: 0, ease: Power2.easeInOut }, 'startAnimation')
+    .fromTo('.shape-arrow-2', 2.1,
+      { y: 200 },
+      { y: 0, ease: Power2.easeInOut }, 'startAnimation')
+    .fromTo('.shape-arrow-7', 2.1,
+      { y: 150 },
+      { y: 0, ease: Power2.easeInOut }, 'startAnimation')
+    .fromTo('.shape-arrow-9', 2.2,
+      { y: 150 },
+      { y: 0, ease: Power2.easeInOut }, 'startAnimation')
+    .fromTo('.shape-arrow-14', .8,
+      { scale: 0.1, transformOrigin: 'center' },
+      { scale: 1 }, '-=1.4')
+    .fromTo('.shape-arrow-8', .8,
+      { scale: .5, transformOrigin: 'center' },
+      { scale: 1 }, '-=1.6')
+    .fromTo('.shape-arrow-17', .8,
+      { scale: .1, transformOrigin: 'center' },
+      { scale: 1 }, '-=1.4')
+    .fromTo('.shape-arrow-13', .8,
+      { scale: .1, transformOrigin: 'center' },
+      { scale: 1 }, '-=1.5')
+    .fromTo('.shape-arrow-15', 1,
+      { scale: .5, transformOrigin: 'center' },
+      { scale: 1 }, '-=1.5')
+    .fromTo('.shape-arrow-10', .8,
+      { scale: .5, transformOrigin: 'center' },
+      { scale: 1 }, '-=1.4')
+    .fromTo('.shape-arrow-16', .8,
+      { scale: .2, transformOrigin: 'center' },
+      { scale: 1 }, '-=1.1')
+    .fromTo('.shape-arrow-11', .8,
+      { scale: 0.05, transformOrigin: 'center' },
+      { scale: 1 }, '-=.8')
+    .fromTo('.shape-arrow-12', .8,
+      { scale: 0.05, transformOrigin: 'center' },
+      { scale: 1 }, '-=1.2')
+    .fromTo('.shape-arrow-1', .8,
+      { scale: .3, transformOrigin: 'center' },
+      { scale: 1 }, '-=1.4')
+    .fromTo('.shape-arrow-6', .8,
+      { scale: .2, transformOrigin: 'center' },
+      { scale: 1 }, '-=1.1')
+    .fromTo('.shape-arrow-4', .8,
+      { scale: .05, transformOrigin: 'center' },
+      { scale: 1 }, '-=1.1')
+    .fromTo('.shape-arrow-5', .8,
+      { scale: .05, transformOrigin: 'center' },
+      { scale: 1 }, '-=1.1')
+    .fromTo('.shape-arrow-3', .8,
+      { scale: .05, transformOrigin: 'center' },
+      { scale: 1 }, '-=1.1')
+    .fromTo('.shape-arrow-2', .8,
+      { scale: .5, transformOrigin: 'center' },
+      { scale: 1 }, '-=1.1')
+    .fromTo('.shape-arrow-7', .8,
+      { scale: .5, transformOrigin: 'center' },
+      { scale: 1 }, '-=1.2')
+    .fromTo('.shape-arrow-9', .8,
+      { scale: .5, transformOrigin: 'center' },
+      { scale: 1 }, '-=.9')
+    .fromTo('.page-header-new__title', .8,
+      { opacity: 0, transformOrigin: 'center' },
+      { opacity: 1, ease: Power3.easeIn }, '-=1.4')
+    .fromTo('.page-header-new__title', .8,
+      { y: 40 },
+      { y: 0, ease: Power3.easeOut }, '-=1.2')
 }

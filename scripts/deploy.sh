a3e297c590a803b6a226329be1f60c96dbd7d1c7
Thadee Trompetter <thadee@voorhoede.nl>
2019-08-13 17:32:40 +0200

    try different production alias

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -26,7 +26,8 @@ disallow_robots () {
 }
 
 # Production deployment: a new tag
-if [[ "$TRAVIS_BRANCH" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+if [ "$TRAVIS_BRANCH" == 'chore/zeit-now-v2' ]; then
+# if [[ "$TRAVIS_BRANCH" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
   echo 'Build production environment';
   deployment='production';
   webhook_url=$(get_webhook_url "$DATO_ENV_ID_PRODUCTION");
@@ -39,8 +40,7 @@ if [[ "$TRAVIS_BRANCH" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     -e ENVIRONMENT="$deployment";
 
 # Staging is deployed from master, pull requests targeting master are ignored
-elif [ "$TRAVIS_BRANCH" == 'chore/zeit-now-v2' ]; then
-# elif [ "$TRAVIS_BRANCH" == 'master' ] && [ -z "$TRAVIS_PULL_REQUEST_BRANCH" ]; then
+elif [ "$TRAVIS_BRANCH" == 'master' ] && [ -z "$TRAVIS_PULL_REQUEST_BRANCH" ]; then
   echo 'Build staging environment';
   deployment='staging';
   disallow_robots;

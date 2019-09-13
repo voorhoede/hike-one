14fabe6ec68aaa1392e97becc57bc483477f5b2c
Vincent van Dijck <vincent@voorhoede.nl>
2019-09-13 14:34:37 +0200

    fix(social-share) move url’s to component

--- components/social-share/social-share.js
+++ components/social-share/social-share.js
@@ -4,24 +4,24 @@ import Facebook from '../icons/facebook-circle'
 import Twitter from '../icons/twitter-circle'
 import LinkedIn from '../icons/linkedin-circle'
 
-const SocialShare = ({ facebookLink = '', linkedinLink = '', twitterLink = '' }) => (
+const SocialShare = ({ description = '', title = '', url = '' }) => (
   <div className="social-share">
-    <a href={facebookLink} className="social-share-item" target="_blank" rel="noopener noreferrer">
+    <a href={`https://www.facebook.com/sharer/sharer.php?u=${url}`} className="social-share-item" target="_blank" rel="noopener noreferrer">
       <Facebook color="facebook-blue" />
     </a>
-    <a href={twitterLink} className="social-share-item" target="_blank" rel="noopener noreferrer">
+    <a href={`https://twitter.com/intent/tweet?text=${title}&url=${url}`} className="social-share-item" target="_blank" rel="noopener noreferrer">
       <Twitter color="twitter-blue" />
     </a>
-    <a href={linkedinLink} className="social-share-item" target="_blank" rel="noopener noreferrer">
+    <a href={`https://www.linkedin.com/shareArticle?mini=true&url=${url}&title=${title}&summary=${description}&source=Hike&20One`} className="social-share-item" target="_blank" rel="noopener noreferrer">
       <LinkedIn color="linkedin-blue" />
     </a>
   </div>
 )
 
 SocialShare.propTypes = {
-  facebookLink: PropTypes.string,
-  linkedinLink: PropTypes.string,
-  twitterLink: PropTypes.string,
+  description: PropTypes.string,
+  title: PropTypes.string,
+  url: PropTypes.string,
 }
 
 export default SocialShare

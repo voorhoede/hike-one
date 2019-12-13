d7a1d754d5b8eaa978c4355b041b2b6711880f81
James Peter Perrone Jefferies <james.p.perrone@protonmail.com>
2019-12-13 16:12:27 +0100

    📦 NEW: Redesign the contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import getData from '../lib/get-data'
-import cookie from '../components/_helpers/cookie'
+import React from 'react';
+import PropTypes from 'prop-types';
+import getData from '../lib/get-data';
+import cookie from '../components/_helpers/cookie';
 import {
   ContactForm,
   Footer,
@@ -10,22 +10,35 @@ import {
   OfficeCard,
   OfficeOverview,
   PageHeader,
-  TextCenter,
-  TextCenterShapes,
-  VacancyCard,
-} from '../components'
+  VacancyOverview
+} from '../components';
 
-const Contact = ({ data = {}, footer = {}, fontsLoaded = '', fullUrl = '' }) => (
+let scrapeJobs;
+
+if (!process.browser) {
+  scrapeJobs = require('../lib/job-scraper/server');
+} else {
+  scrapeJobs = require('../lib/job-scraper/browser');
+}
+
+const Contact = ({
+  data = {},
+  footer = {},
+  fontsLoaded = '',
+  fullUrl = '',
+  vacanciesOverview = {},
+  vacancies = []
+}) => (
   <Layout
-    title="Hike One - Contact"
+    title='Hike One - Contact'
     fontsLoaded={fontsLoaded}
     seo={data.seo}
-    url={fullUrl}>
-    <main className="main js-main">
+    url={fullUrl}
+  >
+    <main className='main js-main contact-page'>
+      <MenuBar color='white' />
 
-      <MenuBar color="white" />
-
-      <article className="article">
+      <article className='article'>
         <PageHeader
           isSmall={true}
           title={data.header.title}
@@ -33,15 +46,18 @@ const Contact = ({ data = {}, footer = {}, fontsLoaded = '', fullUrl = '' }) => 
           image={data.header.backgroundImage.url}
         />
 
-        <div className="page-scrolling-content-small">
-          <ContactForm form={data.contactForm} />
-
-          <TextCenter classes="text-center-font-large contact-text-center" text={data.content}>
-            <TextCenterShapes.variation2Back position="back" />
-          </TextCenter>
-
-          <VacancyCard data={data.vacancyCard} />
-
+        <div className='page-scrolling-content-small'>
+          <div className="contact-details container">
+            <h2>Contact us at</h2>
+            <a className="contact-details__tel" href="tel:+31-202044577">+31 20 204 45 77</a>
+            <a className="contact-details__mail" href="mailto:hello@hike.one">hello@hike.one</a>
+          </div>
+          <ContactForm
+            form={data.contactForm}
+            showBody={false}
+            singleForm={true}
+          />
+          <VacancyOverview overview={vacanciesOverview} vacancies={vacancies} />
           <OfficeOverview header={data.officesHeader}>
             {data.office.map((item, index) => (
               <OfficeCard
@@ -61,27 +77,47 @@ const Contact = ({ data = {}, footer = {}, fontsLoaded = '', fullUrl = '' }) => 
       </article>
 
       <Footer form={footer.form} />
-
     </main>
   </Layout>
-)
+);
 
 Contact.getInitialProps = async ({ req, res, asPath }) => {
-  const baseUrl = req ? `${req.protocol}://${req.get('Host')}` : ''
-  const fontsLoaded = req ? req.cookies['fonts-loaded'] : cookie('fonts-loaded')
-  const fullUrl = `${baseUrl}${asPath}`
-  const fetchJson = model => getData(baseUrl, model, res)
-  const fetchAll = models => Promise.all(models.map(fetchJson))
-  const [footer, data] = await fetchAll(['footer', 'contact'])
+  const baseUrl = req ? `${req.protocol}://${req.get('Host')}` : '';
+  const fontsLoaded = req
+    ? req.cookies['fonts-loaded']
+    : cookie('fonts-loaded');
+  const fullUrl = `${baseUrl}${asPath}`;
+  const fetchJson = model => getData(baseUrl, model, res);
+  const fetchAll = models => Promise.all(models.map(fetchJson));
+  const [footer, data, vacanciesOverview] = await fetchAll([
+    'footer',
+    'contact',
+    'vacancies-overview'
+  ]);
 
-  return { data, footer, fontsLoaded, fullUrl }
-}
+  const vacancies = await fetch(
+    `https://homerun.co/embed/ahz3le8c0dl4ivfruo0n/widget.html?t=${Date.now()}`
+  )
+    .then(response => response.text())
+    .then(await scrapeJobs);
+
+  return {
+    data,
+    fontsLoaded,
+    footer,
+    fullUrl,
+    vacancies,
+    vacanciesOverview
+  };
+};
 
 Contact.propTypes = {
-  data: PropTypes.object,
-  footer: PropTypes.object,
+  data: PropTypes,
   fontsLoaded: PropTypes.string,
+  footer: PropTypes.object,
   fullUrl: PropTypes.string,
-}
+  vacancies: PropTypes.array,
+  vacanciesOverview: PropTypes.object
+};
 
-export default Contact
+export default Contact;

0a4be4a90f6a734f5d61e91d651e9dcf9745f059
Selwyn <talk@selwyn.cc>
2020-01-20 16:53:46 +0100

    Add generic thank you page

---
+++ pages/thank-you.js
@@ -0,0 +1,62 @@
+import '../styles/index.less';
+
+import fetchContent from '../lib/fetch-content';
+import withCacheControl from '../lib/with-cache-control';
+
+import Head from '../components/head/head';
+import MenuBar from '../components/menu-bar/menu-bar';
+import TextCenter from '../components/text-center/text-center';
+import * as TextCenterShapes from '../components/text-center/text-center-shapes';
+import ButtonPrimaryLink from '../components/buttons/button-primary/button-primary';
+import Footer from '../components/footer/footer';
+
+const Page = ({ thankYouPage, footer }) => (
+	<>
+		<Head title="Hike One - Thank you" />
+
+		<MenuBar color="black" />
+
+		<article className="article article-thank-you">
+			<TextCenter title={thankYouPage.title} text={thankYouPage.content}>
+				<TextCenterShapes.variation3Back position="back" />
+				<TextCenterShapes.variation4Front position="front" />
+			</TextCenter>
+
+			<ButtonPrimaryLink href={thankYouPage.callToActionUrl} classes="btn btn-large btn-centered">
+				{thankYouPage.callToActionLabel}
+			</ButtonPrimaryLink>
+		</article>
+
+		<Footer form={footer.form} disableParallax />
+	</>
+);
+
+Page.getInitialProps = withCacheControl(({ req }) => {
+	const graphqlQuery = `{
+    thankYouPage {
+      title
+      content
+      callToActionUrl
+      callToActionLabel
+    }
+
+		footer {
+			form {
+				title
+				description
+				listId
+				button
+				hasShadow
+				extraInputFields {
+					label
+					inputType
+					required
+				}
+			}
+		}
+	}`;
+
+	return fetchContent({ graphqlQuery, req });
+});
+
+export default Page;

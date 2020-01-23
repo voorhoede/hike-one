9794b1d259f24dc09d474ae333ab034c16da40ab
Vincent van Dijck <vincent@voorhoede.nl>
2020-01-23 09:56:10 +0100

    fix(authors) removed brackets

--- components/authors/authors.js
+++ components/authors/authors.js
@@ -1,8 +1,6 @@
 import PropTypes from 'prop-types';
 
-const Authors = ({ authors = [] }) => {
-	return <span>{authors.map(author => author.name).join(', ')}</span>;
-};
+const Authors = ({ authors = [] }) => <span>{authors.map(author => author.name).join(', ')}</span>;
 
 Authors.propTypes = {
 	authors: PropTypes.array,

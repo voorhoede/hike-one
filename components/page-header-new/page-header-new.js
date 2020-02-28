c056777447d7d7316816bf1763dd717481bb4988
James Peter Perrone Jefferies <james.p.perrone@protonmail.com>
2020-02-28 16:10:15 +0100

    Feat(page-header-new) Basic animation

--- components/page-header-new/page-header-new.js
+++ components/page-header-new/page-header-new.js
@@ -23,35 +23,39 @@ class PageHeaderNew extends Component {
 
 	componentDidMount() {
 		const { animation } = this.props;
-		const animationByName = {
-			diamond: timelineDiamond(),
-			doubleDiamond: timelineDoubleDiamond(),
-			triangles: timelineTriangles(),
-		};
+		if (animation && animation !== 'basic') {
+			const animationByName = {
+				diamond: timelineDiamond(),
+				doubleDiamond: timelineDoubleDiamond(),
+				triangles: timelineTriangles(),
+			};
 
-		this.timeline = animationByName[animation];
-		this.timeline.timeScale(1).play();
+			this.timeline = animationByName[animation];
+			this.timeline.timeScale(1).play();
+		}
 	}
 
 	componentDidUpdate(prevProps, prevState) {
 		const { animation } = this.props;
-		const animationByName = {
-			diamond: timelineDiamond(),
-			doubleDiamond: timelineDoubleDiamond(),
-			triangles: timelineTriangles(),
-		};
+		if (animation && animation !== 'basic') {
+			const animationByName = {
+				diamond: timelineDiamond(),
+				doubleDiamond: timelineDoubleDiamond(),
+				triangles: timelineTriangles(),
+			};
 
-		if (prevProps.animation !== animation) {
-			this.setState({ currentColor: this.colorByName[animation] });
+			if (prevProps.animation !== animation) {
+				this.setState({ currentColor: this.colorByName[animation] });
+			}
+
+			this.timeline.pause();
+			this.timeline = animationByName[animation];
+			this.timeline.timeScale(1).play();
 		}
-
-		this.timeline.pause();
-		this.timeline = animationByName[animation];
-		this.timeline.timeScale(1).play();
 	}
 
 	render() {
-		const { animation, title } = this.props;
+		const { animation, animationBackgroundColor, animationTriangleColor, title } = this.props;
 		const { currentColor } = this.state;
 		const shapesByName = {
 			diamond: ShapesDiamond,
@@ -61,10 +65,17 @@ class PageHeaderNew extends Component {
 
 		return (
 			<section className="page-header-new" style={{ backgroundColor: currentColor }}>
-				<PageHeaderNewAnimation
-					Component={shapesByName[animation]}
-					color={this.colorByName[animation]}
-				/>
+				{animation === 'basic' ? (
+					<PageHeaderNewAnimationBasic
+						animationTriangleColor={animationTriangleColor.hex}
+						animationBackgroundColor={animationBackgroundColor.hex}
+					/>
+				) : (
+					<PageHeaderNewAnimation
+						Component={shapesByName[animation]}
+						color={this.colorByName[animation]}
+					/>
+				)}
 				<div className="page-header-new__content container">
 					<h1 className="page-header-new__title">{title}</h1>
 				</div>
@@ -80,13 +91,40 @@ const PageHeaderNewAnimation = ({ Component = null, color = '' }) => (
 	</div>
 );
 
+const PageHeaderNewAnimationBasic = ({ animationTriangleColor, animationBackgroundColor }) => (
+	<div className="page-header-new__animation--basic">
+		<div
+			className="page-header-new__animation-background"
+			style={{ backgroundColor: animationBackgroundColor }}
+		>
+			{['-50s', '-30s', '-10s'].map((x, i) => (
+				<svg
+					key={i}
+					style={{ animationDelay: x }}
+					fill={animationTriangleColor}
+					className="page-header-new__animation-triangle"
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 96 109"
+				>
+					<path
+						fillRule="evenodd"
+						d="M2.502.064l91.42 23.673a2 2 0 0 1 1.205 2.98L45.803 107.28a2 2 0 0 1-3.56-.296L.146 2.75A2 2 0 0 1 2.502.064z"
+					/>
+				</svg>
+			))}
+		</div>
+	</div>
+);
+
 PageHeaderNewAnimation.propTypes = {
 	Component: PropTypes.func,
 	color: PropTypes.string,
 };
 
 PageHeaderNew.propTypes = {
-	animation: PropTypes.oneOf(['diamond', 'doubleDiamond', 'triangles']),
+	animation: PropTypes.oneOf(['basic', 'diamond', 'doubleDiamond', 'triangles']),
+	animationBackgroundColor: PropTypes.object,
+	animationTriangleColor: PropTypes.object,
 	title: PropTypes.string,
 };
 

c779fe02db4faf045e68c9f58a8837b6cc289dfb
James Peter Perrone Jefferies <james.p.perrone@protonmail.com>
2020-08-07 16:32:26 +0200

    Fix: Custom 404 page. (#562)
    * feat(app): add a 404.js file
    
    * feat(404): use getStaticProps
    
    * refactor(404): clean up
    
    * fix(404): data fetch

---
+++ pages/404.js
@@ -0,0 +1,56 @@
+import '../styles/index.less';
+
+import fetchContent from '../lib/fetch-content';
+
+import Head from '../components/head/head';
+import MenuBar from '../components/menu-bar/menu-bar';
+import TextCenter from '../components/text-center/text-center';
+import * as TextCenterShapes from '../components/text-center/text-center-shapes';
+import InlineImage from '../components/inline-image/inline-image';
+
+const Custom404 = ({ data }) => (
+	<>
+		<Head title="Hike One - Error" />
+
+		<MenuBar />
+
+		<article className={`article article-error ${data.image ? 'article-error--has-image' : ''}`}>
+			<TextCenter title={data.title} text={data.description}>
+				<TextCenterShapes.variation3Back position="back" />
+				<TextCenterShapes.variation4Front position="front" />
+			</TextCenter>
+
+			{data.image && (
+				<section className="error-image container">
+					<div className="container-inner">
+						<InlineImage url={data.image.url} width={data.image.width} height={data.image.height} />
+					</div>
+				</section>
+			)}
+		</article>
+	</>
+);
+
+export async function getStaticProps() {
+	const graphqlQuery = `{
+			errorPage(filter: {error: {eq: "404"}}) {
+				title
+				description
+				image {
+					url
+					width
+					height
+				}
+			}
+		}`;
+
+	const data = await fetchContent({ graphqlQuery });
+
+	return {
+		props: {
+			data: data.errorPage,
+		},
+	};
+}
+
+export default Custom404;

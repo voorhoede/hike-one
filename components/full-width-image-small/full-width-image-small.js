fa812c316436d1d408b27141105c836808e30227
Vincent van Dijck <vincent@voorhoede.nl>
2020-10-07 14:35:01 +0200

    feat(full-width-image-small) improved responsive images

--- components/full-width-image-small/full-width-image-small.js
+++ components/full-width-image-small/full-width-image-small.js
@@ -1,20 +1,58 @@
 import PropTypes from 'prop-types';
 import setImageParams from '../_helpers/setImageParameters';
 
-const FullWidthImageSmall = ({ image = '#', index = 0 }) => {
+const FullWidthImageSmall = ({ image = '', index = 0 }) => {
+	const imageParams = { fit: 'crop' };
 	const style = {
 		__html: `<style>
-			.full-width-image-small-${index} {
-				background-image: url('${setImageParams(image, { w: 500 })}')
+			@media (max-width: 499px) {
+				.full-width-image-small-${index} {
+					background-image: url('${setImageParams(image, { ...imageParams, w: 500, h: 214 })}')
+				}
+			}
+			@media (max-width: 499px) and (min-resolution: 192dpi) {
+				.full-width-image-small-${index} {
+					background-image: url('${setImageParams(image, { ...imageParams, w: 500, h: 214, dpr: 2 })}')
+				}
 			}
 			@media (min-width: 500px) {
 				.full-width-image-small-${index} {
-					background-image: url('${setImageParams(image, { w: 1000 })}')
+					background-image: url('${setImageParams(image, { ...imageParams, w: 768, h: 300 })}')
+				}
+			}
+			@media (min-width: 500px) and (min-resolution: 192dpi) {
+				.full-width-image-small-${index} {
+					background-image: url('${setImageParams(image, { ...imageParams, w: 768, h: 300, dpr: 2 })}')
+				}
+			}
+			@media (min-width: 768px) {
+				.full-width-image-small-${index} {
+					background-image: url('${setImageParams(image, { ...imageParams, w: 1170, h: 400 })}')
+				}
+			}
+			@media (min-width: 768px) and (min-resolution: 192dpi) {
+				.full-width-image-small-${index} {
+					background-image: url('${setImageParams(image, { ...imageParams, w: 1170, h: 400, dpr: 2 })}')
 				}
 			}
 			@media (min-width: 1170px) {
 				.full-width-image-small-${index} {
-					background-image: url('${setImageParams(image, { w: 1920 })}')
+					background-image: url('${setImageParams(image, { ...imageParams, w: 1440, h: 610 })}')
+				}
+			}
+			@media (min-width: 1170px) and (min-resolution: 192dpi) {
+				.full-width-image-small-${index} {
+					background-image: url('${setImageParams(image, { ...imageParams, w: 1440, h: 610, dpr: 2 })}')
+				}
+			}
+			@media (min-width: 1440px) {
+				.full-width-image-small-${index} {
+					background-image: url('${setImageParams(image, { ...imageParams, w: 1920, h: 610 })}')
+				}
+			}
+			@media (min-width: 1440px) and (min-resolution: 192dpi) {
+				.full-width-image-small-${index} {
+					background-image: url('${setImageParams(image, { ...imageParams, w: 1920, h: 610, dpr: 2 })}')
 				}
 			}
 		</style>`,

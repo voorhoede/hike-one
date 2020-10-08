5c54e2497fba9ac0fcb2931ca6d068f64a81afd3
Vincent van Dijck <vincent@voorhoede.nl>
2020-10-08 11:50:41 +0200

    feat(quote-block) improved responsive images

--- components/quote-block/quote-block.js
+++ components/quote-block/quote-block.js
@@ -1,4 +1,7 @@
 import PropTypes from 'prop-types';
+import setImageParams from '../_helpers/setImageParameters';
+
+const imageParams = { fit: 'max' };
 
 const QuoteBlock = ({
 	color = '',
@@ -11,7 +14,19 @@ const QuoteBlock = ({
 	<blockquote className={`colored-block ${color} ${alignment}`}>
 		{quote && <span className="content">“{quote}”</span>}
 		<cite className="quote-cite">
-			{quote && <img src={citeImage} alt="" />}
+			{quote && (
+				<img
+					srcSet={`
+						${setImageParams(citeImage, { ...imageParams, w: 70, h: 70 })} 70w,
+						${setImageParams(citeImage, { ...imageParams, w: 70, h: 70, dpr: 2 })} 140w,
+						${setImageParams(citeImage, { ...imageParams, w: 100, h: 100 })} 100w,
+						${setImageParams(citeImage, { ...imageParams, w: 100, h: 100, dpr: 2 })} 200w
+					`}
+					sizes={`(max-width: 767px) 70px, (min-width: 768px) 100px`}
+					src={`${setImageParams(citeImage, { ...imageParams, w: 100, h: 100 })}`}
+					alt=""
+				/>
+			)}
 			<div className="quote-cite-content content">
 				<p className="quote-cite-name">{citeName}</p>
 				<p className="quote-cite-title">{citeTitle}</p>

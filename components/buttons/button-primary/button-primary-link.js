c4498000b2c6a571413c78638e8946e120f3512d
Vincent van Dijck <vincent@voorhoede.nl>
2020-10-28 14:41:06 +0100

    feat(buttons) if href starts with hashtag use anchor tag

--- components/buttons/button-primary/button-primary-link.js
+++ components/buttons/button-primary/button-primary-link.js
@@ -2,14 +2,23 @@ import PropTypes from 'prop-types';
 import Link from 'next/link';
 import Icon from '../../icon/icon';
 
-const ButtonPrimaryLink = ({ classes = '', href = '', children, icon = '', target = '_self' }) => (
-	<Link href={href.replace('https://hike.one/', '/')}>
-		<a target={target} className={`btn-primary ${classes} ${icon ? 'btn-icon' : ''}`}>
+const ButtonPrimaryLink = ({ classes = '', href = '', children, icon = '', target = '_self' }) => {
+	const hrefIsTarget = href.startsWith('#');
+
+	return hrefIsTarget ? (
+		<a href={href} target={target} className={`btn-primary ${classes} ${icon ? 'btn-icon' : ''}`}>
 			{children}
 			{icon && <Icon icon={icon} />}
 		</a>
-	</Link>
-);
+	) : (
+		<Link href={href.replace('https://hike.one/', '/')}>
+			<a target={target} className={`btn-primary ${classes} ${icon ? 'btn-icon' : ''}`}>
+				{children}
+				{icon && <Icon icon={icon} />}
+			</a>
+		</Link>
+	);
+};
 
 ButtonPrimaryLink.propTypes = {
 	children: PropTypes.node,

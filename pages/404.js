578626b87ddc2258f7c3deced99d3fae3e83bfc5
James Peter Perrone Jefferies <james.p.perrone@protonmail.com>
2020-11-09 12:02:37 +0100

    fix(redirects): try 3

--- pages/404.js
+++ pages/404.js
@@ -8,22 +8,28 @@ import TextCenter from '../components/text-center/text-center';
 import * as TextCenterShapes from '../components/text-center/text-center-shapes';
 import InlineImage from '../components/inline-image/inline-image';
 
-const Custom404 = ({ data }) => (
+const Custom404 = ({ errorPage }) => (
 	<>
 		<Head title="Hike One - Error" />
 
 		<MenuBar />
 
-		<article className={`article article-error ${data.image ? 'article-error--has-image' : ''}`}>
-			<TextCenter title={data.title} text={data.description}>
+		<article
+			className={`article article-error ${errorPage.image ? 'article-error--has-image' : ''}`}
+		>
+			<TextCenter title={errorPage.title} text={errorPage.description}>
 				<TextCenterShapes.variation3Back position="back" />
 				<TextCenterShapes.variation4Front position="front" />
 			</TextCenter>
 
-			{data.image && (
+			{errorPage.image && (
 				<section className="error-image container">
 					<div className="container-inner">
-						<InlineImage url={data.image.url} width={data.image.width} height={data.image.height} />
+						<InlineImage
+							url={errorPage.image.url}
+							width={errorPage.image.width}
+							height={errorPage.image.height}
+						/>
 					</div>
 				</section>
 			)}
@@ -31,25 +37,21 @@ const Custom404 = ({ data }) => (
 	</>
 );
 
-export async function getStaticProps({ preview }) {
+export async function getStaticProps() {
 	const graphqlQuery = `{
-			errorPage(filter: {error: {eq: "404"}}) {
-				title
-				description
-				image {
-					url
-					width
-					height
-				}
+		errorPage(filter: {error: {eq: "404"}}) {
+			title
+			description
+			image {
+				url
+				width
+				height
 			}
-		}`;
-
-	const data = await fetchContent({ graphqlQuery, preview });
+		}
+	}`;
 
 	return {
-		props: {
-			data: data.errorPage,
-		},
+		props: await fetchContent({ graphqlQuery }),
 	};
 }
 

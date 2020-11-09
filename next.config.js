9abbbf82979a2ce10b4939fc6384e17e44c6eb41
James Peter Perrone Jefferies <james.p.perrone@protonmail.com>
2020-11-09 12:15:20 +0100

    fix(redirects): disable `localeDetection`

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ const withLess = require('@zeit/next-less');
 module.exports = withLess({
 	poweredByHeader: false,
 	reactStrictMode: true,
+	i18n: {
+		localeDetection: false,
+		defaultLocale: 'en',
+		locales: ['en'],
+	},
 });

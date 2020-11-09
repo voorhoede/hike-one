8797d2100608eb9775838d3ce440652a9f27e469
James Peter Perrone Jefferies <james.p.perrone@protonmail.com>
2020-11-09 11:55:36 +0100

    fix(redirects): try 2

--- pages/404.js
+++ pages/404.js
@@ -8,28 +8,22 @@ import TextCenter from '../components/text-center/text-center';
 import * as TextCenterShapes from '../components/text-center/text-center-shapes';
 import InlineImage from '../components/inline-image/inline-image';
 
-const Custom404 = ({ errorPage }) => (
+const Custom404 = ({ data }) => (
 	<>
 		<Head title="Hike One - Error" />
 
 		<MenuBar />
 
-		<article
-			className={`article article-error ${errorPage.image ? 'article-error--has-image' : ''}`}
-		>
-			<TextCenter title={errorPage.title} text={errorPage.description}>
+		<article className={`article article-error ${data.image ? 'article-error--has-image' : ''}`}>
+			<TextCenter title={data.title} text={data.description}>
 				<TextCenterShapes.variation3Back position="back" />
 				<TextCenterShapes.variation4Front position="front" />
 			</TextCenter>
 
-			{errorPage.image && (
+			{data.image && (
 				<section className="error-image container">
 					<div className="container-inner">
-						<InlineImage
-							url={errorPage.image.url}
-							width={errorPage.image.width}
-							height={errorPage.image.height}
-						/>
+						<InlineImage url={data.image.url} width={data.image.width} height={data.image.height} />
 					</div>
 				</section>
 			)}
@@ -37,21 +31,25 @@ const Custom404 = ({ errorPage }) => (
 	</>
 );
 
-export async function getStaticProps() {
+export async function getStaticProps({ preview }) {
 	const graphqlQuery = `{
-		errorPage(filter: {error: {eq: "404"}}) {
-			title
-			description
-			image {
-				url
-				width
-				height
+			errorPage(filter: {error: {eq: "404"}}) {
+				title
+				description
+				image {
+					url
+					width
+					height
+				}
 			}
-		}
-	}`;
+		}`;
+
+	const data = await fetchContent({ graphqlQuery, preview });
 
 	return {
-		props: await fetchContent({ graphqlQuery }),
+		props: {
+			data: data.errorPage,
+		},
 	};
 }
 

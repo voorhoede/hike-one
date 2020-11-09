9cc6e082f923741207e74b0e95d0f57fc17186e0
James Peter Perrone Jefferies <james.p.perrone@protonmail.com>
2020-11-09 12:19:30 +0100

    Fix: Redirects (#599)

--- next.config.js
+++ next.config.js
@@ -3,8 +3,4 @@ const withLess = require('@zeit/next-less');
 module.exports = withLess({
 	poweredByHeader: false,
 	reactStrictMode: true,
-	i18n: {
-		defaultLocale: 'en',
-		locales: ['en'],
-	},
 });

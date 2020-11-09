d63d866995a8315c95b87ee499a0665acb588fa4
James Peter Perrone Jefferies <james.p.perrone@protonmail.com>
2020-11-09 12:17:51 +0100

    fix(redirects): remove `i18n` from `next.config.js`

--- next.config.js
+++ next.config.js
@@ -3,9 +3,4 @@ const withLess = require('@zeit/next-less');
 module.exports = withLess({
 	poweredByHeader: false,
 	reactStrictMode: true,
-	i18n: {
-		localeDetection: false,
-		defaultLocale: 'en',
-		locales: ['en'],
-	},
 });

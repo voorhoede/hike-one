d09006a5b282e2a2eabcd8401d26242d98963ed2
James Peter Perrone Jefferies <james.p.perrone@protonmail.com>
2020-11-09 12:09:17 +0100

    fix(redirects): try 4

--- next.config.js
+++ next.config.js
@@ -3,26 +3,4 @@ const withLess = require('@zeit/next-less');
 module.exports = withLess({
 	poweredByHeader: false,
 	reactStrictMode: true,
-	// Taken from: https://github.com/developit/nextjs-preact-demo/blob/e7f359b52e8fdd25b8a55f95725c0f8b0fc883c0/next.config.js
-	webpack(config) {
-		const splitChunks = config.optimization && config.optimization.splitChunks;
-		if (splitChunks) {
-			const cacheGroups = splitChunks.cacheGroups;
-			const preactModules = /[\\/]node_modules[\\/](preact|preact-render-to-string|preact-context-provider)[\\/]/;
-			if (cacheGroups.framework) {
-				cacheGroups.preact = Object.assign({}, cacheGroups.framework, {
-					test: preactModules,
-				});
-				cacheGroups.commons.name = 'framework';
-			} else {
-				cacheGroups.preact = {
-					name: 'commons',
-					chunks: 'all',
-					test: preactModules,
-				};
-			}
-		}
-
-		return config;
-	},
 });

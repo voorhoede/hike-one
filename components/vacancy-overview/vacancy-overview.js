d048c5dfddef6e28d2f01f8f11603ee3177d716a
Vincent van Dijck <vvandijck@users.noreply.github.com>
2021-05-04 14:57:19 +0200

    Feat/homerun vacancies component (#625)
    * feat(lib) sort vacancies by department
    
    * feat(vacancy-overview) show vacancies by department
    
    * chore(vacancy-overview) linting fix
    
    * chore(job-scraper) linting fix
    
    * feat(contact) move VacancyOverview to bottom
    
    * feat(vacancy-overview) use component for title and tagline
    
    * feat(vacancy-overview) responsive font sizes
    
    * feat(index) add VacancyOverview
    
    * fix(vacancy-overview) use different button style
    
    * feat(menu-bar) extra ‘jobs’ menu item

--- components/vacancy-overview/vacancy-overview.js
+++ components/vacancy-overview/vacancy-overview.js
@@ -1,25 +1,37 @@
 import PropTypes from 'prop-types';
-import ButtonPrimaryLink from '../buttons/button-primary/button-primary-link';
+import ButtonSecondaryLink from '../buttons/button-secondary/button-secondary-link';
+import TextCenter from '../text-center/text-center';
 
 const VacancyOverview = ({ overview = {}, vacancies = [] }) => (
 	<div className="vacancy-overview container">
-		<h2 className="vacancy-overview-title">{overview.title}</h2>
-		<p className="vacancy-overview-tagline">{overview.tagline}</p>
-		<ul className="vacancy-overview-list">
-			{vacancies.map((vacancy, index) => (
-				<li key={index} className="vacancy-overview-list-item">
-					<a href={vacancy.url} target="_blank" rel="noopener noreferrer">
-						<h4>{vacancy.title}</h4>
-						<span>{vacancy.duration}</span>
-						<span>{vacancy.location}</span>
-					</a>
-				</li>
-			))}
-		</ul>
+		<TextCenter title={overview.title} text={overview.tagline} />
 
-		<ButtonPrimaryLink href={overview.callToActionUrl} target="_blank" classes="btn-large content">
+		{vacancies.map((department, index) => (
+			<div className="vacancy-overview__department" key={index}>
+				<h3 className="vacancy-overview__department-name">{department.name}</h3>
+
+				<ul className="vacancy-overview__list">
+					{department.jobs.map((job, index) => (
+						<li className="vacancy-overview__list-item" key={index}>
+							<a href={job.url} target="_blank" rel="noopener noreferrer">
+								<h4>{job.title}</h4>
+								<span>{job.duration}</span>
+								<span>{job.location}</span>
+							</a>
+						</li>
+					))}
+				</ul>
+			</div>
+		))}
+
+		<ButtonSecondaryLink
+			href={overview.callToActionUrl}
+			target="_blank"
+			classes="btn-red-border"
+			icon="arrowRight"
+		>
 			{overview.callToActionTitle}
-		</ButtonPrimaryLink>
+		</ButtonSecondaryLink>
 	</div>
 );
 

4417d6d419c462b02bcfaf2677c93f7e517b99ba
Bas de Vleeschhouwer <bdevleeschhouwer@protonmail.com>
2021-10-29 16:10:31 +0200

    Also remove confetti children from components

--- components/image-combo/image-combo.js
+++ components/image-combo/image-combo.js
@@ -2,16 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const ImageCombo = ({ hasText = false, children }) => {
-	const childrenArray = React.Children.toArray(children);
-	const childrenRest = childrenArray.filter(
-		(child) => child.props.position !== 'front' || child.props.position !== 'back'
-	);
-	const parallaxLayerFront = childrenArray.find((child) => child.props.position === 'front');
-
 	return (
 		<div className={`image-combo ${hasText ? 'image-combo--has-text' : ''}`}>
-			{childrenRest}
-			{parallaxLayerFront}
+			{children}
 		</div>
 	);
 };
